70515640025f1c7a2a03901224636f8267278a78
Danny Burkes & Mark Rushakoff <pair+danny+mrushakoff@pivotallabs.com>
2011-11-07 16:33:01 -0800

    Handle users with no fullName

--- spec/javascripts/views/header_spec.js
+++ spec/javascripts/views/header_spec.js
@@ -16,27 +16,58 @@ describe("chorus.views.Header", function() {
     });
 
     it("should have a link to the dashboard", function() {
-       expect(this.view.$(".logo a").attr("href")).toBe("#/");
+        expect(this.view.$(".logo a").attr("href")).toBe("#/");
     });
 
     describe("username", function() {
-        describe("less than or equal to 20 characters", function() {
-            it("displays the user's full name", function() {
-                expect(this.view.$(".username").text()).toBe("Daniel Francis Burke");
-            })
-        })
-
-        describe("greater than 20 characters", function() {
+        describe("where the user has no fullName", function() {
             beforeEach(function() {
-                chorus.user.set({
-                    "lastName" : "Burkes",
-                    "fullName": "Daniel Francis Burkes"
-                });
-                this.view.render();
-            });
+                chorus.user.unset("fullName");
+            })
 
-            it("displays the user's full name", function() {
-                expect(this.view.$(".username").text()).toBe("Daniel B.");
+            describe("and the synthesized full name is less than 21 characters", function() {
+                beforeEach(function() {
+                    chorus.user.set({ firstName: "0123456789", lastName: "012345" });
+                    this.view.render();
+                });
+
+                it("displays the synthesized full name", function() {
+                    expect(this.view.$(".username").text()).toBe("0123456789 012345");
+                });
+            })
+
+            describe("and the synthesized full name is more than 20 characters", function() {
+                beforeEach(function() {
+                    chorus.user.set({ firstName: "012345678901234", lastName: "0123456789" });
+                    this.view.render();
+                })
+
+                it("displays the abbreviated synthesized full name", function() {
+                    expect(this.view.$(".username").text()).toBe("012345678901234 0.");
+                });
+             })
+
+        });
+
+        describe("where the user has a fullName", function() {
+            describe("less than or equal to 20 characters", function() {
+                it("displays the user's full name", function() {
+                    expect(this.view.$(".username").text()).toBe("Daniel Francis Burke");
+                })
+            })
+
+            describe("greater than 20 characters", function() {
+                beforeEach(function() {
+                    chorus.user.set({
+                        "lastName" : "Burkes",
+                        "fullName": "Daniel Francis Burkes"
+                    });
+                    this.view.render();
+                });
+
+                it("displays the user's abbreviated full name", function() {
+                    expect(this.view.$(".username").text()).toBe("Daniel B.");
+                })
             })
         })
     })
