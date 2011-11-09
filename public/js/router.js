df0752bfd44e0111073c7d5edc625162fed82973
Mark Rushakoff <pair+mrushakoff@pivotallabs.com>
2011-11-09 13:59:02 -0800

    Further meta/refactoring on Router

--- public/js/router.js
+++ public/js/router.js
@@ -1,31 +1,18 @@
 (function($, ns) {
     ns.Router = Backbone.Router.extend({
-        routes: {
-            ""    :   "dashboard",
-            "/"    :   "dashboard",
-            "/login"        :   "login",
-            "/users"        :   "users",
-            "/users/new"        :   "userNew"
-        },
-
-        dashboard : function() {
-            page("Dashboard");
-        },
-
-        login : function() {
-            page("Login");
-        },
-
-        users : function () {
-            page("UserIndex");
-        },
-
-        userNew : function () {
-            page("UserNew");
+        initialize : function(){
+            this.route("", "dashboard", makePage("Dashboard"));
+            this.route("/", "dashboard", makePage("Dashboard"));
+            this.route("/login", "login", makePage("Login"));
+            this.route("/users", "users", makePage("UserIndex"));
+            this.route("/users/new", "userNew", makePage("UserNew"));
         }
     });
 
     function page(className){
         $("#content").html(new chorus.pages[className + "Page"]().render().el);
+    }
+    function makePage(className) {
+        return function() { page(className); }
     }
 })(jQuery, chorus);
