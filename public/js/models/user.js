5429dd9df923453dea55f7e653ac3198aa2d59af
Charles Hansen, Mark Rushakoff & Max Brunsfeld <pair+chansen+mrushakoff+mbrunsfeld@pivotallabs.com>
2011-11-16 15:13:35 -0800

    User#getWorkspaces

--- public/js/models/user.js
+++ public/js/models/user.js
@@ -1,5 +1,11 @@
 (function(ns) {
     ns.User = chorus.models.Base.extend({
-        urlTemplate : "user/{{userName}}"
+        urlTemplate : "user/{{userName}}",
+
+        getWorkspaces: function() {
+            var workspaces = new ns.WorkspaceSet();
+            workspaces.urlTemplate = "workspaces?user=" + this.get("userName");
+            return workspaces;
+        }
     });
 })(chorus.models);
--- spec/javascripts/models/user_spec.js
+++ spec/javascripts/models/user_spec.js
@@ -7,5 +7,27 @@ describe("chorus.models.User", function() {
 
     it("has the correct urlTemplate", function() {
         expect(this.model.urlTemplate).toBe("user/{{userName}}");
-    })
+    });
+
+    describe("#getWorkspaces", function() {
+       beforeEach(function() {
+          this.user = new models.User({userName: "dr_charlzz"});
+          this.workspaces = this.user.getWorkspaces();
+       });
+
+       it("returns an instance of WorkspaceSet", function() {
+          expect(this.workspaces instanceof chorus.models.WorkspaceSet).toBeTruthy();
+       });
+
+       context("when fetched", function() {
+          beforeEach(function() {
+              this.workspaces.fetch();
+          });
+
+          it("hits the right url for that user", function() {
+            var expectedUrl = "/edc/workspaces?user=" + this.user.get("userName");
+            expect(this.server.requests[0].url).toBe(expectedUrl);
+          });
+       });
+    });
 });
