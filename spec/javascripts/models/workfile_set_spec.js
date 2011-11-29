617d4c0e03f5f0765886c4637682dbe88494503a
Michael Sofaer & Nitin Grewal <pair+msofaer+nitin@pivotallabs.com>
2011-11-29 10:49:10 -0800

    WorkfileSet supports a type attribute

--- spec/javascripts/models/workfile_set_spec.js
+++ spec/javascripts/models/workfile_set_spec.js
@@ -1,11 +1,21 @@
-describe("workfile_set", function(){
-    describe("#fetch", function(){
-        beforeEach(function(){
+describe("workfile_set", function() {
+    describe("#fetch", function() {
+        beforeEach(function() {
             this.collection = new chorus.models.WorkfileSet([], {workspaceId : 1234});
         });
 
-        it("creates the right URL", function(){
-            expect(this.collection.url()).toBe("/edc/workspace/1234/workfile");
-        });
+        describe("without filtering", function() {
+            it("creates the right URL", function() {
+                expect(this.collection.url()).toBe("/edc/workspace/1234/workfile");
+            });
+        })
+        describe("with filtering", function() {
+            beforeEach(function() {
+                this.collection.attributes.type = "sql"
+            })
+            it("it has correct Url", function() {
+                expect(this.collection.url()).toBe("/edc/workspace/1234/workfile?type=sql");
+            })
+        })
     });
 });
