1c2548097316cdeace716cb20af22b8d5f59f3af
Charles Hansen & Nitin Grewal <pair+chansen+nitin@pivotallabs.com>
2011-11-30 17:56:55 -0800

    When duplicate file in uploading , displaying the error message.

--- public/js/dialogs/workspaces/workfiles_import_dialog.js
+++ public/js/dialogs/workspaces/workfiles_import_dialog.js
@@ -16,11 +16,17 @@
 
         setup : function() {
             var self = this;
-            $(document).one('close.facebox', function(){
+            $(document).one('close.facebox', function() {
                 if (self.request) {
                     self.request.abort();
                 }
             });
+        },
+
+        additionalContext : function(ctx) {
+            return {
+                serverErrors : this.serverErrors
+            }
         },
 
         upload : function(e) {
@@ -39,7 +45,7 @@
                 }).spin();
                 this.$("button.submit").
                     text(t("workfiles.import_dialog.uploading")).
-                    append(spinner.el).
+                    append($('<span class="spinner"/>').append(spinner.el)).
                     attr("disabled", "disabled").
                     addClass("expanded");
             }
@@ -58,7 +64,7 @@
             var self = this;
             // FF3.6 fails tests with multipart true, but it will only upload in the real world with multipart true
             var multipart = !window.jasmine;
-            
+
             this.$("input[type=file]").fileupload({
                 change : fileChosen,
                 add : fileChosen,
@@ -66,7 +72,7 @@
                 multipart : multipart
             });
 
-            function fileChosen(e,data) {
+            function fileChosen(e, data) {
                 if (data.files.length > 0) {
                     self.$("button.submit").removeAttr("disabled");
                     self.uploadObj = data;
@@ -76,20 +82,31 @@
                     self.$('img').attr('src', iconSrc);
                     self.$('span.fileName').text(filename);
                 }
-            }
+            }http://localhost:8888/?spec=WorkfilesImportDialog%20when%20a%20text%20file%20has%20been%20chosen%20when%20the%20upload%20completes%20navigates%20to%20the%20workfile%20index.
 
-            function uploadFinished(e, data){
+            function uploadFinished(e, data) {
                 var json = $.parseJSON(data.result)
-                self.model.set({id: json.resource[0].id});
-                self.closeModal();
-                var url;
-                if (self.uploadExtension.toLowerCase() == "txt" || self.uploadExtension.toLowerCase() == "sql") {
-                    url = self.model.showUrl();
-                } else {
-                    url = self.model.workspace.showUrl() +"/workfiles"
-                }
+                if (json.status == "ok") {
+                    self.model.set({id: json.resource[0].id});
+                    self.closeModal();
+                    var url;
+                    if (self.uploadExtension.toLowerCase() == "txt" || self.uploadExtension.toLowerCase() == "sql") {
+                        url = self.model.showUrl();
+                    } else {
+                        url = self.model.workspace.showUrl() + "/workfiles"
+                    }
 
-                chorus.router.navigate(url, true);
+                    chorus.router.navigate(url, true);
+                }
+                else {
+                    e.preventDefault();
+                    if (self.request) {
+                        self.request.abort();
+                    }
+                    self.serverErrors = json.message
+                    self.render();
+
+                }
             }
         }
     });
--- spec/javascripts/dialogs/workfiles_import_dialog_spec.js
+++ spec/javascripts/dialogs/workfiles_import_dialog_spec.js
@@ -19,11 +19,11 @@ describe("WorkfilesImportDialog", function() {
         });
 
         it("disables the upload button", function() {
-                expect(this.dialog.$("button.submit").attr("disabled")).toBe("disabled");
+            expect(this.dialog.$("button.submit").attr("disabled")).toBe("disabled");
         });
 
-        context("clicking on the cancel button", function(){
-            it("closes the dialog", function(){
+        context("clicking on the cancel button", function() {
+            it("closes the dialog", function() {
                 spyOn(this.dialog, "closeModal");
                 this.dialog.$("button.cancel").click();
                 expect(this.dialog.closeModal).toHaveBeenCalled();
@@ -31,21 +31,23 @@ describe("WorkfilesImportDialog", function() {
         });
     });
 
-    context("when a text file has been chosen", function(){
-        context("when the upload completes", function(){
-            beforeEach(function(){
+    context("when a text file has been chosen", function() {
+        context("when the upload completes", function() {
+            beforeEach(function() {
                 this.dialog.render();
-                this.fileList = [{fileName: 'foo.txt'}];
+                this.fileList = [
+                    {fileName: 'foo.txt'}
+                ];
                 this.dialog.$("input[type=file]").fileupload('add', {files: this.fileList});
                 this.dialog.$("button.submit").click();
 
                 spyOn(chorus.router, "navigate");
                 // calls any 'done' callbacks
-                this.server.respondWith([200, {'Content-Type': 'text/plain'}, '{"resource":[{"id":"9"}]}']);
+                this.server.respondWith([200, {'Content-Type': 'text/plain'}, '{"resource":[{"id":"9"}], "status": "ok"}']);
                 this.server.respond();
             });
 
-            it("navigates to the workfile index", function(){
+            it("navigates to the workfile index", function() {
                 expect(chorus.router.navigate).toHaveBeenCalledWith(this.dialog.model.showUrl(), true);
             });
         });
@@ -55,7 +57,9 @@ describe("WorkfilesImportDialog", function() {
         beforeEach(function() {
             spyOn(this.dialog, "closeModal");
             this.dialog.render();
-            this.fileList = [{fileName: 'foo.bar'}];
+            this.fileList = [
+                {fileName: 'foo.bar'}
+            ];
             this.dialog.$("input[type=file]").fileupload('add', {files: this.fileList});
         });
 
@@ -95,7 +99,7 @@ describe("WorkfilesImportDialog", function() {
                 expect(this.dialog.$("button.submit").attr("disabled")).toBe("disabled");
             });
 
-            it("adds the expanded class to the upload button", function(){
+            it("adds the expanded class to the upload button", function() {
                 expect(this.dialog.$("button.submit")).toHaveClass("expanded");
             });
 
@@ -103,48 +107,76 @@ describe("WorkfilesImportDialog", function() {
                 expect(this.dialog.$("button.submit").text()).toBe(t("workfiles.import_dialog.uploading"));
             });
 
-            context("when cancel is clicked before the upload completes", function(){
-                beforeEach(function(){
+            context("when cancel is clicked before the upload completes", function() {
+                beforeEach(function() {
                     spyOn(this.dialog.request, "abort");
                     this.dialog.$("button.cancel").click();
                 });
 
-                it("closes the dialog", function(){
+                it("closes the dialog", function() {
                     expect(this.dialog.closeModal).toHaveBeenCalled();
                 });
 
-                it("cancels the upload", function(){
+                it("cancels the upload", function() {
                     expect(this.dialog.request.abort).toHaveBeenCalled();
                 });
             });
 
-            context("when the close 'X' is clicked", function(){
-                beforeEach(function(){
+            context("when the close 'X' is clicked", function() {
+                beforeEach(function() {
                     spyOn(this.dialog.request, "abort");
                     $(document).trigger("close.facebox");
                 });
 
-                it("cancels the upload", function(){
+                it("cancels the upload", function() {
                     expect(this.dialog.request.abort).toHaveBeenCalled();
                 });
             });
 
-            context("when the upload completes", function(){
-                beforeEach(function(){
+            context("when the upload completes", function() {
+                beforeEach(function() {
                     spyOn(chorus.router, "navigate");
                     // calls any 'done' callbacks
                     this.server.respondWith("OK");
-                    this.server.respondWith([200, {'Content-Type': 'text/plain'}, '{"resource":[{"id":"9"}]}']);
+                    this.server.respondWith([200, {'Content-Type': 'text/plain'}, '{"resource":[{"id":"9"}],"status": "ok"}']);
                     this.server.respond();
                 });
 
-                it("closes the dialog", function(){
+                it("closes the dialog", function() {
                     expect(this.dialog.closeModal).toHaveBeenCalled();
                 });
 
-                it("navigates to the workfile index", function(){
+                it("navigates to the workfile index", function() {
                     expect(chorus.router.navigate).toHaveBeenCalledWith("#/workspaces/4/workfiles", true);
                 });
+            });
+
+            context("when the upload gives a server error", function() {
+                beforeEach(function() {
+                    spyOn(this.dialog.request, "abort");
+                    // calls any 'done' callbacks
+                    this.server.respondWith("OK");
+                    this.server.respondWith([200, {'Content-Type': 'text/plain'}, '{"status": "fail", "message" :[{"message":"Workspace already has a workfile with this name. Specify a different name."}]}']);
+                    this.server.respond();
+//                    this.dialog.uploadFinished();
+                });
+
+                it("cancels the upload", function() {
+                    expect(this.dialog.request.abort).toHaveBeenCalled();
+                });
+
+                it("disables the upload button", function() {
+                    expect(this.dialog.$("button.submit").attr("disabled")).toBe("disabled");
+                });
+
+                it("does not display a spinner on the upload button", function() {
+                    expect(this.dialog.$("button.submit div[aria-role=progressbar]").length).toBe(0);
+                });
+
+                it("display the correct error" ,function(){
+                    expect(this.dialog.$(".errors").text()).toBe("Workspace already has a workfile with this name. Specify a different name.")
+                })
+                
             });
         });
     });
