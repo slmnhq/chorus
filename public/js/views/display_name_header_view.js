b1f67c3c85deb80a82e597899da79e792ab58430
Charles Hansen & Mark Rushakoff <pair+chansen+mrushakoff@pivotallabs.com>
2011-12-05 10:16:19 -0800

    Remove stray console.log

--- public/js/views/display_name_header_view.js
+++ public/js/views/display_name_header_view.js
@@ -3,7 +3,6 @@
         className : "default_content_header",
 
         additionalContext : function(ctx) {
-            console.log(this.model)
             return {
                title : this.model && this.model.loaded ? this.model.displayName() : ""
             }
