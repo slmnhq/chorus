c22a9ce62203176e97b2076acf45e6f303b40d8c
Charles Hansen & Mark Rushakoff <pair+chansen+mrushakoff@pivotallabs.com>
2011-12-06 09:43:38 -0800

    save workspace settings functionality

--- public/js/dialogs/workspaces/workspace_settings_dialog.js
+++ public/js/dialogs/workspaces/workspace_settings_dialog.js
@@ -4,6 +4,21 @@
         className : "workspace_settings",
         title : t("workspace.settings.title"),
 
+        events : {
+            "submit form" : "updateWorkspace",
+            "click button.submit" : "updateWorkspace"
+        },
+
+        updateWorkspace : function(e) {
+            e.preventDefault();
+            this.model.set({
+                name: this.$("input[name=name]").val().trim(),
+                summary: this.$("textarea[name=summary]").val().trim()
+            });
+
+            this.model.save();
+        },
+
         makeModel : function() {
             this.id = this.options.launchElement.data("id");
 
--- spec/javascripts/dialogs/workspace_settings_dialog_spec.js
+++ spec/javascripts/dialogs/workspace_settings_dialog_spec.js
@@ -10,14 +10,34 @@ describe("WorkspaceSettings", function() {
         beforeEach(function() {
             this.dialog.render();
         });
-        it("should have the correct title", function() {
+        it("has the correct title", function() {
             expect(this.dialog.title).toMatchTranslation("workspace.settings.title");
         });
-        it("should have an input for workspace name", function() {
+        it("has an input for workspace name", function() {
             expect(this.dialog.$("input[name=name]").val()).toBe(this.dialog.model.get("name"))
         });
-        it("should have a text area for summary", function() {
+        it("has a text area for summary", function() {
            expect(this.dialog.$("textarea[name=summary]").val()).toBe(this.dialog.model.get("summary"));
         });
+        context("submitting the form", function() {
+            beforeEach(function() {
+                this.spy = spyOn(this.dialog.model, "save");
+                this.dialog.$("input[name=name]").val("my modified name");
+                this.dialog.$("textarea[name=summary]").val("my modified summary");
+                this.dialog.$('form').submit();
+            })
+
+            it("saves the workspace", function() {
+                expect(this.spy).toHaveBeenCalled();
+            });
+
+            it("sets the name on the workspace", function() {
+                expect(this.dialog.model.get("name")).toBe("my modified name");
+            });
+
+            it("sets the name on the workspace", function() {
+                expect(this.dialog.model.get("summary")).toBe("my modified summary");
+            })
+        })
     })
 })
