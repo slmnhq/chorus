9cbece6ecb6dfc3011ad9cc93885649212ef4e51
Danny Burkes & Max Brunsfeld <pair+danny+mbrunsfeld@pivotallabs.com>
2011-12-06 15:34:17 -0800

    Allow for cache-busting param in user show view spec.

--- spec/javascripts/views/users/user_show_view_spec.js
+++ spec/javascripts/views/users/user_show_view_spec.js
@@ -37,7 +37,7 @@ describe("chorus.views.UserShow", function() {
             it("renders a profile image for the user", function() {
                 var image = this.view.$(".edit_photo img");
                 var userName = this.model.get("userName");
-                expect(image.attr('src')).toBe(this.model.imageUrl());
+                expect(image.attr('src').indexOf(this.model.imageUrl())).toBe(0);
             });
 
             it("renders the title text", function() {
