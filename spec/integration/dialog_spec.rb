54ca6117e476e34fd15c774be1a45c545b165db6
Danny Burkes & Mark Rushakoff <pair+danny+mrushakoff@pivotallabs.com>
2011-12-07 15:26:55 -0800

    save failing test's page

--- spec/integration/dialog_spec.rb
+++ spec/integration/dialog_spec.rb
@@ -2,12 +2,9 @@ require File.join(File.dirname(__FILE__), 'spec_helper')
 
 describe "dialogs" do
   it "pops up a dialog" do
-    pending
-
-    # failing on CI now, pending because popping up a dialog is tested by other specs
-    #
     login('edcadmin', 'secret')
     visit("/#/workspaces")
+    save_page
 
     page.should_not have_selector("#facebox .popup")
     page.find("button[data-dialog=WorkspacesNew]").click
