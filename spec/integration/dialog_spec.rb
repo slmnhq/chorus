43304497ca810321067e842444d65f8cdf5dc075
Mark Rushakoff <pair+mrushakoff@pivotallabs.com>
2011-12-07 14:22:28 -0800

    Fix failing dialog integration test

--- spec/integration/dialog_spec.rb
+++ spec/integration/dialog_spec.rb
@@ -3,8 +3,10 @@ require File.join(File.dirname(__FILE__), 'spec_helper')
 describe "dialogs" do
   it "pops up a dialog" do
     login('edcadmin', 'secret')
-    page.should_not have_selector(".popup")
+    visit("/#/workspaces")
+
+    page.should_not have_selector("#facebox .popup")
     page.find("button[data-dialog=WorkspacesNew]").click
-    page.should have_selector(".popup")
+    page.should have_selector("#facebox .popup")
   end
 end
