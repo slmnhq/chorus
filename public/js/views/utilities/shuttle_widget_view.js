720cf107fc8901264a007afb107e64280d5b7f89
Charles Hansen & Mark Rushakoff <pair+chansen+mrushakoff@pivotallabs.com>
2011-12-12 13:57:05 -0800

    First pass at shuttle widget

---
+++ public/js/views/utilities/shuttle_widget_view.js
@@ -0,0 +1,34 @@
+;(function($, ns) {
+    ns.views.ShuttleWidget = ns.views.Base.extend({
+        className : "shuttle_widget",
+
+        events : {
+            "click a.add" : "toggleAdd",
+            "click a.remove" : "toggleAdd"
+        },
+
+        collectionModelContext : function(model) {
+            var ctx = {};
+            ctx.isAdded = _.include(this.options.selectedIDs, model.get("id"));
+
+            return ctx;
+        },
+
+        toggleAdd : function(e) {
+            e.preventDefault();
+
+            var target = this.$(e.currentTarget);
+            var id = target.closest("li").data("id");
+            var isAdding = target.closest("ul").hasClass("available");
+
+            this.$("li[data-id='" + id + "']").toggleClass("added", isAdding);
+        },
+
+        getSelectedIDs : function() {
+            var selectedItems = this.$('ul.selected li.added');
+            return _.map(selectedItems, function(item) {
+                return $(item).data("id").toString();
+            });
+        }
+    })
+})(jQuery, chorus);
---
+++ spec/javascripts/views/utilities/shuttle_widget_view_spec.js
@@ -0,0 +1,75 @@
+describe("chorus.views.ShuttleWidget", function() {
+    beforeEach(function() {
+        this.loadTemplate("shuttle_widget")
+        fixtures.model = "UserSet";
+        this.collection = fixtures.modelFor("fetch");
+        this.selectedIDs = ["10001"];
+        this.view = new chorus.views.ShuttleWidget({ collection : this.collection, selectedIDs : this.selectedIDs });
+    });
+
+    describe("#render", function() {
+        beforeEach(function() {
+            this.view.render();
+        });
+
+        it("renders an li for each item in the available collection", function() {
+            expect(this.view.$("ul.available li").length).toBe(this.collection.length);
+        });
+
+        it("renders an li for each item in the selected list", function() {
+            expect(this.view.$("ul.selected li.added").length).toBe(this.selectedIDs.length);
+        });
+
+        it("adds the added class to each available item that is in the selected ID list", function() {
+            expect(this.view.$("ul.available li.added").length).toBe(this.selectedIDs.length);
+        });
+
+        it("returns the original list of selected IDs", function() {
+            var selected = this.view.getSelectedIDs();
+            expect(selected.length).toBe(1);
+            expect(selected[0]).toBe("10001");
+        });
+
+        describe("clicking the add link", function() {
+            beforeEach(function() {
+                this.view.$("li[data-id='10002'] a.add").click();
+            });
+
+            it("adds the 'added' class to the containing li", function() {
+                expect(this.view.$("ul.available li[data-id='10002']")).toHaveClass("added");
+            });
+
+            it("adds the 'added' class to the corresponding li in the selected ul", function(){
+                expect(this.view.$("ul.selected li[data-id='10002']")).toHaveClass("added");
+            });
+
+            it("returns the new list of selected IDs", function() {
+                var selected = this.view.getSelectedIDs();
+                expect(selected.length).toBe(2);
+                expect(selected[0]).toBe("10001");
+                expect(selected[1]).toBe("10002");
+            });
+        });
+
+        describe("clicking the remove link", function() {
+            beforeEach(function() {
+                this.view.$("li[data-id='10001'] a.remove").click();
+            });
+
+            it("removes the 'added' class from the li in the 'selected' ul", function() {
+                expect(this.view.$("ul.selected li[data-id='10001']")).not.toHaveClass("added");
+            });
+
+            it("removes the 'added' class from the corresponding li in the 'available' ul", function(){
+                expect(this.view.$("ul.available li[data-id='10001']")).not.toHaveClass("added");
+            });
+
+            it("returns the new list of selected IDs", function() {
+                var selected = this.view.getSelectedIDs();
+                expect(selected.length).toBe(0);
+            });
+        });
+    });
+});
+
+
