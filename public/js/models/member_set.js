b5540885c36aef60f4dbf6fb3e86b87be100d4ef
Charles Hansen & Max Brunsfeld <pair+chansen+mbrunsfeld@pivotallabs.com>
2011-12-14 14:48:14 -0800

    Add MemberSet#save

--- public/js/models/member_set.js
+++ public/js/models/member_set.js
@@ -1,6 +1,17 @@
 (function(ns) {
     ns.MemberSet = ns.Collection.extend({
         model : ns.User,
-        urlTemplate : "workspace/{{workspaceId}}/member"
+        urlTemplate : "workspace/{{workspaceId}}/member",
+
+        save: function() {
+            Backbone.sync('update', this, { data: this.toUrlParams() });
+        },
+
+        toUrlParams: function() {
+            return this.reduce(function(memo, model) {
+                var param = "members=" + model.get("userName");
+                return (memo.length === 0) ? param : (memo + "&" + param)
+            }, "");
+        }
     });
 })(chorus.models);
