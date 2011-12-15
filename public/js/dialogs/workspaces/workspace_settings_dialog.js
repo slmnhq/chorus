c5fc25b14cd31d1bbf7e412aa395ebe1d32dcf9a
Charles Hansen & Max Brunsfeld <pair+chansen+mbrunsfeld@pivotallabs.com>
2011-12-14 17:19:56 -0800

    when saving workspace settings, use the 'owner' parameter (not ownerId)

--- public/js/dialogs/workspaces/workspace_settings_dialog.js
+++ public/js/dialogs/workspaces/workspace_settings_dialog.js
@@ -40,12 +40,14 @@
         updateWorkspace : function(e) {
             e.preventDefault();
             var active = !!this.$("input#workspace_active").is(":checked");
+            var ownerId = this.$("select.owner").val();
+            var ownerName = this.model.members().get(ownerId).get("userName");
 
             this.pageModel.save({
                 name: this.$("input[name=name]").val().trim(),
                 summary: this.$("textarea[name=summary]").val().trim(),
                 isPublic : !!this.$("input[name=isPublic]").is(":checked"),
-                ownerId : this.$("select.owner").val(),
+                owner : ownerName,
                 active: active,
                 archived: !active
             });
