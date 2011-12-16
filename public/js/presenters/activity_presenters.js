5867332d50254a4ea2f631175b5b43224357683a
Dennis Collinson & Mark Rushakoff <pair+dennis+mrushakoff@pivotallabs.com>
2011-12-16 15:48:08 -0800

    presenters are hashes without their own context. party!

--- public/js/presenters/activity_presenters.js
+++ public/js/presenters/activity_presenters.js
@@ -2,7 +2,7 @@
 (function(ns) {
     ns.presenters.Activity = function(model){
         var constructor = ns.presenters.Activity[model.get("type")] || ns.presenters.Activity.Base;
-        var presenter = new constructor().make(model)
+        var presenter = constructor.make(model)
         presenter.header = {
             type: model.get("type"),
             authorUrl: model.author().showUrl(),
@@ -16,8 +16,8 @@
         return presenter
     };
 
-    ns.presenters.Activity.Base = function() {
-        this.make = function(model) {
+    ns.presenters.Activity.Base = {
+        make : function(model) {
             var type = model.get("type");
             var author = model.author();
             var workspace = model.get("workspace") && new ns.models.Workspace(model.get("workspace"));
@@ -47,7 +47,7 @@
             return presenter
         },
 
-        this.subComments = function(model){
+        subComments : function(model){
             var comments = model.get("comments");
             return _.map(comments, function(comment) {
                 comment = new chorus.models.Comment(comment);
@@ -67,14 +67,14 @@
 
 
 
-    ns.presenters.Activity.NOTE = function() {
-        this.make = function(model) {
+    ns.presenters.Activity.NOTE = {
+        make : function(model) {
             return extendBase(model, { objectUrl : 'foo' })
         }
     };
 
-    ns.presenters.Activity.WORKSPACE_DELETED = function(){
-        this.make = function(model){
+    ns.presenters.Activity.WORKSPACE_DELETED = {
+        make : function(model){
             var original = extendBase(model);
             return extendBase(model, {
                 objectName : original.workspaceName
@@ -82,8 +82,8 @@
         }
     };
 
-    ns.presenters.Activity.WORKSPACE_CREATED = function(){
-        this.make = function(model){
+    ns.presenters.Activity.WORKSPACE_CREATED = {
+        make : function(model){
             var original = extendBase(model);
             return extendBase(model, {
                 objectName : original.workspaceName,
@@ -92,8 +92,8 @@
         }
     };
 
-    ns.presenters.Activity.WORKFILE_CREATED = function(){
-        this.make = function(model) {
+    ns.presenters.Activity.WORKFILE_CREATED = {
+        make : function(model) {
             return extendBase(model, {
                 objectName : model.get("workfile").name,
                 objectUrl : new ns.models.Workfile({id: model.get("workfile").id, workspaceId : model.get("workspace").id}).showUrl()
@@ -101,8 +101,8 @@
         }
     };
 
-    ns.presenters.Activity.USER_ADDED = function(){
-        this.make = function(model) {
+    ns.presenters.Activity.USER_ADDED = {
+        make : function(model) {
             var user = new ns.models.User({id: model.get("user").id});
             return extendBase(model, {
                 objectName : model.get("user").name,
@@ -114,6 +114,6 @@
     };
 
     function extendBase(model, overrides) {
-        return _.extend(new ns.presenters.Activity.Base().make(model) , overrides)
+        return _.extend(ns.presenters.Activity.Base.make(model) , overrides)
     }
 })(chorus);
