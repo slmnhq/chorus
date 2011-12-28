dc2a88a670289f2c6ef7efffa07e558cc0700399
Mark Rushakoff & Max Brunsfeld <pair+mrushakoff+mbrunsfeld@pivotallabs.com>
2011-12-28 11:23:47 -0800

    Add Activity#attachments

--- public/js/models/activity.js
+++ public/js/models/activity.js
@@ -8,6 +8,16 @@
         comments: function(){
             this._comments || (this._comments = new chorus.models.CommentSet(this.get("comments")));
             return this._comments;
+        },
+
+        attachments: function() {
+            if (!this._attachments) {
+                this._attachments = _.map(this.get("artifacts"), function(artifactJson) {
+                    var klass = (artifactJson.entityType == "workfile") ? ns.Workfile : ns.Artifact;
+                    return new klass(artifactJson);
+                });
+            }
+            return this._attachments;
         }
     });
 })(chorus.models);
