6e27f067beb8544ea74d3a92464d83f9d0a56905
Corina Lawu & Max Brunsfeld <pair+corina+mbrunsfeld@pivotallabs.com>
2012-01-03 19:28:38 -0800

    Add workfile showUrlTemplate

--- public/js/collections/workfile_set.js
+++ public/js/collections/workfile_set.js
@@ -1,6 +1,7 @@
 (function(ns) {
     ns.WorkfileSet = ns.Collection.extend({
         model : ns.Workfile,
-        urlTemplate : "workspace/{{workspaceId}}/workfile{{#if fileType}}?fileType={{fileType}}{{/if}}"
+        urlTemplate : "workspace/{{workspaceId}}/workfile{{#if fileType}}?fileType={{fileType}}{{/if}}",
+        showUrlTemplate : "workspaces/{{workspaceId}}/workfiles"
     });
 })(chorus.models);
