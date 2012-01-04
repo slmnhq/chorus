00a0e030d30b955e8e96bf45f0cfa9fd520c1205
Charles Hansen & Max Brunsfeld <pair+chansen+mbrunsfeld@pivotallabs.com>
2012-01-04 13:41:12 -0800

    user jquery.url in instance account spec

--- spec/javascripts/models/instance_account_spec.js
+++ spec/javascripts/models/instance_account_spec.js
@@ -19,10 +19,10 @@ describe("chorus.models.InstanceAccount", function() {
 
         context("when fetching", function() {
             it("has the right url for fetching an account by user name and instance id", function() {
-                var url = this.model.url({ method: 'read' });
-                expect(url).toContain("/edc/instance/accountmap");
-                expect(url).toContain("instanceId=1045");
-                expect(url).toContain("userName=iceCream");
+                var url = $.url(this.model.url({ method: 'read' }));
+
+                expect(url.attr("path")).toBe("/edc/instance/accountmap");
+                expect(url.param()).toEqual({ instanceId: "1045", userName: "iceCream" });
             });
         });
     });
