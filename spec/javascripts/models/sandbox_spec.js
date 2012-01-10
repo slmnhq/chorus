e24de8c4717518527690423ce98fcd226f7ad670
Erich Douglass & Max Brunsfeld <pair+edouglass+mbrunsfeld@pivotallabs.com>
2012-01-10 10:33:59 -0800

    add validations on Sandbox

--- spec/javascripts/models/sandbox_spec.js
+++ spec/javascripts/models/sandbox_spec.js
@@ -18,16 +18,91 @@ describe("chorus.models.Sandbox", function() {
             expect(this.model.get("type")).toBe("000");
 
             this.model.clear();
-            this.model.save({ instance: '22', database: '11' });
+            this.model.save({ instance: '22', database: '11', schemaName: "baz" });
             expect(this.model.get("type")).toBe("001");
 
             this.model.clear();
-            this.model.save({ database: '11', schema: '33' });
-            expect(this.model.get("type")).toBe("100");
-
-            this.model.clear();
-            this.model.save();
-            expect(this.model.get("type")).toBe("111");
+            this.model.save({ instance: '22', databaseName: "foobar", schemaName: "meow" });
+            expect(this.model.get("type")).toBe("011");
         });
     });
+
+    describe("validations", function() {
+        beforeEach(function() {
+            this.model.set({
+                instance: '1',
+                database: '2',
+                schema: '3'
+            });
+            expectValid({}, this.model);
+        });
+
+        it("requires an instance id", function() {
+            expectInvalid({ instance: "" }, this.model);
+        });
+
+        context("with a database id", function() {
+            context("without a schema id", function() {
+                beforeEach(function() {
+                    this.model.set({ schema: "" });
+                });
+
+                it("requires a schema name", function() {
+                    expectInvalid({ schemaName: "" }, this.model);
+                });
+
+                it("requires that the schema name not start with a number", function() {
+                    expectInvalid({ schemaName: "3friends_of_the_forest" }, this.model);
+                    expectValid({ schemaName: "friends_of_the_forest" }, this.model);
+                });
+
+                it("requires that the schema name does NOT contain whitespace", function() {
+                    expectInvalid({ schemaName: "friends of the forest" }, this.model);
+                });
+            });
+        });
+
+        context("without a database id", function() {
+            beforeEach(function() {
+                this.model.set({ database: "", databaseName: "bernards_db", schemaName: "cool_schema" });
+                expectValid({}, this.model);
+            });
+
+            it("requires a database name", function() {
+                expectInvalid({ databaseName: "" }, this.model);
+            });
+
+            it("requires that the database name not start with a number", function() {
+                expectInvalid({ databaseName: "3friends_of_the_forest" }, this.model);
+            });
+
+            it("requires that the database name does NOT contain whitespace", function() {
+                expectInvalid({ databaseName: "friends of the forest" }, this.model);
+            });
+
+            it("requires a schema name", function() {
+                expectInvalid({ schemaName: "" }, this.model);
+            });
+
+            context("when a schema name is specified", function() {
+                it("requires that the name not start with a number", function() {
+                    expectInvalid({ schemaName: "3friends_of_the_forest" }, this.model);
+                });
+
+                it("requires that the name does NOT contain whitespace", function() {
+                    expectInvalid({ schemaName: "friends of the forest" }, this.model);
+                });
+            });
+        });
+    });
+
+    function expectValid(attrs, model) {
+        model.performValidation(attrs);
+        expect(model.isValid()).toBeTruthy();
+    }
+
+    function expectInvalid(attrs, model) {
+        model.performValidation(attrs);
+        expect(model.isValid()).toBeFalsy();
+    }
 });
