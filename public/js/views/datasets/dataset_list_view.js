b1e09c9f6203d4cb1565db27d7784c1c20f6a87f
Corina Lawu & Mark Rushakoff <pair+corina+mrushakoff@pivotallabs.com>
2012-01-24 15:49:23 -0800

    dataset:selected event on dataset list view

--- public/js/views/datasets/dataset_list_view.js
+++ public/js/views/datasets/dataset_list_view.js
@@ -3,6 +3,9 @@
         tagName : "ul",
         className : "dataset_list",
         additionalClass : "list",
+        events : {
+            "click li" : "selectDataset"
+        },
 
         iconFor: function(model) {
             var type = model.get("type");
@@ -18,10 +21,28 @@
             }
         },
 
+        postRender : function() {
+            var lis = this.$("li");
+
+            _.each(this.collection.models, function(model, index) {
+                lis.eq(index).data("dataset", model);
+            });
+
+            lis.eq(0).click();
+        },
+
         collectionModelContext : function(model) {
             return {
                 iconImgUrl : this.iconFor(model)
             }
+        },
+
+        selectDataset : function(e) {
+            e.preventDefault();
+            this.$("li").removeClass("selected");
+            var selectedDataset = $(e.target).closest("li");
+            selectedDataset.addClass("selected");
+            this.trigger("dataset:selected", selectedDataset.data("dataset"));
         }
     });
 })(jQuery, chorus);
--- spec/javascripts/views/datasets/dataset_list_view_spec.js
+++ spec/javascripts/views/datasets/dataset_list_view_spec.js
@@ -32,6 +32,27 @@ describe("chorus.views.DatasetList", function() {
                 expect(this.view.$("li .location").eq(i).find("a").eq(2).text()).toBe(model.get("schemaName"));
             }
         })
+
+        it("pre-selects the first item", function() {
+            expect(this.view.$("li").eq(0)).toHaveClass("selected");
+        });
+
+        describe("clicking an li", function() {
+            beforeEach(function() {
+                this.selectedSpy = jasmine.createSpy("li selected");
+                this.view.bind("dataset:selected", this.selectedSpy);
+                this.view.$("li").eq(1).click();
+            });
+
+            it("selects only that item", function() {
+                expect(this.view.$("li.selected").length).toBe(1);
+                expect(this.view.$("li").eq(1)).toHaveClass("selected");
+            });
+
+            it("triggers dataset:selected with an argument of the selected dataset", function() {
+                expect(this.selectedSpy).toHaveBeenCalledWith(this.collection.models[1]);
+            });
+        });
     });
 
     describe("iconFor", function() {
