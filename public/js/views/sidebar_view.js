83107553a75d50f74897f4f7088afe98476a7081
Danny Burkes & Lenny Turetsky <pair+danny+lenny@pivotallabs.com>
2012-01-30 16:53:17 -0800

    Goodbye to lionbars, hello to nanoscroller

--- public/js/views/sidebar_view.js
+++ public/js/views/sidebar_view.js
@@ -41,53 +41,34 @@ chorus.views.Sidebar = chorus.views.Base.extend({
         }, this));
 
         $("#sidebar_wrapper").find(".jump_to_top").bind("click", function (e) {
+            $(this).siblings("#sidebar_nano").nanoScroller({scroll:'top'});
             $("#sidebar").find(".lb-wrap").scrollTop(0);
             $(this).removeClass("clickable");
         });
     },
 
     setupSidebarScrolling:function () {
-        var sidebar = $(this.el).closest("#sidebar");
-        if (sidebar && sidebar.parent().get(0)) {
-
-            if (this.oldSidebarPadding) {
-                sidebar.find(".lb-wrap").css("padding-right", this.oldSidebarPadding);
-            }
-
-            sidebar.lionbars();
-
-            sidebar.css("position", "absolute")
-
-            sidebar.unbind('hover').hover(function () {
-                sidebar.find('.lb-v-scrollbar, .lb-h-scrollbar').fadeIn(150)
-            }, function () {
-                sidebar.find('.lb-v-scrollbar, .lb-h-scrollbar').fadeOut(150)
-            });
-
-            sidebar.find('.lb-v-scrollbar, .lb-h-scrollbar').fadeOut(0)
-
-            sidebar.find('.lb-wrap').unbind('mousewheel').bind('mousewheel', this.onMouseWheel)
-            sidebar.siblings('.jump_to_top').unbind('mousewheel').bind('mousewheel', this.onJumpMouseWheel)
-
-            this.oldSidebarPadding = sidebar.find('.lb-wrap').css("padding-right");
-
-            if (!$.browser.msie) {
-                sidebar.find('.lb-wrap').css("padding-right", "32px");
-            }
+        var sidebar = $(this.el).closest("#sidebar_nano");
+        if (sidebar) {
+            sidebar.nanoScroller();
+            sidebar.find('.nano_content').unbind('mousewheel').bind('mousewheel', this.onMouseWheel)
+            sidebar.find('.jump_to_top').unbind('mousewheel').bind('mousewheel', this.onJumpMouseWheel)
         }
     },
 
     onMouseWheel:function (event, d) {
+        $("#sidebar_wrapper").find(".jump_to_top").toggleClass("clickable", this.scrollTop > 10);
+
         if ((this.scrollTop === 0 && d > 0)) {
             event.preventDefault();
         } else if ((this.clientHeight + this.scrollTop >= this.scrollHeight) && d < 0) {
             event.preventDefault();
         }
 
-        $("#sidebar_wrapper").find(".jump_to_top").toggleClass("clickable", this.scrollTop > 10);
+        return true;
     },
 
     onJumpMouseWheel:function (event) {
         event.preventDefault();
     }
-});
+}); 
