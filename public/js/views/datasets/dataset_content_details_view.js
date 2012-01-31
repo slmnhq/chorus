493bb0548c22499fdd97d4e27df30e499fd4fe63
Bleicke Petersen & Charles Hansen <pair+bpetersen+chansen@pivotallabs.com>
2012-01-31 10:46:02 -0800

    Added transform button to Dataset Content Details View

--- public/js/views/datasets/dataset_content_details_view.js
+++ public/js/views/datasets/dataset_content_details_view.js
@@ -1,3 +1,19 @@
 chorus.views.DatasetContentDetails = chorus.views.Base.extend({
-    className:"dataset_content_details"
+    className:"dataset_content_details",
+
+    postRender:function () {
+        var self = this;
+        chorus.menu(this.$('.transform'), {
+            content:this.$(".transform_options").html(),
+            orientation:"left",
+            contentEvents:{
+                '.visualize':_.bind(this.startVisualizationWizard, this)
+            }
+        });
+    },
+
+    startVisualizationWizard : function() {
+        this.trigger("transform:visualize")
+    }
+
 });
--- spec/javascripts/views/datasets/dataset_content_details_view_spec.js
+++ spec/javascripts/views/datasets/dataset_content_details_view_spec.js
@@ -1,21 +1,56 @@
 describe("chorus.views.DatasetContentDetails", function() {
     describe("#render", function() {
         beforeEach(function() {
+            this.qtipMenu = stubQtip();
             this.collection = fixtures.databaseColumnSet();
             this.view = new chorus.views.DatasetContentDetails({collection: this.collection});
             this.view.render();
         });
 
-        it("renders the definition bar", function() {
-            expect(this.view.$(".definition")).toExist();
+        describe("definition bar", function() {
+            it("renders", function() {
+                expect(this.view.$(".definition")).toExist();
+            })
+
+            it("renders the 'Transform' button", function() {
+                expect(this.view.$("button.transform")).toExist();
+                expect(this.view.$("button.transform").text()).toMatchTranslation("dataset.content_details.transform");
+            })
+
+            describe("the 'Transform' button is clicked", function() {
+                beforeEach(function() {
+                    this.view.$(".transform").click();
+                })
+
+                it("should show the qtip menu", function() {
+                    expect(this.qtipMenu).toHaveVisibleQtip()
+                })
+
+                it("should render the qtip content", function() {
+                    expect(this.qtipMenu).toContainTranslation("dataset.content_details.visualize")
+                })
+
+                context("and the visualize dataset link is clicked", function() {
+                    beforeEach(function() {
+                        spyOnEvent(this.view, "transform:visualize");
+                        this.qtipMenu.find('.visualize').click();
+                    })
+
+                    it("triggers the transform:visualize event", function() {
+                        expect("transform:visualize").toHaveBeenTriggeredOn(this.view);
+                    })
+                })
+            })
         })
 
-        it("renders the column count bar", function() {
-            expect(this.view.$(".column_count")).toExist();
-        })
+        describe("column count bar", function() {
+            it("renders", function() {
+                expect(this.view.$(".column_count")).toExist();
+            })
 
-        it("renders the column count in the column count bar", function() {
-            expect(this.view.$(".column_count .count").text().trim()).toMatchTranslation("dataset.column_count", { count : this.collection.models.length })
+            it("renders the column count", function() {
+                expect(this.view.$(".column_count .count").text().trim()).toMatchTranslation("dataset.column_count", { count : this.collection.models.length })
+            })
         })
     })
 });
