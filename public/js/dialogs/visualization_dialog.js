be51dc06e808a77cb67fd05007d6c4e88ccbd0c3
Bleicke Petersen & Erich Douglass <pair+bpetersen+edouglass@pivotallabs.com>
2012-02-01 11:44:30 -0800

    Visualization dialog show and hide button

--- public/js/dialogs/visualization_dialog.js
+++ public/js/dialogs/visualization_dialog.js
@@ -1,9 +1,19 @@
 chorus.dialogs.Visualization = chorus.dialogs.Base.extend({
     className : "visualization",
 
+    subviews: {
+        ".tabledata": "chartData"
+    },
+
+    events: {
+        "click a.show" : "showTabularData",
+        "click a.hide" : "hideTabularData",
+    },
+
     setup : function() {
         this.title = t("visualization.title", {name: this.options.launchElement.data("name")});
 
+        this.chartData = new chorus.views.ResultsConsole();
         $(document).one('reveal.facebox', _.bind(function(){ chorus.styleSelect(this.$(".headerbar select")); }, this));
     },
 
@@ -11,5 +21,19 @@ chorus.dialogs.Visualization = chorus.dialogs.Base.extend({
         return {
             filterCount: 2
         }
+    },
+
+    showTabularData: function(e) {
+        e && e.preventDefault();
+        this.$('.results_console').removeClass("hidden");
+        this.$(".controls a.hide").removeClass("hidden");
+        this.$(".controls a.show").addClass("hidden");
+    },
+
+    hideTabularData: function(e) {
+        e && e.preventDefault();
+        this.$('.results_console').addClass("hidden")
+        this.$(".controls a.show").removeClass("hidden");
+        this.$(".controls a.hide").addClass("hidden");
     }
 });
--- spec/javascripts/dialogs/visualization_dialog_spec.js
+++ spec/javascripts/dialogs/visualization_dialog_spec.js
@@ -5,9 +5,84 @@ describe("chorus.dialogs.Visualization", function() {
     });
 
     describe("#render", function() {
-
+        beforeEach(function() {
+            this.dialog.render();
+        });
+        
         it("should have a title", function() {
             expect(this.dialog.title).toMatchTranslation("visualization.title", {name: "Foo"});
         });
+
+        it("should have a 'Show Data Table' link", function() {
+            expect(this.dialog.$(".controls a.show").text().trim()).toMatchTranslation("visualization.show_table")
+        });
+
+        it("should have a 'Hide Data Table' link", function() {
+            expect(this.dialog.$(".controls a.hide").text().trim()).toMatchTranslation("visualization.hide_table");
+        });
+
+        it("should have a 'Save As' button", function() {
+            expect(this.dialog.$("button.save").text().trim()).toMatchTranslation("actions.save_as");
+        });
+
+        it("should have a 'Close' button", function() {
+            expect(this.dialog.$("button.close_dialog").text().trim()).toMatchTranslation("actions.close");
+        });
+
+        describe("the results console", function() {
+            it("should be hidden", function() {
+                expect(this.dialog.$(".results_console")).toHaveClass("hidden");
+            });
+
+            it("should show a 'Show Data Table' link", function() {
+                expect(this.dialog.$(".controls a.show")).not.toHaveClass("hidden");
+            });
+
+            it("should hide the 'Hide Data Table' link", function() {
+                expect(this.dialog.$(".controls a.hide")).toHaveClass("hidden");
+            });
+        });
     })
+
+    describe("show and hide tabular data", function() {
+        beforeEach(function() {
+            this.dialog.render();
+        });
+        
+        describe("clicking on the 'Show Data Table' link", function() {
+            beforeEach(function() {
+                this.dialog.$(".controls a.show").click();
+            });
+
+            it("should show the data table", function() {
+                expect(this.dialog.$(".results_console")).not.toHaveClass("hidden");
+            })
+
+            it("should hide the show table link", function() {
+                expect(this.dialog.$(".controls a.show")).toHaveClass("hidden");
+            })
+
+            it("should show the hide table link", function() {
+                expect(this.dialog.$(".controls a.hide")).not.toHaveClass("hidden");
+            })
+
+            describe("clicking on the 'Hide Chart Data' link", function() {
+                beforeEach(function() {
+                    this.dialog.$(".controls a.hide").click();
+                });
+
+                it("should hide the data table", function() {
+                    expect(this.dialog.$(".results_console")).toHaveClass("hidden");
+                });
+
+                it("should show the 'Show Data Table' link", function() {
+                    expect(this.dialog.$(".controls a.show")).not.toHaveClass("hidden");
+                });
+
+                it("should hide the 'Hide Data Table' link", function() {
+                    expect(this.dialog.$(".controls a.hide")).toHaveClass("hidden");
+                });
+            });
+        });
+    });
 });
