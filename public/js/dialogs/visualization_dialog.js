1616c76476c1fb7623ea653506573f3fd42bc2d7
Erich Douglass & Mark Rushakoff <pair+edouglass+mrushakoff@pivotallabs.com>
2012-01-31 17:59:03 -0800

    Visualization dialog WIP

--- public/js/dialogs/visualization_dialog.js
+++ public/js/dialogs/visualization_dialog.js
@@ -3,5 +3,7 @@ chorus.dialogs.Visualization = chorus.dialogs.Base.extend({
 
     setup : function() {
         this.title = t("visualization.title", {name: this.options.launchElement.data("name")});
+
+        $(document).one('reveal.facebox', _.bind(function(){ chorus.styleSelect(this.$(".headerbar select")); }, this));
     }
 });
