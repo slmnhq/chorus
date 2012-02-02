b70d72d5487cc441ad658cd6a327d4baa40a8573
Corina Lawu & Erich Douglass <pair+corina+edouglass@pivotallabs.com>
2012-02-02 14:50:43 -0800

    Change translation keys to match server response.

--- public/js/models/tasks/boxplot_task.js
+++ public/js/models/tasks/boxplot_task.js
@@ -2,12 +2,12 @@ chorus.models.BoxplotTask = chorus.models.ChartTask.extend({
     chartType: "boxplot",
     columnLabels : {
         "bucket" : "dataset.visualization.boxplot.bucket",
-        "minimum" : "dataset.visualization.boxplot.minimum",
+        "min" : "dataset.visualization.boxplot.minimum",
         "median" : "dataset.visualization.boxplot.median",
-        "maximum" : "dataset.visualization.boxplot.maximum",
+        "max" : "dataset.visualization.boxplot.maximum",
         "percentage" : "dataset.visualization.boxplot.percentage",
-        "1stquartile" : "dataset.visualization.boxplot.1stquartile",
-        "3rdquartile" : "dataset.visualization.boxplot.3rdquartile"
+        "firstQuartile" : "dataset.visualization.boxplot.1stquartile",
+        "thirdQuartile" : "dataset.visualization.boxplot.3rdquartile"
     },
 
     beforeSave: function() {
