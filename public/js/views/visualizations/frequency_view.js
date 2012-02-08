1a48ed095b19422979ce224f6c674b55fa911988
Charles Hansen & Jesse Toth <pair+chansen+jesse@pivotallabs.com>
2012-02-08 15:36:19 -0800

    convert freqency view to use axes.js from chart.js

--- public/js/views/visualizations/frequency_view.js
+++ public/js/views/visualizations/frequency_view.js
@@ -2,28 +2,48 @@ chorus.views.visualizations.Frequency = chorus.views.Base.extend({
     render: function() {
         var $el = $(this.el);
         $el.addClass("visualization");
+
         var data = new chorus.presenters.visualizations.Frequency(this.model).present();
-        chorus.chart.frequency(
-            this.chartArea(this.el),
-            data,
-            { yAxisTitle: this.model.get("chart[yAxis]")}
-        );
+        var buckets = _.pluck(data, 'bucket');
+
+        var counts = _.pluck(data, 'count')
+
+        var svg = d3.select(this.el).append("svg").
+            attr("class", "chart frequency").
+            attr("width", 925).
+            attr("height", 340);
+
+        this.axes = new chorus.views.visualizations.Axes({
+            el: svg,
+            minXValue: 0,
+            maxXValue: _.max(counts),
+            xScaleType: "numeric",
+            yLabels: buckets,
+            xAxisLabel: "count",
+            yAxisLabel: this.model.get("yAxis"),
+            ticks: true
+        });
+
+        if (!$el.isOnDom()) return;
+
+        this.axes.render();
+        var scales = this.axes.scales();
+
+        var plot = svg.append("svg:g").attr("class", "plot");
+
+        plot.selectAll("rect")
+            .data(data)
+            .enter().append("svg:rect")
+            .attr("x", scales.x.range()[0])
+            .attr("y", function(row) {
+                return scales.y(row.bucket)+scales.y.rangeBand();
+            })
+            .attr("height", Math.abs(scales.y.rangeBand()))
+            .attr("width", function(row) {
+                return scales.x(row.count) - scales.x(0);
+            });
+
         this.postRender();
-    },
-
-    chartArea : function(el) {
-        this.plotWidth = 925;
-        this.plotHeight = 340;
-
-        return d3.select(el)
-            .append("svg:svg")
-            .attr("class", "chart")
-            .attr("width", this.plotWidth)
-            .attr("height", this.plotHeight)
-            .append("svg:g")
-            .attr("class", "plot")
-            .attr("width", this.plotWidth)
-            .attr("height", this.plotHeight);
     }
 });
 
