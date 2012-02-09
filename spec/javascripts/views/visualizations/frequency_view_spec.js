edb018a62bdf8b3162c457f7d4eb936a4d2a723f
Charles Hansen & Chuck Fitzpatrick <pair+chansen+cfitzpatrick@pivotallabs.com>
2012-02-09 10:38:11 -0800

    Specs for frequency view

--- spec/javascripts/views/visualizations/frequency_view_spec.js
+++ spec/javascripts/views/visualizations/frequency_view_spec.js
@@ -11,9 +11,9 @@ describe("chorus.views.visualizations.FrequencyView", function() {
     beforeEach(function() {
         this.task = fixtures.frequencyTaskWithResult({
             rows: [
-                { bucket: 'aardvark',   count: 4000},
-                { bucket: 'beluga',     count: 500},
-                { bucket: 'chupacabra', count: 10000}
+                { bucket: 'aardvark',   count: 10000},
+                { bucket: 'beluga',     count: 4000},
+                { bucket: 'chupacabra', count: 500}
             ],
             "chart[yAxis]": "animals"
         });
@@ -34,15 +34,53 @@ describe("chorus.views.visualizations.FrequencyView", function() {
             expect(this.view.$('.yaxis .axis_label').text()).toBe("animals")
         });
 
-        describe("the count rectangles", function() {
-           it("has one for each bucket", function() {
-               expect(this.boxes.length).toBe(3);
-           });
+        describe("the frequency rectangles", function() {
+            it("has one for each bucket", function() {
+                expect(this.boxes.length).toBe(3);
+            });
 
             it("draws them in order from top to bottom", function() {
-               expect(this.boxes).toBeOrderedTopToBottom();
+                expect(this.boxes).toBeOrderedTopToBottom();
+            });
+
+            it("have the corrects widths", function() {
+                expect(width(this.boxes[2])).toBeGreaterThan(width(this.boxes[1]));
+                expect(width(this.boxes[1])).toBeGreaterThan(width(this.boxes[0]));
+            })
+
+            it("have the same heights", function() {
+                expect(height(this.boxes[2])).toEqual(height(this.boxes[1]));
+                expect(height(this.boxes[1])).toEqual(height(this.boxes[0]));
+            })
+
+            it("draws them with some padding in between", function() {
+                _.each(this.boxes, function(rect, i) {
+                    if (i === 0) return;
+                    expect(bottomY(rect)).toBeLessThan(topY(this.boxes[i - 1]) + 5);
+                }, this);
+            });
+
+            it("centers the rectangles on the y axis ticks", function() {
+                var yTicks = this.view.$(".yaxis .tick");
+                _.each(this.boxes, function(rect, i) {
+                    expect(centerY(rect)).toBeCloseTo(centerY(yTicks[i]), 1);
+                }, this);
             });
         });
+
+        it("draws vertical grid lines", function() {
+            expect(this.view.$(".xaxis line.grid").length).toBeGreaterThan(1)
+        })
+        
+        it("does not draw horizontal grid lines", function() {
+            expect(this.view.$(".yaxis line.grid").length).toBe(0)
+        })
+
+        it("draws the grid lines after the rectangles", function() {
+            var gridRect = this.view.$(".plot rect, line.grid");
+            expect($(gridRect[0]).attr("class")).not.toBe("grid")
+            expect($(gridRect[gridRect.length-1]).attr("class")).toBe("grid")
+        })
     });
 
 });
