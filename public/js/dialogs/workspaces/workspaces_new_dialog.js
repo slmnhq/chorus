dd585f21cece3862f4382eea0e1fcfed0e119237
Bleicke Petersen & Mark Rushakoff <pair+bpetersen+mrushakoff@pivotallabs.com>
2012-02-14 15:38:26 -0800

    Enable create workspace button only when a name is given
    [finishes #24904601]

--- public/js/dialogs/workspaces/workspaces_new_dialog.js
+++ public/js/dialogs/workspaces/workspaces_new_dialog.js
@@ -5,6 +5,8 @@ chorus.dialogs.WorkspacesNew = chorus.dialogs.Base.extend({
     persistent:true,
 
     events:{
+        "keyup input[name=name]": "checkInput",
+        "paste input[name=name]": "checkInput",
         "submit form.new_workspace":"createWorkspace"
     },
 
@@ -24,11 +26,17 @@ chorus.dialogs.WorkspacesNew = chorus.dialogs.Base.extend({
             isPublic:!!this.$("input[name=isPublic]").is(":checked")
         })
 
+        this.$("button.submit").startLoading("actions.creating")
         this.resource.save();
     },
 
     workspaceSaved:function () {
         $(document).trigger("close.facebox");
         chorus.router.navigate("/workspaces/" + this.model.get("id"), true);
+    },
+
+    checkInput : function() {
+        var hasText = this.$("input[name=name]").val().trim().length > 0;
+        this.$("button.submit").prop("disabled", hasText ? false : "disabled");
     }
 });
