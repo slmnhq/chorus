fce424d046aca6fe6275c553e3e664bc4179baa8
Danny Burkes <pair+danny@pivotallabs.com>
2012-02-15 17:08:14 -0800

    Trying to fixup intermittently failing ci specs

--- spec/javascripts/views/datasets/dataset_edit_chorus_view_spec.js
+++ spec/javascripts/views/datasets/dataset_edit_chorus_view_spec.js
@@ -11,106 +11,98 @@ describe("chorus.views.DatasetEditChorusView", function() {
             spyOn(window.TextRange.prototype, 'select');
         }
         spyOn(CodeMirror, "fromTextArea").andCallThrough();
+        stubDefer()
     })
 
-    context ("#setup", function(){
-        it ("saves the initial query value", function(){
+    context("#setup", function() {
+        it("saves the initial query value", function() {
             expect(this.view.model.initialQuery).toBe(this.view.model.get("query"));
         })
     })
 
-    context("without defer stubbed out", function() {
+    describe("#render", function() {
+        beforeEach(function() {
+            spyOn(this.view, "updateQueryInModel").andCallThrough();
+            this.view.render();
+        });
+
         it("defers call to CodeMirror", function() {
             this.view.render();
-            expect(CodeMirror.fromTextArea).not.toHaveBeenCalled();
-            this.clock.tick(1000);
+            expect(_.defer).toHaveBeenCalled();
             expect(CodeMirror.fromTextArea).toHaveBeenCalled();
         });
+
+        context("when deferred CodeMirror creation happens twice in one dom render", function() {
+            beforeEach(function() {
+                var deferredCodeMirror = _.defer.calls[0].args[0];
+                deferredCodeMirror();
+            });
+
+            it("only calls CodeMirror once", function() {
+                expect(CodeMirror.fromTextArea.callCount).toBe(1);
+            });
+        });
+
+        it("displays line numbers", function() {
+            expect(this.view.editor.getOption("lineNumbers")).toBe(true);
+        });
+
+        it("displays the query", function() {
+            expect(this.view.editor.getValue()).toBe(this.dataset.get("query"));
+        });
+
+        it("uses the 'text/x-sql' mode", function() {
+            expect(this.view.editor.getOption("mode")).toBe("text/x-sql");
+        });
+
+        it("set the query in the model when blur is received by the editor", function() {
+            this.view.editor.focus();
+            this.view.editor.setValue("select * from hello;")
+            $(this.view.$(".CodeMirror")[0].firstChild.firstChild).blur();
+            expect(this.view.updateQueryInModel).toHaveBeenCalled();
+            expect(this.view.model.get("query")).toBe("select * from hello;")
+        })
     });
 
-    context("with defer stubbed out", function() {
+
+    describe("#saveChanges", function() {
         beforeEach(function() {
-            stubDefer()
+            this.view.render();
+            spyOn(chorus.router, "navigate");
+            this.view.editor.setValue("select * from table_abc");
+            spyOn(this.view.model, "save");
+            this.view.trigger("dataset:saveEdit");
+            this.clock.tick(1000);
+
+        });
+        context("when save succeed", function() {
+            beforeEach(function() {
+                this.view.model.trigger("saved");
+            });
+            it("saves the model", function() {
+                expect(this.view.model.save).toHaveBeenCalled();
+            });
+
+            it("sets the query in the model", function() {
+                expect(this.view.model.get("query")).toBe("select * from table_abc");
+            });
+
+            it("should return the user to the standard page view", function() {
+                expect(chorus.router.navigate).toHaveBeenCalledWith(this.view.model.showUrl(), true)
+            });
         });
 
-        describe("#render", function() {
+        context("when save fails", function() {
             beforeEach(function() {
-                spyOn(this.view,"updateQueryInModel").andCallThrough();
+                this.view.model.set({serverErrors: [
+                    { message: "SQL error!" }
+                ]})
+                this.view.model.trigger("saveFailed");
                 this.view.render();
             });
-
-            context("when deferred CodeMirror creation happens twice in one dom render", function() {
-                beforeEach(function() {
-                    var deferredCodeMirror = _.defer.calls[0].args[0];
-                    deferredCodeMirror();
-                });
-
-                it("only calls CodeMirror once", function() {
-                    expect(CodeMirror.fromTextArea.callCount).toBe(1);
-                });
-            });
-
-            it("displays line numbers", function() {
-                expect(this.view.editor.getOption("lineNumbers")).toBe(true);
-            });
-
-            it("displays the query", function() {
-                expect(this.view.editor.getValue()).toBe(this.dataset.get("query"));
-            });
-
-            it("uses the 'text/x-sql' mode", function() {
-                expect(this.view.editor.getOption("mode")).toBe("text/x-sql");
-            });
-
-            it("set the query in the model when blur is received by the editor", function() {
-                this.view.editor.focus();
-                this.view.editor.setValue("select * from hello;")
-                $(this.view.$(".CodeMirror")[0].firstChild.firstChild).blur();
-                expect(this.view.updateQueryInModel).toHaveBeenCalled();
-                expect(this.view.model.get("query")).toBe("select * from hello;")
+            it("displays the error message", function() {
+                expect(this.view.$(".errors ul li").text()).toBe("SQL error!");
             })
-        });
-
-
-        describe("#saveChanges", function() {
-            beforeEach(function() {
-                this.view.render();
-                spyOn(chorus.router, "navigate");
-                this.view.editor.setValue("select * from table_abc");
-                spyOn(this.view.model, "save");
-                this.view.trigger("dataset:saveEdit");
-                this.clock.tick(1000);
-
-            });
-            context("when save succeed", function() {
-                beforeEach(function() {
-                    this.view.model.trigger("saved");
-                });
-                it("saves the model", function() {
-                    expect(this.view.model.save).toHaveBeenCalled();
-                });
-
-                it("sets the query in the model", function() {
-                    expect(this.view.model.get("query")).toBe("select * from table_abc");
-                });
-
-                it("should return the user to the standard page view", function() {
-                    expect(chorus.router.navigate).toHaveBeenCalledWith(this.view.model.showUrl(), true)
-                });
-            });
-
-            context("when save fails", function() {
-                beforeEach(function() {
-                    this.view.model.set({serverErrors: [
-                        { message: "SQL error!" }
-                    ]})
-                    this.view.model.trigger("saveFailed");
-                    this.view.render();
-                });
-                it("displays the error message", function() {
-                    expect(this.view.$(".errors ul li").text()).toBe("SQL error!");
-                })
-            })
-        });
+        })
     });
 });
