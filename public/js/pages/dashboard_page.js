274b76ab6af7a032221184b9d5d9e8d5ef6ae0fe
Lenny Turetsky & Mark Rushakoff <pair+lenny+mrushakoff@pivotallabs.com>
2012-02-15 17:52:45 -0800

    Show the number of Chorus users on the Dashboard [fixed #24969587]

--- public/js/pages/dashboard_page.js
+++ public/js/pages/dashboard_page.js
@@ -16,10 +16,25 @@ chorus.pages.DashboardPage = chorus.pages.Base.extend({
         this.model = chorus.session.user();
 
         this.mainContent = new chorus.views.Dashboard({ collection: this.workspaceSet, instanceSet: this.instanceSet });
+
+        this.userSet = new chorus.collections.UserSet();
+        this.userSet.bindOnce("loaded", function() {
+            this.userCount = this.userSet.pagination.records;
+            this.showUserCount()
+        }, this);
+        this.userSet.fetch();
+    },
+
+    showUserCount: function() {
+        if (this.userCount) {
+            this.$("#user_count a").text(t("dashboard.user_count", {count: this.userCount}));
+            this.$("#user_count").removeClass("hidden");
+        }
     },
 
     postRender:function () {
         this._super('postRender');
         this.$("#sidebar_wrapper").remove();
+        this.showUserCount();
     }
 });
--- spec/javascripts/pages/dashboard_page_spec.js
+++ spec/javascripts/pages/dashboard_page_spec.js
@@ -1,17 +1,11 @@
 describe("chorus.pages.DashboardPage", function() {
     beforeEach(function() {
-        chorus.session = new chorus.models.Session({ id : "foo" })
+        chorus.session = new chorus.models.Session({ id: "foo" })
         this.page = new chorus.pages.DashboardPage();
     });
 
     it("has a helpId", function() {
         expect(this.page.helpId).toBe("dashboard")
-    })
-
-    describe("#setup", function() {
-        it("sets chorus.session.user as the model", function() {
-            expect(this.page.model).toBe(chorus.session.user())
-        })
     })
 
     describe("#render", function() {
@@ -23,8 +17,8 @@ describe("chorus.pages.DashboardPage", function() {
             expect(this.page.$("#header.header")).toExist();
         })
 
-        context("the workspace list", function(){
-            beforeEach(function(){
+        context("the workspace list", function() {
+            beforeEach(function() {
                 this.workspaceList = this.page.mainContent.workspaceList;
             })
 
@@ -40,10 +34,29 @@ describe("chorus.pages.DashboardPage", function() {
         it("does not have a sidebar", function() {
             expect(this.page.$("#sidebar_wrapper")).not.toExist();
         });
+
+        context("when the users fetch completes", function() {
+            beforeEach(function() {
+                this.server.completeFetchFor(new chorus.collections.UserSet([], {page:1, rows:1}), [fixtures.user()], null, {page:1, total: 1234, records: 1234});
+            });
+
+            it("shows the number of users", function() {
+                expect(this.page.$("#user_count a")).toContainTranslation("dashboard.user_count", {count: 1234});
+                expect(this.page.$("#user_count")).not.toHaveClass("hidden");
+            });
+        });
     });
 
-    context("#setup", function(){
-        it("passes the collection through to the workspaceSet view", function(){
+    context("#setup", function() {
+        it("sets chorus.session.user as the model", function() {
+            expect(this.page.model).toBe(chorus.session.user())
+        });
+
+        it("gets the number of users", function() {
+            expect(this.server.lastFetchFor(new chorus.collections.UserSet([], {page:1, rows:1}))).toBeTruthy();
+        });
+
+        it("passes the collection through to the workspaceSet view", function() {
             expect(this.page.mainContent.workspaceList.collection).toBe(this.page.workspaceSet);
         });
 
@@ -51,7 +64,7 @@ describe("chorus.pages.DashboardPage", function() {
             expect(this.page.workspaceSet.attributes.showLatestComments).toBeTruthy();
         })
 
-        it("should sort the workspaceSet by name Descending", function(){
+        it("should sort the workspaceSet by name Descending", function() {
             expect(this.page.workspaceSet.sortIndex).toBe("name");
             expect(this.page.workspaceSet.sortOrder).toBe("asc");
         })
