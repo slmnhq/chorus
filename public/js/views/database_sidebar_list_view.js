09ead482d643bb05380e366ba42d6a410164baef
Danny Burkes <pair+danny@pivotallabs.com>
2012-02-21 18:05:21 -0800

    Executing a workfile in a new schema updates sidebar database list views

--- public/js/views/database_sidebar_list_view.js
+++ public/js/views/database_sidebar_list_view.js
@@ -1,24 +1,20 @@
 chorus.views.DatabaseSidebarList = chorus.views.Base.extend({
-    events:{
-        "click .context a":"contextClicked"
+    events: {
+        "click .context a": "contextClicked"
     },
 
-    collectionModelContext:function (model) {
+    collectionModelContext: function(model) {
         return {
-            cid:model.cid
+            cid: model.cid
         }
     },
 
-    setup:function () {
-        this.schema = this.options.schema;
-        if (this.schema) {
-            this.schemas = this.schema.database().schemas();
-            this.schemas.fetch();
-            this.fetchResourceAfterSchemaSelected();
-        }
+    setup: function() {
+        this.setSchema(this.options.schema);
+        chorus.PageEvents.subscribe("workfile:executed", this.workfileExecuted, this);
     },
 
-    additionalContext:function () {
+    additionalContext: function() {
         if (!this.schema) {
             return {
                 schemaAssociated: false
@@ -27,73 +23,73 @@ chorus.views.DatabaseSidebarList = chorus.views.Base.extend({
             return {
                 schemaAssociated: true,
                 schemaName: this.schema && this.schema.get("name"),
-                schemaLink:chorus.helpers.linkTo("#", this.schema.get('name')),
-                schemas:this.schemas.map(function (schema) {
+                schemaLink: chorus.helpers.linkTo("#", this.schema.get('name')),
+                schemas: this.schemas.map(function(schema) {
                     return {
-                        id:schema.get("id"),
-                        name:schema.get("name"),
-                        isCurrent:this.schema.get('id') === schema.get('id')
+                        id: schema.get("id"),
+                        name: schema.get("name"),
+                        isCurrent: this.schema.get('id') === schema.get('id')
                     };
                 }, this)
             };
         }
     },
 
-    fetchResourceAfterSchemaSelected:$.noop,
+    fetchResourceAfterSchemaSelected: $.noop,
 
-    postRender:function () {
+    postRender: function() {
         chorus.search({
-            input:this.$('input.search'),
-            list:this.$('ul')
+            input: this.$('input.search'),
+            list: this.$('ul')
         });
 
         chorus.menu(this.$(".context a"), {
-            content:this.$(".schema_menu_container").html(),
-            container:$('#sidebar_wrapper'),
-            contentEvents:{
-                'a.schema':_.bind(this.schemaSelected, this)
+            content: this.$(".schema_menu_container").html(),
+            container: $('#sidebar_wrapper'),
+            contentEvents: {
+                'a.schema': _.bind(this.schemaSelected, this)
             }
         });
 
         this.$("li a").click(this.closeQtip);
         this.$("li").qtip({
-            content:"<a>" + t('database.sidebar.insert') + "</a>",
-            events:{
-                render:_.bind(function (e, api) {
+            content: "<a>" + t('database.sidebar.insert') + "</a>",
+            events: {
+                render: _.bind(function(e, api) {
                     e.preventDefault();
                     e.stopPropagation();
                     $(api.elements.content).find('a').click(_.bind(this.insertText, this, $(api.elements.target).data('cid')));
                 }, this),
-                show:function (e, api) {
+                show: function(e, api) {
                     $(api.elements.target).addClass('hover');
                 },
-                hide:function (e, api) {
+                hide: function(e, api) {
                     $(api.elements.target).removeClass('hover');
                 }
             },
-            show:{
-                delay:0,
-                solo:true,
-                effect:false
+            show: {
+                delay: 0,
+                solo: true,
+                effect: false
             },
-            hide:{
-                delay:0,
-                fixed:true,
-                effect:false
+            hide: {
+                delay: 0,
+                fixed: true,
+                effect: false
             },
-            position:{
-                my:"right center",
-                at:"left center",
-                adjust:{
-                    x:-16
+            position: {
+                my: "right center",
+                at: "left center",
+                adjust: {
+                    x: -16
                 }
             },
-            style:{
-                classes:"tooltip-insert",
-                tip:{
-                    corner:"left center",
-                    width:16,
-                    height:29
+            style: {
+                classes: "tooltip-insert",
+                tip: {
+                    corner: "left center",
+                    width: 16,
+                    height: 29
                 }
             }
         });
@@ -104,24 +100,43 @@ chorus.views.DatabaseSidebarList = chorus.views.Base.extend({
         }, this));
     },
 
-    schemaSelected:function (e) {
+    schemaSelected: function(e) {
         var schemaId = $(e.target).data("id")
         this.schema = this.schemas.get(schemaId)
         this.fetchResourceAfterSchemaSelected();
         this.render();
     },
 
-    insertText:function (cid, e) {
+    insertText: function(cid, e) {
         e && e.preventDefault();
         var model = this.collection.getByCid(cid)
         chorus.PageEvents.broadcast("file:insertText", model.toText());
     },
 
-    closeQtip:function () {
+    closeQtip: function() {
         $("li:hover a").trigger("mouseleave");
     },
 
-    contextClicked:function (e) {
+    contextClicked: function(e) {
         e.preventDefault();
+    },
+
+    workfileExecuted: function(workfile, executionSchema) {
+        if (!this.schema ||
+            (executionSchema.instanceId != this.schema.get("instanceId")) ||
+            (executionSchema.databaseName != this.schema.get("databaseName")) ||
+            (executionSchema.name != this.schema.get("name")))
+        {
+            this.setSchema(new chorus.models.Schema(_.extend(executionSchema, { name: executionSchema.schemaName })))
+        }
+    },
+
+    setSchema: function(schema) {
+        this.schema = schema;
+        if (this.schema) {
+            this.schemas = this.schema.database().schemas();
+            this.schemas.fetch();
+            this.fetchResourceAfterSchemaSelected();
+        }
     }
 });
--- spec/javascripts/views/database_sidebar_list_view_spec.js
+++ spec/javascripts/views/database_sidebar_list_view_spec.js
@@ -15,14 +15,14 @@ describe("chorus.views.DatabaseSidebarList", function() {
             beforeEach(function() {
                 this.view.render();
             })
-            
+
             it("should not crash", function() {
                 expect($(this.view.el)).toHaveClass("database_function_sidebar_list");
             })
         })
     })
 
-    context("render", function() {
+    context("when there is a schema", function() {
         beforeEach(function() {
             var object0 = new chorus.models.DatabaseObject();
             var object1 = new chorus.models.DatabaseObject();
@@ -36,101 +36,149 @@ describe("chorus.views.DatabaseSidebarList", function() {
             spyOn(this.collection.models[1], 'toText').andReturn('object2');
             this.view = new chorus.views.DatabaseSidebarList({collection: this.collection, schema: this.schema });
             this.view.className = "database_dataset_sidebar_list";
-            this.schemaMenuQtip = stubQtip(".context a");
-            this.insertArrowQtip = stubQtip("li");
-            spyOn(this.view, 'closeQtip');
-            this.view.render();
         });
 
-        context("after schemas have loaded", function() {
+        it("fetches the schemas", function() {
+            expect(this.server.lastFetchFor(this.schema.database().schemas())).toBeDefined();
+        })
+
+        context("when the fetch completes", function() {
             beforeEach(function() {
                 this.server.completeFetchFor(this.schema.database().schemas(), [
                     this.schema,
                     fixtures.schema({ name: "awesome_tables", id: "5" }),
                     fixtures.schema({ name: "orphaned_tables", id: "6" })
                 ]);
-                this.view.render();
             });
 
-            describe("selecting a schema", function() {
+            context("render", function() {
                 beforeEach(function() {
-                    spyOn(this.view, 'fetchResourceAfterSchemaSelected');
-                    this.view.$(".context a").click();
-                });
-
-                it("opens a chorus menu", function() {
-                    expect(this.schemaMenuQtip).toHaveVisibleQtip();
-                });
-
-                it("shows a check mark next to the current schema", function() {
-                    expect(this.view.$("li:contains('righteous_tables')")).toContain('.check')
-                    expect(this.view.$("li:contains('awesome_tables')")).not.toContain('.check')
-                })
-
-                it("shows the names of all of the workspace's database's schemas", function() {
-                    expect(this.schemaMenuQtip.find("li").length).toBe(3);
-                    expect(this.schemaMenuQtip).toContainText("righteous_tables");
-                    expect(this.schemaMenuQtip).toContainText("awesome_tables");
-                    expect(this.schemaMenuQtip).toContainText("orphaned_tables");
-                });
-
-                describe("when a schema is clicked", function() {
-                    beforeEach(function() {
-                        this.schemaMenuQtip.find("a[data-id=5]").click()
-                        this.otherSchema = this.view.schemas.get("5");
-                    });
-
-                    it("calls the 'fetchResourceAfterSchemaSelected' hook", function() {
-                        expect(this.view.fetchResourceAfterSchemaSelected).toHaveBeenCalled();
-                    });
-                });
-            });
-        });
-
-        context("when hovering over a collection li", function() {
-            beforeEach(function() {
-                this.collection.trigger("reset");
-                this.view.$('.list li:eq(1)').mouseenter();
-            });
-
-            it("has the insert text in the insert arrow", function() {
-                expect(this.insertArrowQtip.find("a")).toContainTranslation('database.sidebar.insert')
-            })
-
-            context("when clicking the insert arrow", function() {
-                beforeEach(function() {
-                    spyOn(chorus.PageEvents, "broadcast").andCallThrough();
-                    this.insertArrowQtip.find("a").click()
-                })
-
-                it("broadcasts a file:insertText with the string representation", function() {
-                    expect(chorus.PageEvents.broadcast).toHaveBeenCalledWith("file:insertText", this.view.collection.models[1].toText());
-                })
-            })
-
-            context("when clicking a link within the li", function() {
-                beforeEach(function() {
-                    this.view.$('.list li:eq(1) a').click()
-                })
-
-                it("closes the open insert arrow", function() {
-                    expect(this.view.closeQtip).toHaveBeenCalled();
-                })
-            });
-
-            context("when scrolling", function() {
-                beforeEach(function() {
-                    chorus.page = new chorus.pages.Base();
-                    chorus.page.sidebar = new chorus.views.Sidebar();
-
+                    this.schemaMenuQtip = stubQtip(".context a");
+                    this.insertArrowQtip = stubQtip("li");
+                    spyOn(this.view, 'closeQtip');
                     this.view.render();
-                    chorus.page.sidebar.trigger("scroll");
                 });
 
-                it("closes the open insert arrow", function() {
-                    expect(this.view.closeQtip).toHaveBeenCalled();
+                context("after schemas have loaded", function() {
+                    beforeEach(function() {
+                        this.server.completeFetchFor(this.schema.database().schemas(), [
+                            this.schema,
+                            fixtures.schema({ name: "awesome_tables", id: "5" }),
+                            fixtures.schema({ name: "orphaned_tables", id: "6" })
+                        ]);
+                        this.view.render();
+                    });
+
+                    describe("selecting a schema", function() {
+                        beforeEach(function() {
+                            spyOn(this.view, 'fetchResourceAfterSchemaSelected');
+                            this.view.$(".context a").click();
+                        });
+
+                        it("opens a chorus menu", function() {
+                            expect(this.schemaMenuQtip).toHaveVisibleQtip();
+                        });
+
+                        it("shows a check mark next to the current schema", function() {
+                            expect(this.view.$("li:contains('righteous_tables')")).toContain('.check')
+                            expect(this.view.$("li:contains('awesome_tables')")).not.toContain('.check')
+                        })
+
+                        it("shows the names of all of the workspace's database's schemas", function() {
+                            expect(this.schemaMenuQtip.find("li").length).toBe(3);
+                            expect(this.schemaMenuQtip).toContainText("righteous_tables");
+                            expect(this.schemaMenuQtip).toContainText("awesome_tables");
+                            expect(this.schemaMenuQtip).toContainText("orphaned_tables");
+                        });
+
+                        describe("when a schema is clicked", function() {
+                            beforeEach(function() {
+                                this.schemaMenuQtip.find("a[data-id=5]").click()
+                                this.otherSchema = this.view.schemas.get("5");
+                            });
+
+                            it("calls the 'fetchResourceAfterSchemaSelected' hook", function() {
+                                expect(this.view.fetchResourceAfterSchemaSelected).toHaveBeenCalled();
+                            });
+                        });
+                    });
                 });
+
+                context("when hovering over a collection li", function() {
+                    beforeEach(function() {
+                        this.collection.trigger("reset");
+                        this.view.$('.list li:eq(1)').mouseenter();
+                    });
+
+                    it("has the insert text in the insert arrow", function() {
+                        expect(this.insertArrowQtip.find("a")).toContainTranslation('database.sidebar.insert')
+                    })
+
+                    context("when clicking the insert arrow", function() {
+                        beforeEach(function() {
+                            spyOn(chorus.PageEvents, "broadcast").andCallThrough();
+                            this.insertArrowQtip.find("a").click()
+                        })
+
+                        it("broadcasts a file:insertText with the string representation", function() {
+                            expect(chorus.PageEvents.broadcast).toHaveBeenCalledWith("file:insertText", this.view.collection.models[1].toText());
+                        })
+                    })
+
+                    context("when clicking a link within the li", function() {
+                        beforeEach(function() {
+                            this.view.$('.list li:eq(1) a').click()
+                        })
+
+                        it("closes the open insert arrow", function() {
+                            expect(this.view.closeQtip).toHaveBeenCalled();
+                        })
+                    });
+
+                    context("when scrolling", function() {
+                        beforeEach(function() {
+                            chorus.page = new chorus.pages.Base();
+                            chorus.page.sidebar = new chorus.views.Sidebar();
+
+                            this.view.render();
+                            chorus.page.sidebar.trigger("scroll");
+                        });
+
+                        it("closes the open insert arrow", function() {
+                            expect(this.view.closeQtip).toHaveBeenCalled();
+                        });
+                    });
+                })
             });
-        })
-    });
-});
+
+            describe("event handling", function() {
+                describe("workfile:executed", function() {
+                    beforeEach(function() {
+                        this.server.reset();
+                    });
+
+                    context("when the execution schema is the same as the view's schema", function() {
+                        beforeEach(function() {
+                            chorus.PageEvents.broadcast("workfile:executed", fixtures.workfile(), this.schema.attributes)
+                        });
+
+                        it("does not fetch anything", function() {
+                            expect(this.server.fetches().length).toBe(0);
+                        })
+                    })
+
+                    context("when the execution schema is not the same as the view's schema", function() {
+                        beforeEach(function() {
+                            this.executionSchema = fixtures.schema();
+                            chorus.PageEvents.broadcast("workfile:executed", fixtures.workfile(), this.executionSchema)
+                        });
+
+                        it("fetches the execution schema", function() {
+                            expect(this.server.lastFetchFor(this.executionSchema.database().schemas())).toBeDefined();
+                        })
+                    })
+                })
+            })
+        });
+    })
+})
