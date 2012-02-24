c446372f333d129d252b3fa4b19560d14cc006ea
Bleicke Petersen & Max Brunsfeld <pair+bpetersen+mbrunsfeld@pivotallabs.com>
2012-02-24 11:53:58 -0800

    Add SearchResult#workfiles

--- public/js/models/search_result.js
+++ public/js/models/search_result.js
@@ -11,4 +11,12 @@ chorus.models.SearchResult = chorus.models.Base.extend({
         var name = this.get("query") || "";
         return (name.length < length) ? name : name.slice(0, length) + "...";
     },
+
+    workfiles: function() {
+        var workfiles = _.map(this.get("workfile").docs, function(workfileJson) {
+            workfileJson.fileName = $.stripHtml(workfileJson.name);
+            return new chorus.models.Workfile(workfileJson);
+        });
+        return new chorus.collections.WorkfileSet(workfiles);
+    }
 });
--- spec/javascripts/models/search_result_spec.js
+++ spec/javascripts/models/search_result_spec.js
@@ -5,5 +5,51 @@ describe("chorus.models.SearchResult", function() {
 
     it("returns a short name", function() {
         expect(this.model.displayShortName()).toBe("the longest query in...")
-    })
+    });
+
+    describe("#workfiles", function() {
+        beforeEach(function() {
+            this.model.set({
+                workfile: {
+                    numFound: 3,
+                    docs: [
+                        {
+                            id: '1',
+                            name: 'My <em>Cool</em> Workfile'
+                        },
+                        {
+                            id: '2',
+                            name: 'Workfiles are <em>Cool</em>'
+                        },
+                        {
+                            id: '3',
+                            name: '<em>Cool</em> Breeze'
+                        }
+                    ]
+                }
+            });
+
+            this.workfiles = this.model.workfiles();
+        });
+
+        it("returns a WorkfileSet", function() {
+            expect(this.workfiles).toBeA(chorus.collections.WorkfileSet)
+        });
+
+        it("has the right filenames, with em tags stripped out", function() {
+            expect(this.workfiles.models.length).toBe(3);
+
+            expect(this.workfiles.models[0].get('id')).toBe('1');
+            expect(this.workfiles.models[0].get('name')).toBe('My <em>Cool</em> Workfile');
+            expect(this.workfiles.models[0].get('fileName')).toBe('My Cool Workfile');
+
+            expect(this.workfiles.models[1].get('id')).toBe('2');
+            expect(this.workfiles.models[1].get('name')).toBe('Workfiles are <em>Cool</em>');
+            expect(this.workfiles.models[1].get('fileName')).toBe('Workfiles are Cool');
+
+            expect(this.workfiles.models[2].get('id')).toBe('3');
+            expect(this.workfiles.models[2].get('name')).toBe('<em>Cool</em> Breeze');
+            expect(this.workfiles.models[2].get('fileName')).toBe('Cool Breeze');
+        });
+    });
 })
