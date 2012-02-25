dbe8b824185e1cbfa257ec625afcdaeb84b23f43
Bleicke Petersen & Max Brunsfeld <pair+bpetersen+mbrunsfeld@pivotallabs.com>
2012-02-24 17:49:21 -0800

    add missing integration test for workspace settings dialog

--- spec/integration/workspace_settings_spec.rb
+++ spec/integration/workspace_settings_spec.rb
@@ -28,7 +28,13 @@ describe "workspace settings dialog" do
         it "shows the correct formatting buttons" do
             @toolbar.should have_selector(".cleditorButton[title='Bold']")
             @toolbar.should have_selector(".cleditorButton[title='Italic']")
+            @toolbar.should have_selector(".cleditorButton[title='Bullets']")
+            @toolbar.should have_selector(".cleditorButton[title='Numbering']")
+
             @toolbar.should_not have_selector(".cleditorButton[title='Underline']")
+            @toolbar.should_not have_selector(".cleditorButton[title='Print']")
+            @toolbar.should_not have_selector(".cleditorButton[title='Font Color']")
+            @toolbar.should_not have_selector(".cleditorButton[title='Align Text Left']")
         end
     end
 end
