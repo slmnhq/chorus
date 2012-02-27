dcba64610745622b4935fd641fe372275fb0fed3
Bleicke Petersen & Erich Douglass <pair+bpetersen+edouglass@pivotallabs.com>
2012-02-27 12:24:33 -0800

    Don't break HTML tags when truncating summary text on the workspace show page. [#25445567]

--- spec/javascripts/views/truncated_text_view_spec.js
+++ spec/javascripts/views/truncated_text_view_spec.js
@@ -8,7 +8,7 @@ describe("chorus.views.TruncatedText", function() {
         context("when the model is not yet loaded", function() {
             beforeEach(function() {
                 this.model.loaded = undefined;
-                this.view = new chorus.views.TruncatedText({model: this.model, attribute: "summary", characters : 20 })
+                this.view = new chorus.views.TruncatedText({model: this.model, attribute: "summary", characters: 20 })
                 this.view.render();
             })
 
@@ -22,46 +22,45 @@ describe("chorus.views.TruncatedText", function() {
                 context("when the text has equal to or greater than the maximum number of characters", function() {
                     beforeEach(function() {
                         this.text = "Now is the time for all good men to come to the aid of their country";
-                        this.model.set({ summary : this.text });
-                        this.displayText = this.text.substring(0, 20)
-                        this.truncatedText = this.text.substring(20);
-                        this.view = new chorus.views.TruncatedText({model: this.model, attribute: "summary", characters : 20 })
+                        this.model.set({ summary: this.text });
+                        this.view = new chorus.views.TruncatedText({model: this.model, attribute: "summary", characters: 20 })
                         this.view.render();
-                    })
-
-                    it("truncates the text", function() {
-                        expect(this.view.$(".text").text()).toBe(this.displayText);
                     })
 
                     it("renders the truncated text", function() {
-                        expect(this.view.$(".truncated").text()).toBe(this.truncatedText)
+                        expect(this.view.$(".text_content").text()).toBe("Now is the time for ");
                     })
 
-                    it("does not add space between displayText and truncatedText", function() {
-                        expect(this.view.$(".entire_text .text, .entire_text .truncated").text()).toBe(this.text);
-                    });
-
                     it("renders 'read more' and 'read less' links", function() {
-                        expect(this.view.$(".links a.more")).toExist();
-                        expect(this.view.$(".links a.less")).toExist();
+                        expect(this.view.$(".links a.more")).not.toHaveClass("hidden");
+                        expect(this.view.$(".links a.less")).toHaveClass("hidden");
                         expect(this.view.$(".links a.more").text().trim()).toMatchTranslation("truncated_text.more");
                         expect(this.view.$(".links a.less").text().trim()).toMatchTranslation("truncated_text.less");
                     })
-
-                    it("starts out without the 'more' class on the surrounding div", function() {
-                        expect(this.view.$("> div")).not.toHaveClass("more");
-                    });
                 })
+
                 context("when the text has less than the maximum number of characters", function() {
                     beforeEach(function() {
                         this.text = "Now is the time for all good men to come to the aid of their country";
-                        this.model.set({ summary : this.text });
-                        this.view = new chorus.views.TruncatedText({model: this.model, attribute: "summary", characters : 200 })
+                        this.model.set({ summary: this.text });
+                        this.view = new chorus.views.TruncatedText({model: this.model, attribute: "summary", characters: 200 })
                         this.view.render();
                     })
 
-                    it("renders the bare text", function() {
-                        expect($(this.view.el).text().trim()).toBe(this.text);
+                    it("renders all of the text", function() {
+                        expect(this.view.$(".text_content").text()).toBe(this.text);
+                    })
+                })
+
+                context("when the content has HTML tags", function() {
+                    beforeEach(function() {
+                        this.model.set({summary: "<ul><li><b>One</b></li><li><b>Duo</b></li><li><b>Drei</b></li></ul>"});
+                        this.view = new chorus.views.TruncatedText({model: this.model, attribute: "summary", characters: 20 })
+                        this.view.render();
+                    });
+
+                    it("does not show half a tag", function() {
+                        expect(this.view.$(".text_content").html()).toBe("<ul><li><b>One</b></li></ul>");
                     })
                 })
             });
@@ -77,35 +76,28 @@ describe("chorus.views.TruncatedText", function() {
                         }
                     }
 
-                    this.view = new chorus.views.TruncatedText({model: this.model, attribute: "summary", characters : 20, lines: 2 })
+                    this.view = new chorus.views.TruncatedText({model: this.model, attribute: "summary", characters: 20, lines: 2 })
                     this.view.render();
-                    this.expectSimilarStrings = function(expectedDisplay, expectedTruncated) {
-                        var truncated = this.view.truncate(this.view.model.get("summary"), 20, 2);
-                        expect(truncated[0]).toBe(expectedDisplay);
-                        expect(truncated[1]).toBe(expectedTruncated);
-                        expect(textRegularize(this.view.$(".text").text())).toBe(textRegularize(expectedDisplay));
-                        expect(textRegularize(this.view.$(".truncated").text())).toBe(textRegularize(expectedTruncated));
-                    }
                 });
 
                 context("when the text has less than the maximum number of characters", function() {
                     context("when the text has less than or equal to the maximum number of lines", function() {
                         beforeEach(function() {
-                            this.model.set({summary : "really short"});
+                            this.model.set({summary: "really short"});
                         });
 
-                        it("renders the bare text", function() {
-                            expect($(this.view.el).text().trim()).toBe("really short");
+                        it("renders all of the text", function() {
+                            expect(this.view.$(".text_content").text().trim()).toBe("really short");
                         })
                     });
 
                     context("when the text has more than the maximum number of lines", function() {
                         beforeEach(function() {
-                            this.model.set({summary : "1\n2\n3\n"});
+                            this.model.set({summary: "1\n2\n3\n"});
                         });
 
                         it("renders as expected", function() {
-                            this.expectSimilarStrings("1\n2", "\n3\n");
+                            expect(this.view.$(".text_content").text()).toBe("1\n2");
                         });
                     });
                 });
@@ -113,32 +105,32 @@ describe("chorus.views.TruncatedText", function() {
                 context("when the text has more than the maximum number of characters", function() {
                     context("when the text has less than or equal to the maximum number of lines", function() {
                         beforeEach(function() {
-                            this.model.set({summary : "1234567890123456789012345"});
+                            this.model.set({summary: "1234567890123456789012345"});
                         });
 
-                        it("renders as expected", function() {
-                            this.expectSimilarStrings("12345678901234567890", "12345");
+                        it("renders all of the text", function() {
+                            expect(this.view.$(".text_content").text()).toBe("12345678901234567890");
                         });
                     });
 
                     context("when the text has more than the maximum number of lines", function() {
                         context("when the character limit is reached before the line limit", function() {
                             beforeEach(function() {
-                                this.model.set({summary : "123456789012\n4567890AB\nC\nD\nE"});
+                                this.model.set({summary: "123456789012\n4567890AB\nC\nD\nE"});
                             });
 
-                            it("renders as expected", function() {
-                                this.expectSimilarStrings("123456789012\n4567890", "AB\nC\nD\nE");
+                            it("renders the truncated text", function() {
+                                expect(this.view.$(".text_content").text()).toBe("123456789012\n4567890");
                             });
                         });
 
                         context("when the line limit is reached before the character limit", function() {
                             beforeEach(function() {
-                                this.model.set({summary : "12345\n6789012\n3456789012345"});
+                                this.model.set({summary: "12345\n6789012\n3456789012345"});
                             });
 
-                            it("renders as expected", function() {
-                                this.expectSimilarStrings("12345\n6789012", "\n3456789012345");
+                            it("renders the truncated text", function() {
+                                expect(this.view.$(".text_content").text()).toBe("12345\n6789012");
                             });
                         });
                     });
@@ -148,12 +140,12 @@ describe("chorus.views.TruncatedText", function() {
 
         context("when the attribute does not exist on the model", function() {
             beforeEach(function() {
-                this.view = new chorus.views.TruncatedText({model: this.model, attribute: "herpderp", characters : 200 })
+                this.view = new chorus.views.TruncatedText({model: this.model, attribute: "herpderp", characters: 200 })
                 this.view.render();
             })
 
             it("renders nothing", function() {
-                expect($(this.view.el).text().trim()).toBe("");
+                expect(this.view.$(".text_content").text()).toBe("");
             })
         });
     })
@@ -161,8 +153,8 @@ describe("chorus.views.TruncatedText", function() {
     describe("expanding and contracting truncated text", function() {
         beforeEach(function() {
             this.text = "Now is the time for all good men to come to the aid of their country";
-            this.model.set({ summary : this.text });
-            this.view = new chorus.views.TruncatedText({model: this.model, attribute: "summary", characters : 20 })
+            this.model.set({ summary: this.text });
+            this.view = new chorus.views.TruncatedText({model: this.model, attribute: "summary", characters: 20 })
             this.view.render();
         })
 
@@ -171,8 +163,16 @@ describe("chorus.views.TruncatedText", function() {
                 this.view.$("a.more").click();
             })
 
-            it("adds the 'more' class to the surrounding div", function() {
-                expect(this.view.$(".text_container")).toHaveClass("more");
+            it("displays the full text", function() {
+                expect(this.view.$(".text_content").text()).toBe(this.text);
+            })
+
+            it("hides the 'read more' link", function() {
+                expect(this.view.$("a.more")).toHaveClass('hidden');
+            })
+
+            it("shows the 'read less' link", function() {
+                expect(this.view.$("a.less")).not.toHaveClass('hidden');
             })
 
             describe("clicking 'read less'", function() {
@@ -180,30 +180,44 @@ describe("chorus.views.TruncatedText", function() {
                     this.view.$("a.less").click();
                 })
 
-                it("removes the 'more' class from the surrounding div", function() {
-                    expect(this.view.$(".text_container")).not.toHaveClass("more");
+                it("display the truncated text", function() {
+                    expect(this.view.$(".text_content").text()).toBe("Now is the time for ")
                 })
 
-                context("when the view re-renders", function() {
-                    beforeEach(function() {
-                        this.view.render();
-                    });
+                it("shows the 'read more' link", function() {
+                    expect(this.view.$("a.more")).not.toHaveClass('hidden');
+                })
 
-                    it("still does not have the 'more' class on the surrounding div", function() {
-                        expect(this.view.$(".text_container")).not.toHaveClass("more");
-                    });
-                });
+                it("hides the 'read less' link", function() {
+                    expect(this.view.$("a.less")).toHaveClass('hidden');
+                })
             })
-
-            context("when the view re-renders", function() {
-                beforeEach(function() {
-                    this.view.render();
-                });
-
-                it("retains the more class on the surrounding div", function() {
-                    expect(this.view.$(".text_container")).toHaveClass("more");
-                });
-            });
         })
     })
+ 
+    describe("#truncate", function() {
+        beforeEach(function() {
+            this.view = new chorus.views.TruncatedText();
+        });
+
+        it("should handle no truncation", function() {
+            expect(this.view.truncate("<b>One</b>", 30, undefined)).toBe("<b>One</b>");
+        });
+
+        it("should handle truncating to zero", function() {
+            expect(this.view.truncate("<b>One</b>", 0, undefined)).toBe("");
+        });
+
+        it("should not truncate in the middle of a tag", function() {
+            expect(this.view.truncate("<i>Foo</i><b>Bar</b>", 8, undefined)).toBe("<i>Foo");
+        });
+
+        it("should handle nested tags", function() {
+            expect(this.view.truncate("<ul><li>One</li><li>Two</li></ul>", 18, undefined)).toBe("<ul><li>One</li>");
+        });
+
+        it("should not truncate the text after a tag", function() {
+            expect(this.view.truncate("<i>Foo</i><b>Bar</b>Hello how are you", 33, undefined)).toBe("<i>Foo</i><b>Bar</b>Hello how are");
+        });
+    });
 })
