5337b5f9531e5304c3f471ab2ad6de31d78e8133
Bleicke Petersen & Mark Rushakoff <pair+bpetersen+mrushakoff@pivotallabs.com>
2012-03-01 16:51:58 -0800

    Workspace list sidebar WIP
    [#25809701]

---
+++ public/js/views/workspaces/workspace_list_sidebar.js
@@ -0,0 +1,40 @@
+chorus.views.WorkspaceListSidebar = chorus.views.Sidebar.extend({
+    constructorName: "WorkspaceListSidebar",
+    className: "workspace_list_sidebar",
+
+    subviews: {
+        '.activity_list': 'activityList',
+        '.tab_control': 'tabControl'
+    },
+
+    setup: function() {
+        chorus.PageEvents.subscribe("workspace:selected", this.setWorkspace, this)
+        this.tabControl = new chorus.views.TabControl([ {name: 'activity', selector: ".activity_list"} ])
+    },
+
+    additionalContext: function() {
+        return {
+            imageSrc: this.model.imageUrl(),
+            hasImage: this.model.hasImage()
+        }
+    },
+
+    setWorkspace: function(model) {
+        this.resource = this.model = model;
+
+        if (model) {
+            var activities = model.activities();
+            activities.fetch();
+            this.activityList = new chorus.views.ActivityList({
+                collection: activities,
+                additionalClass: "sidebar"
+            });
+
+            this.activityList.bind("content:changed", this.recalculateScrolling, this)
+        } else {
+            delete this.activityList;
+        }
+
+        this.render();
+    }
+});
