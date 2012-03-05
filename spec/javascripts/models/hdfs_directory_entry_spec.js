587a3907e5e6fe9ddeb6af0f893c0d9e99a99249
Corina Lawu & Mark Rushakoff <pair+corina+mrushakoff@pivotallabs.com>
2012-03-05 15:23:09 -0800

    Fix bad spec

--- spec/javascripts/models/hdfs_directory_entry_spec.js
+++ spec/javascripts/models/hdfs_directory_entry_spec.js
@@ -1,5 +1,6 @@
 describe("chorus.models.HdfsDirectoryEntry", function() {
     it("has the right entityType", function() {
+        this.model = fixtures.hdfsDirectoryEntryFile();
         expect(this.model.entityType).toBe("hdfs");
     });
 });
