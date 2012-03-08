5926e32b6da8ac8da930e0f244d9e3c80191a29e
Corina Lawu & Joseph Palermo <pair+corina+joseph@pivotallabs.com>
2012-03-07 16:37:18 -0800

    showUrl for HdfsEntry

--- public/js/models/hdfs_entry.js
+++ public/js/models/hdfs_entry.js
@@ -1,5 +1,16 @@
 chorus.models.HdfsEntry = chorus.models.Base.extend({
     constructorName: "HdfsEntry",
     entityType: "hdfs",
-    showUrlTemplate: "#"
+
+    showUrlTemplate: function() {
+        if(this.get("isDir")) {
+            return "instances/{{instance.id}}/browse" + this.getPath() + "/{{name}}";
+        } else {
+            return "instances/{{instance.id}}/browseFile" + this.getPath() + "/{{name}}";
+        }
+    },
+
+    getPath: function() {
+        return (this.get("path") == "/") ? "" : this.get("path")
+    }
 });
--- spec/javascripts/models/hdfs_entry_spec.js
+++ spec/javascripts/models/hdfs_entry_spec.js
@@ -3,4 +3,44 @@ describe("chorus.models.HdfsEntry", function() {
         this.model = fixtures.hdfsEntryFile();
         expect(this.model.entityType).toBe("hdfs");
     });
+
+    describe("showUrl", function() {
+        context("when entry is a directory", function() {
+            beforeEach(function() {
+                this.model = fixtures.hdfsEntryDir({
+                    instance: {
+                        id: '42'
+                    },
+                    path: '/data/somewhere'
+                });
+            });
+
+            it("is correct", function() {
+                var url = "#/instances/42/browse/data/somewhere/" + this.model.get("name");
+                expect(this.model.showUrl()).toBe(url);
+            });
+        });
+
+        context("when entry is a file", function() {
+            beforeEach(function() {
+                this.model = fixtures.hdfsEntryFile({
+                    instance: {
+                        id: '42'
+                    },
+                    path: '/data/somewhere'
+                });
+            });
+
+            it("is correct", function() {
+                var url = "#/instances/42/browseFile/data/somewhere/" + this.model.get("name");
+                expect(this.model.showUrl()).toBe(url);
+            });
+
+            it("is correct when path is /", function() {
+                this.model.set({path: "/"})
+                var url = "#/instances/42/browseFile/" + this.model.get("name");
+                expect(this.model.showUrl()).toBe(url);
+            });
+        });
+    });
 });
