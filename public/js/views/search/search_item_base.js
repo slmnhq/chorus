5bccb75c93adcbdb16859d13faf44cefe7777b2b
Charles Hansen & Jacob Maine <pair+chansen+jacob@pivotallabs.com>
2012-03-08 15:37:40 -0800

    Remove extraneous wrappers around LIs.  Move search instance view specs out of list view specs.

--- public/js/views/search/search_item_base.js
+++ public/js/views/search/search_item_base.js
@@ -1,9 +1,13 @@
 chorus.views.SearchItemBase = chorus.views.Base.extend({
+    tagName: "li",
+    additionalClass: "result_item",
+
     makeCommentList: function() {
         return new chorus.views.SearchResultCommentList({comments: this.model.get("comments")});
     },
 
     postRender: function() {
+        $(this.el).data('cid', this.model.cid);
         var commentsView = this.makeCommentList();
         this.$(".comments_container").append(commentsView.render().el);
     }
