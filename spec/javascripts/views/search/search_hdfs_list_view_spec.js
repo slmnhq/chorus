14d22f3a2b9103c9682ef9cc1f1c271ae43c39b8
Charles Hansen & Jacob Maine <pair+chansen+jacob@pivotallabs.com>
2012-03-08 17:07:34 -0800

    Move specs for rendering of hdfs search items out of list spec

--- spec/javascripts/views/search/search_hdfs_list_view_spec.js
+++ spec/javascripts/views/search/search_hdfs_list_view_spec.js
@@ -138,31 +138,5 @@ describe("chorus.views.SearchHdfsList", function() {
         it("should render a list item for each result", function() {
             expect(this.view.$("li.result_item").length).toBe(3);
         });
-
-        it("should render the name for each file", function() {
-            expect(this.view.$("li.result_item:eq(0) a.name")).toContainText(this.models.at(0).get("name"));
-        });
-
-        it("should render the instance location", function() {
-            var $inst = this.view.$("li.result_item:eq(0) .instance a");
-
-            expect($inst.text()).toBe("hadoop");
-            expect($inst.attr("href")).toBe("#/instances/10001/browse/");
-        });
-
-        it("should render a link to each file", function() {
-            expect(this.view.$('a.name:eq(0)').attr('href')).toMatchUrl(this.models.at(0).showUrl());
-        });
-
-        it("should render the location for each file", function() {
-            var $links = this.view.$("li.result_item:eq(0) .path_parts a");
-            expect($links.length).toBe(2);
-
-            expect($links.eq(0).text()).toBe("aaa");
-            expect($links.eq(0).attr("href")).toBe("#/instances/10001/browse/aaa");
-
-            expect($links.eq(1).text()).toBe("bbb");
-            expect($links.eq(1).attr("href")).toBe("#/instances/10001/browse/aaa/bbb");
-        });
     });
 });
---
+++ spec/javascripts/views/search/search_hdfs_view_spec.js
@@ -0,0 +1,42 @@
+describe("chorus.views.SearchHdfs", function() {
+    beforeEach(function() {
+        this.result = fixtures.searchResult({hdfs: {
+            docs: [
+                fixtures.searchResultHdfsJson({
+                    path: "/aaa/bbb"
+                })
+            ],
+            numFound: 10}
+        });
+
+        this.model = this.result.hdfs().models[0];
+        this.view = new chorus.views.SearchHdfs({model: this.model});
+        this.view.render()
+    });
+
+    it("should render the name for each file", function() {
+        expect(this.view.$("a.name")).toContainText(this.model.get("name"));
+    });
+
+    it("should render the instance location", function() {
+        var $inst = this.view.$(".instance a");
+
+        expect($inst.text()).toBe("hadoop");
+        expect($inst.attr("href")).toBe("#/instances/10001/browse/");
+    });
+
+    it("should render a link to each file", function() {
+        expect(this.view.$('a.name').attr('href')).toMatchUrl(this.model.showUrl());
+    });
+
+    it("should render the location for each file", function() {
+        var $links = this.view.$(".path_parts a");
+        expect($links.length).toBe(2);
+
+        expect($links.eq(0).text()).toBe("aaa");
+        expect($links.eq(0).attr("href")).toBe("#/instances/10001/browse/aaa");
+
+        expect($links.eq(1).text()).toBe("bbb");
+        expect($links.eq(1).attr("href")).toBe("#/instances/10001/browse/aaa/bbb");
+    });
+});
