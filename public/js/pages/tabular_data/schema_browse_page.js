63e2f01b3d0c29075a14fd73cfdc98361035e32c
Erich Douglass & Mark Rushakoff <pair+edouglass+mrushakoff@pivotallabs.com>
2012-03-08 17:16:18 -0800

    Update schema browser breadcrumbs to link to schema and database index pages.
    [#25687073]

--- public/js/pages/tabular_data/schema_browse_page.js
+++ public/js/pages/tabular_data/schema_browse_page.js
@@ -1,53 +1,48 @@
-(function() {
-    var breadcrumbsView = chorus.views.ModelBoundBreadcrumbsView.extend({
-        getLoadedCrumbs: function() {
-            var instance = { id: this.model.get('instanceId'), name: this.model.get('instanceName')}
-            return [
-                {label: t("breadcrumbs.home"), url: "#/"},
-                {label: t("breadcrumbs.instances"), url: '#/instances'},
-                {label: this.model.get("instanceName"), dialog: "SchemaBrowser", data: { instance: instance} },
-                {label: this.model.get("databaseName"), dialog: "SchemaBrowser",
-                    data: { instance: instance, databaseName: this.model.get("databaseName")} },
-                {label: this.model.get("name")}
-            ];
-        }
-    });
+chorus.pages.SchemaBrowsePage = chorus.pages.Base.extend({
+    helpId: "schema",
+    useLoadingSection: true,
 
-    chorus.pages.SchemaBrowsePage = chorus.pages.Base.extend({
-        helpId: "schema",
+    setup: function(instanceId, databaseName, schemaName) {
+        this.schema = new chorus.models.Schema({
+            instanceId: instanceId,
+            databaseName: databaseName,
+            name: schemaName
+        });
 
-        setup: function(instanceId, databaseName, schemaName) {
-            this.schema = new chorus.models.Schema({
-                instanceId: instanceId,
-                databaseName : databaseName,
-                name : schemaName
-            });
+        this.instance = new chorus.models.Instance({id: instanceId});
+        this.instance.fetch();
+        this.requiredResources.push(this.instance);
 
-            this.instance = new chorus.models.Instance({id: instanceId});
-            this.instance.bindOnce("loaded", function() {
-                this.schema.set({instanceName : this.instance.get("name")});
-                this.mainContent.contentHeader.options.title = this.schema.canonicalName();
-                this.mainContent.contentHeader.render();
-            }, this);
-            this.instance.fetch();
+        this.collection = new chorus.collections.DatabaseObjectSet([], {instanceId: instanceId, databaseName: databaseName, schemaName: schemaName });
+        this.collection.sortAsc("objectName");
+        this.collection.fetch();
+        this.requiredResources.push(this.collection);
 
-            this.breadcrumbs = new breadcrumbsView({model: this.schema});
+        this.sidebar = new chorus.views.TabularDataSidebar({listMode: true});
 
-            this.collection = new chorus.collections.DatabaseObjectSet([], {instanceId: instanceId, databaseName: databaseName, schemaName:schemaName });
-            this.collection.sortAsc("objectName");
-            this.collection.fetch();
+        chorus.PageEvents.subscribe("tabularData:selected", function(dataset) {
+            this.model = dataset;
+        }, this);
+    },
 
-            this.mainContent = new chorus.views.MainContentList({
-                modelClass: "Dataset",
-                collection: this.collection,
-                title: this.schema.canonicalName()
-            });
+    resourcesLoaded: function() {
+        this.schema.set({instanceName: this.instance.get("name")});
 
-            this.sidebar = new chorus.views.TabularDataSidebar({listMode: true});
+        var database = new chorus.models.Database({instanceId: this.instance.id, name: this.schema.get("databaseName")});
+        this.crumbs = [
+            {label: t("breadcrumbs.home"), url: "#/"},
+            {label: t("breadcrumbs.instances"), url: '#/instances'},
+            {label: this.instance.get("name"), url: this.instance.showUrl()},
+            {label: this.schema.get("databaseName"), url: database.showUrl() },
+            {label: this.schema.get("name")}
+        ];
 
-            chorus.PageEvents.subscribe("tabularData:selected", function(dataset) {
-                this.model = dataset;
-            }, this);
-        }
-    });
-})();
+        this.mainContent = new chorus.views.MainContentList({
+            modelClass: "Dataset",
+            collection: this.collection,
+            title: this.schema.canonicalName()
+        });
+
+        this.render();
+    }
+});
--- spec/javascripts/views/tabular_data/schema_browse_page_spec.js
+++ spec/javascripts/views/tabular_data/schema_browse_page_spec.js
@@ -2,6 +2,7 @@ describe("chorus.pages.SchemaBrowsePage", function() {
     beforeEach(function() {
         this.schema = fixtures.schema();
         this.instance = fixtures.instance({id: this.schema.get("instanceId")});
+        this.database = fixtures.database({name: this.schema.get("databaseName"), instanceId: this.instance.get("id")});
         this.page = new chorus.pages.SchemaBrowsePage(this.schema.get("instanceId"), this.schema.get("databaseName"), this.schema.get("name"));
     })
 
@@ -9,86 +10,62 @@ describe("chorus.pages.SchemaBrowsePage", function() {
         expect(this.page.helpId).toBe("schema")
     })
 
-    describe("#initialize", function() {
-        it("fetches the instance", function() {
-            expect(this.server.lastFetchFor(this.instance)).not.toBeUndefined();
+    context("after everything has been fetched", function() {
+        beforeEach(function() {
+            this.server.completeFetchFor(this.instance);
+            this.server.completeFetchFor(this.page.collection, [
+                fixtures.databaseTable(),
+                fixtures.databaseView()
+            ]);
         });
 
-        context("after the instance has been fetched", function() {
-            beforeEach(function() {
-                this.server.completeFetchFor(this.instance);
-            });
-
-            it("sets the instanceName on the schema", function() {
-                expect(this.page.schema.get("instanceName")).toBe(this.instance.get("name"));
-            });
+        it("sets the instanceName on the schema", function() {
+            expect(this.page.schema.get("instanceName")).toBe(this.instance.get("name"));
         });
 
-        context("after everything has been fetched", function() {
-            beforeEach(function() {
-                this.server.completeFetchFor(this.instance);
-                this.page.render();
-            });
-
-            it("has the right breadcrumbs", function() {
-                expect(this.page.$("#breadcrumbs .breadcrumb a").eq(0).attr("href")).toBe("#/");
-                expect(this.page.$("#breadcrumbs .breadcrumb a").eq(0).text()).toBe(t("breadcrumbs.home"));
-
-                expect(this.page.$("#breadcrumbs .breadcrumb a").eq(1).attr("href")).toBe("#/instances");
-                expect(this.page.$("#breadcrumbs .breadcrumb a").eq(1).text()).toBe(t("breadcrumbs.instances"));
-
-                expect(this.page.$("#breadcrumbs .breadcrumb a").eq(2)).toHaveClass("dialog");
-                expect(this.page.$("#breadcrumbs .breadcrumb a").eq(2).data("dialog")).toBe("SchemaBrowser");
-                expect(this.page.$("#breadcrumbs .breadcrumb a").eq(2).data("instance")).toEqual({id: this.instance.get("id"), name: this.instance.get("name")});
-                expect(this.page.$("#breadcrumbs .breadcrumb a").eq(2).text()).toBe(this.page.schema.get("instanceName"));
-
-                expect(this.page.$("#breadcrumbs .breadcrumb a").eq(3)).toHaveClass("dialog");
-                expect(this.page.$("#breadcrumbs .breadcrumb a").eq(3).data("dialog")).toBe("SchemaBrowser");
-                expect(this.page.$("#breadcrumbs .breadcrumb a").eq(3).data("instance")).toEqual({id: this.instance.get("id"), name: this.instance.get("name")});
-                expect(this.page.$("#breadcrumbs .breadcrumb a").eq(3).data("databaseName")).toBe(this.page.schema.get("databaseName"));
-                expect(this.page.$("#breadcrumbs .breadcrumb a").eq(3).text()).toBe(this.page.schema.get("databaseName"));
-
-                expect(this.page.$("#breadcrumbs .breadcrumb .slug").text()).toBe(this.page.schema.get("name"));
-            });
-
-            it("has the right title", function() {
-                expect(this.page.$(".content_header h1").text()).toBe(this.page.schema.canonicalName());
-            });
-
-            it("constructs the main content list correctly", function() {
-                expect(this.page.mainContent).toBeA(chorus.views.MainContentList);
-                expect(this.page.mainContent.collection).toBe(this.page.collection);
-                expect(this.page.mainContent.collection).toBeA(chorus.collections.DatabaseObjectSet);
-
-                expect(this.page.$(this.page.mainContent.el).length).toBe(1);
-            });
-
-            it("creates the collection with the right options", function() {
-                expect(this.page.collection.attributes.instanceId).toBe(this.schema.get("instanceId"))
-                expect(this.page.collection.attributes.databaseName).toBe(this.schema.get("databaseName"))
-                expect(this.page.collection.attributes.schemaName).toBe(this.schema.get("name"))
-            })
-
-            context("when the fetch for the tables and views in the schema completes", function() {
-                beforeEach(function() {
-                    this.server.completeFetchFor(this.page.collection, [
-                        fixtures.databaseTable(),
-                        fixtures.databaseView(),
-                    ]);
-                });
-
-                it("pre-selects the first item", function() {
-                    expect(this.page.$(".dataset_list li").eq(0)).toHaveClass("selected");
-                });
-
-                it("changes the selection after clicking another item", function() {
-                    this.page.$(".dataset_list li").eq(1).click();
-
-                    expect(this.page.$(".dataset_list li").eq(0)).not.toHaveClass("selected");
-                    expect(this.page.$(".dataset_list li").eq(1)).toHaveClass("selected");
-                });
-
-            });
+        it("pre-selects the first item", function() {
+            expect(this.page.$(".dataset_list li").eq(0)).toHaveClass("selected");
         });
+
+        it("changes the selection after clicking another item", function() {
+            this.page.$(".dataset_list li").eq(1).click();
+
+            expect(this.page.$(".dataset_list li").eq(0)).not.toHaveClass("selected");
+            expect(this.page.$(".dataset_list li").eq(1)).toHaveClass("selected");
+        });
+
+        it("has the right breadcrumbs", function() {
+            expect(this.page.$("#breadcrumbs .breadcrumb a").eq(0)).toHaveHref("#/");
+            expect(this.page.$("#breadcrumbs .breadcrumb a").eq(0)).toContainTranslation("breadcrumbs.home");
+
+            expect(this.page.$("#breadcrumbs .breadcrumb a").eq(1)).toHaveHref("#/instances");
+            expect(this.page.$("#breadcrumbs .breadcrumb a").eq(1)).toContainTranslation("breadcrumbs.instances");
+
+            expect(this.page.$("#breadcrumbs .breadcrumb a").eq(2)).toContainText(this.instance.get("name"));
+            expect(this.page.$("#breadcrumbs .breadcrumb a").eq(2)).toHaveHref(this.instance.showUrl());
+
+            expect(this.page.$("#breadcrumbs .breadcrumb a").eq(3)).toContainText(this.database.get("name"));
+            expect(this.page.$("#breadcrumbs .breadcrumb a").eq(3)).toHaveHref(this.database.showUrl());
+
+            expect(this.page.$("#breadcrumbs .breadcrumb .slug").text()).toBe(this.page.schema.get("name"));
+        });
+
+        it("has the right title", function() {
+            expect(this.page.$(".content_header h1").text()).toBe(this.page.schema.canonicalName());
+        });
+
+        it("constructs the main content list correctly", function() {
+            expect(this.page.mainContent).toBeA(chorus.views.MainContentList);
+            expect(this.page.mainContent.collection).toBe(this.page.collection);
+            expect(this.page.mainContent.collection).toBeA(chorus.collections.DatabaseObjectSet);
+
+            expect(this.page.$(this.page.mainContent.el).length).toBe(1);
+        });
+
+        it("creates the collection with the right options", function() {
+            expect(this.page.collection.attributes.instanceId).toBe(this.schema.get("instanceId"))
+            expect(this.page.collection.attributes.databaseName).toBe(this.schema.get("databaseName"))
+            expect(this.page.collection.attributes.schemaName).toBe(this.schema.get("name"))
+        })
     });
 });
