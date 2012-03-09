3ae7f0551e6bc4b32ebbd05047d71b2e93dd269c
Bleicke Petersen & Erich Douglass <pair+bpetersen+edouglass@pivotallabs.com>
2012-03-09 12:07:48 -0800

    Always display the user's title and only display supporting messages if they match the search.
    [fixes #25443177]

--- public/js/views/search/search_user_view.js
+++ public/js/views/search/search_user_view.js
@@ -2,24 +2,25 @@ chorus.views.SearchUser = chorus.views.SearchItemBase.extend({
     constructorName: "SearchUserView",
     className: "search_user",
 
-    additionalContext: function () {
+    additionalContext: function() {
         var modelWithSearchResults = chorus.helpers.withSearchResults(this.model);
-        var supportingMessage = _.compact(_.map(
-            ["title", "ou", "content", "emailAddress", "name"],
+        var supportingMessage = _.compact(_.map(["ou", "content", "emailAddress", "name"],
             function(fieldName) {
-                var value = modelWithSearchResults.get(fieldName);
-                if (value) {
-                    var result = {};
-                    result[fieldName] = value;
-                    return result
-                };
-            }
-        ));
+                if (this.model.get("highlightedAttributes")) {
+                    var value = this.model.get("highlightedAttributes")[fieldName];
+                    if (value) {
+                        var result = {};
+                        result[fieldName] = value;
+                        return result
+                    }
+                }
+            }, this), this);
 
         return {
             iconSrc: this.model.imageUrl({size:"icon"}),
             link: this.model.showUrl(),
             displayName: modelWithSearchResults.displayName(),
+            title: modelWithSearchResults.get("title"),
             supportingMessage: supportingMessage.slice(0,3),
             moreSupportingMessage: supportingMessage.slice(3),
             hasMoreSupportingMessage: Math.max(0, supportingMessage.length - 3)
