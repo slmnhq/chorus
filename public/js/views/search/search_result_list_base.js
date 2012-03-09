e9f3331707d44d157dc1e4697dc776e9c50aaff1
Jesse Toth & Lenny Turetsky <pair+jesse+lenny@pivotallabs.com>
2012-03-09 12:25:47 -0800

    moved header back into unified SearchResultListBase bc now it doesn't cause duplication

--- public/js/views/search/search_result_list_base.js
+++ public/js/views/search/search_result_list_base.js
@@ -1,27 +1,31 @@
 chorus.views.SearchResultListBase = chorus.views.Base.extend({
     constructorName: "SearchResultListBase",
     additionalClass: "list",
+    className: "search_result_list_base",
 
-    subviews: {
-        ".search_result_header": "header"
+    events: {
+        "click a.show_all": "showAll",
+        "click a.next": "showNext",
+        "click a.previous": "showPrevious"
     },
 
     setup: function() {
         this.query = this.options.query;
         this.entityType = this.options.entityType;
-        this.className = "search_" + this.entityType + "_list";
         this.listItemConstructorName = "Search" + _.capitalize(this.entityType);
-        this.header = new chorus.views.SearchResultHeader({query: this.query, collection: this.collection, entityType: this.entityType});
+        this.additionalClass += " search_" + this.entityType + "_list";
     },
 
     additionalContext: function() {
         var ctx = {
+            entityType: this.entityType,
             shown: this.collection.models.length,
             total: this.collection.attributes.total,
             hasNext: this.query && this.query.hasNextPage(),
             hasPrevious: this.query && this.query.hasPreviousPage(),
             filteredSearch: this.query && this.query.entityType() == this.entityType,
-            moreResults: (this.collection.models.length < this.collection.attributes.total)
+            moreResults: (this.collection.models.length < this.collection.attributes.total),
+            title: t("search.type." + this.options.entityType)
         };
 
         if(ctx.hasNext || ctx.hasPrevious) {
@@ -37,6 +41,24 @@ chorus.views.SearchResultListBase = chorus.views.Base.extend({
         this.collection.each(function(model) {
             ul.append(this.makeListItemView(model).render().el);
         }, this);
+    },
+
+    showAll: function(e) {
+        e && e.preventDefault();
+        this.query.set({entityType: $(e.currentTarget).data("type")})
+        chorus.router.navigate(this.query.showUrl(), true);
+    },
+
+    showNext: function(e) {
+        e && e.preventDefault();
+        this.query.getNextPage();
+        this.render();
+    },
+
+    showPrevious: function(e) {
+        e && e.preventDefault();
+        this.query.getPreviousPage();
+        this.render();
     },
 
     makeListItemView: function(model) {
