2b11ce4e193ff49950d7ac8e7e6210987f667089
Jacob Maine & Max Brunsfeld <pair+jacob+mbrunsfeld@pivotallabs.com>
2012-03-09 10:02:01 -0800

    Clean up search instance list spec, in prep for consolidating all search list specs

--- spec/javascripts/views/search/search_instance_list_view_spec.js
+++ spec/javascripts/views/search/search_instance_list_view_spec.js
@@ -9,102 +9,103 @@ describe("chorus.views.SearchInstanceList", function() {
         this.view.render()
     });
 
+    it("has a list element for each model in the collection", function() {
+        expect(this.view.$('li').length).toBe(2);
+    });
+
     context("filtered search", function() {
         beforeEach(function() {
             this.result.set({entityType: "instance"});
             this.view.render();
         });
 
-        describe("pagination bar", function() {
-            context("when there are two pages of results", function() {
+        it("has a count of total results", function() {
+            expect(this.view.$('.pagination .count')).toContainTranslation("search.results", {count: 24})
+        });
+
+        context("when there is one page of results", function() {
+            beforeEach(function() {
+                spyOn(this.result, "hasNextPage").andReturn(false);
+                spyOn(this.result, "hasPreviousPage").andReturn(false);
+                this.view.render();
+            });
+
+            it("does not have next and previous links", function() {
+                expect(this.view.$('.pagination a.next')).not.toExist();
+                expect(this.view.$('.pagination a.previous')).not.toExist();
+            });
+
+            it("has 'next' and 'previous' in plain text", function() {
+                expect(this.view.$('.pagination span.next')).toContainTranslation("search.next");
+                expect(this.view.$('.pagination span.previous')).toContainTranslation("search.previous");
+            });
+
+            it ("does not have the 'Page x of y' text", function (){
+                expect(this.view.$('.pagination span.page_numbers')).not.toExist();
+            });
+
+        })
+
+        context("when there are two pages of results", function() {
+            beforeEach(function() {
+                spyOn(this.result, "totalPageNumber").andReturn(2);
+            });
+
+            context("and I am on the first page", function() {
                 beforeEach(function() {
-                    spyOn(this.result, "totalPageNumber").andReturn(2);
-                });
-
-                context("and I am on the first page", function() {
-                    beforeEach(function() {
-                        spyOn(this.result, "hasPreviousPage").andReturn(false);
-                        spyOn(this.result, "hasNextPage").andReturn(true);
-                        this.view.render();
-                    });
-
-                    it("should have a next link", function() {
-                        expect(this.view.$('.pagination a.next')).toExist();
-                        expect(this.view.$('.pagination a.next')).toContainTranslation("search.next");
-                    });
-
-                    it("should not have a previous link", function() {
-                        expect(this.view.$('.pagination a.previous')).not.toExist();
-                    });
-
-                    it("should have previous in plain text", function() {
-                        expect(this.view.$('.pagination span.previous')).toContainTranslation("search.previous");
-                    });
-
-                    it ("should have the 'Page x of y' text", function (){
-                        expect(this.view.$('.pagination span.page_numbers')).toExist();
-                        expect(this.view.$('.pagination span.page_numbers')).toContainTranslation("search.page", {shown: 1, total: 2})
-                    });
-
-                });
-
-                context("and I am on the second page", function(){
-                    beforeEach(function() {
-                        spyOn(this.result, "hasNextPage").andReturn(false);
-                        spyOn(this.result, "hasPreviousPage").andReturn(true);
-                        this.view.render();
-                    });
-
-                    it("should have a previous link", function() {
-                        expect(this.view.$('.pagination a.previous')).toExist();
-                        expect(this.view.$('.pagination a.previous')).toContainTranslation("search.previous");
-                    });
-
-                    it("should not have a next link", function() {
-                        expect(this.view.$('.pagination a.next')).not.toExist();
-                    });
-
-                    it("should have next in plain text", function() {
-                        expect(this.view.$('.pagination span.next')).toContainTranslation("search.next");
-                    });
-                });
-            })
-
-            context("when there is one page of results", function() {
-                beforeEach(function() {
-                    spyOn(this.result, "hasNextPage").andReturn(false);
                     spyOn(this.result, "hasPreviousPage").andReturn(false);
+                    spyOn(this.result, "hasNextPage").andReturn(true);
                     this.view.render();
                 });
 
-                it("should not have next and previous links", function() {
-                    expect(this.view.$('.pagination a.next')).not.toExist();
+                it("has a next link", function() {
+                    expect(this.view.$('.pagination a.next')).toExist();
+                    expect(this.view.$('.pagination a.next')).toContainTranslation("search.next");
+                });
+
+                it("has a previous link", function() {
                     expect(this.view.$('.pagination a.previous')).not.toExist();
                 });
 
-                it("should have next and previous in plain text", function() {
-                    expect(this.view.$('.pagination span.next')).toContainTranslation("search.next");
+                it("has 'previous' in plain text", function() {
                     expect(this.view.$('.pagination span.previous')).toContainTranslation("search.previous");
                 });
 
-                it ("should not have the 'Page x of y' text", function (){
-                    expect(this.view.$('.pagination span.page_numbers')).not.toExist();
+                it("has the 'Page x of y' text", function (){
+                    expect(this.view.$('.pagination span.page_numbers')).toExist();
+                    expect(this.view.$('.pagination span.page_numbers')).toContainTranslation("search.page", {shown: 1, total: 2})
+                });
+            });
+
+            context("and I am on the second page", function(){
+                beforeEach(function() {
+                    spyOn(this.result, "hasNextPage").andReturn(false);
+                    spyOn(this.result, "hasPreviousPage").andReturn(true);
+                    this.view.render();
                 });
 
-            })
+                it("has a 'previous' link", function() {
+                    expect(this.view.$('.pagination a.previous')).toExist();
+                    expect(this.view.$('.pagination a.previous')).toContainTranslation("search.previous");
+                });
 
-            it("has a count of total results", function() {
-                expect(this.view.$('.pagination .count')).toContainTranslation("search.results", {count: 24})
+                it("does not have a 'next' link", function() {
+                    expect(this.view.$('.pagination a.next')).not.toExist();
+                });
+
+                it("has 'next' in plain text", function() {
+                    expect(this.view.$('.pagination span.next')).toContainTranslation("search.next");
+                });
             });
-        });
+        })
     });
 
     context("unfiltered search", function() {
-        describe("details bar", function() {
-            it("has a title", function() {
-                expect(this.view.$(".details .title")).toContainTranslation("instances.title");
-            });
+        it("has a title", function() {
+            expect(this.view.$(".details .title")).toContainTranslation("instances.title");
+        });
 
+        context("when there are additional results", function() {
             it("has a long count", function() {
                 expect(this.view.$(".details .count")).toContainTranslation("search.count", {shown: "2", total: "24"});
             });
@@ -121,49 +122,43 @@ describe("chorus.views.SearchInstanceList", function() {
                     this.view.$("a.show_all").click();
                 });
 
-                it("should navigate to the instance results page", function() {
+                it("navigates to the instance results page", function() {
                     expect(chorus.router.navigate).toHaveBeenCalledWith(this.result.showUrl(), true);
                 });
             });
+        });
 
-            context("has no additional results", function() {
-                beforeEach(function() {
-                    this.view = new chorus.views.SearchInstanceList({
-                        collection: new chorus.collections.InstanceSet([fixtures.instance(), fixtures.instance()], { total: "2" })
-                    });
-
-                    this.view.render()
+        context("when there are no additional results", function() {
+            beforeEach(function() {
+                this.view = new chorus.views.SearchInstanceList({
+                    collection: new chorus.collections.InstanceSet([fixtures.instance(), fixtures.instance()], { total: "2" })
                 });
 
-                it("has a short count", function() {
-                    expect(this.view.$(".details .count")).toContainTranslation("search.count_short", {shown: "2"});
-                });
+                this.view.render()
+            });
 
-                it("has no showAll link", function() {
-                    expect(this.view.$(".details a.show_all")).not.toExist();
-                })
-            })
+            it("has a short count", function() {
+                expect(this.view.$(".details .count")).toContainTranslation("search.count_short", {shown: "2"});
+            });
 
-            context("has no results at all", function() {
-                beforeEach(function() {
-                    this.view = new chorus.views.SearchWorkspaceList({
-                        collection: new chorus.collections.InstanceSet([], {total: "0"})
-                    });
-
-                    this.view.render()
-                });
-
-                it("does not show the bar or the list", function() {
-                    expect(this.view.$(".details")).not.toExist();
-                    expect(this.view.$("ul")).not.toExist();
-                });
+            it("has no showAll link", function() {
+                expect(this.view.$(".details a.show_all")).not.toExist();
             })
         })
-    });
 
-    describe("list elements", function() {
-        it("there is one for each model in the collection", function() {
-            expect(this.view.$('li').length).toBe(2);
-        });
+        context("when there are no results at all", function() {
+            beforeEach(function() {
+                this.view = new chorus.views.SearchWorkspaceList({
+                    collection: new chorus.collections.InstanceSet([], {total: "0"})
+                });
+
+                this.view.render()
+            });
+
+            it("does not show the bar or the list", function() {
+                expect(this.view.$(".details")).not.toExist();
+                expect(this.view.$("ul")).not.toExist();
+            });
+        })
     });
 });
