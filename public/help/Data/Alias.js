b8315db8c3e5551c10ffd1005d035b880c4407b9
Danny Burkes <pair+danny@pivotallabs.com>
2012-03-14 11:25:21 -0700

    new help drop

--- public/help/Data/Alias.js
+++ public/help/Data/Alias.js
@@ -3,16 +3,16 @@ CMCXmlParser._FilePathToXmlStringMap.Add(
 	'<?xml version=\"1.0\" encoding=\"utf-8\"?>' +
 	'<CatapultAliasFile DefaultSkinName=\"NewSkin\">' +
 	'    <!-- saved from url=(0014)about:internet -->' +
-	'    <Map Name=\"Dashboard\" Link=\"Dashboard.htm\" ResolvedId=\"1\" />' +
-	'    <Map Name=\"Dataset\" Link=\"Dataset.htm\" ResolvedId=\"1000\" />' +
-	'    <Map Name=\"Datasets\" Link=\"Datasets.htm\" ResolvedId=\"1001\" />' +
-	'    <Map Name=\"Getting_Started\" Link=\"Getting_Started.htm\" ResolvedId=\"1002\" />' +
-	'    <Map Name=\"Instances\" Link=\"Instances.htm\" ResolvedId=\"1003\" />' +
-	'    <Map Name=\"Interface\" Link=\"Interface.htm\" ResolvedId=\"1004\" />' +
-	'    <Map Name=\"User_edit\" Link=\"User_edit.htm\" ResolvedId=\"1005\" />' +
-	'    <Map Name=\"Users\" Link=\"Users.htm\" ResolvedId=\"1006\" />' +
-	'    <Map Name=\"Workfiles\" Link=\"Workfiles.htm\" ResolvedId=\"1007\" />' +
-	'    <Map Name=\"Workspace_summary\" Link=\"Workspace_summary.htm\" ResolvedId=\"1008\" />' +
-	'    <Map Name=\"Workspaces\" Link=\"Workspaces.htm\" ResolvedId=\"1009\" />' +
+	'    <Map Name=\"workfiles\" Link=\"workfiles.htm\" ResolvedId=\"1008\" />' +
+	'    <Map Name=\"workspaces\" Link=\"workspaces.htm\" ResolvedId=\"1010\" />' +
+	'    <Map Name=\"workspace_summary\" Link=\"workspace_summary.htm\" ResolvedId=\"1009\" />' +
+	'    <Map Name=\"users\" Link=\"users.htm\" ResolvedId=\"1007\" />' +
+	'    <Map Name=\"user_edit\" Link=\"user_edit.htm\" ResolvedId=\"1006\" />' +
+	'    <Map Name=\"interface\" Link=\"interface.htm\" ResolvedId=\"1005\" />' +
+	'    <Map Name=\"instances\" Link=\"instances.htm\" ResolvedId=\"1004\" />' +
+	'    <Map Name=\"getting_started\" Link=\"getting_started.htm\" ResolvedId=\"1003\" />' +
+	'    <Map Name=\"datasets\" Link=\"datasets.htm\" ResolvedId=\"1002\" />' +
+	'    <Map Name=\"dataset\" Link=\"dataset.htm\" ResolvedId=\"1001\" />' +
+	'    <Map Name=\"dashboard\" Link=\"dashboard.htm\" ResolvedId=\"1000\" />' +
 	'</CatapultAliasFile>'
 );
