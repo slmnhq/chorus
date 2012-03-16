2a0f44e8fc91653d0d38555e7b049fd4c79fd5c9
Jacob Maine & Max Brunsfeld <pair+jacob+mbrunsfeld@pivotallabs.com>
2012-03-16 14:14:37 -0700

    Make InstanceCredentials mixin work on pages without instances [#26508965]

--- public/js/mixins/instance_credentials.js
+++ public/js/mixins/instance_credentials.js
@@ -1,16 +1,31 @@
 chorus.Mixins.InstanceCredentials = {};
 
 chorus.Mixins.InstanceCredentials.model = {
-    needsInstanceCredentials: function() {
+    instanceRequiringCredentials: function() {
         var errorMessage = this.serverErrors && this.serverErrors[0] && this.serverErrors[0].message
-        return !!(errorMessage && errorMessage.match(/Account.*map.*needed/))
+        if (errorMessage && errorMessage.match(/Account.*map.*needed/)) {
+
+            // temporary gaurd, needed until this API story is fixed:
+            // https://www.pivotaltracker.com/story/show/26584957
+            if (!this.errorData || !this.errorData.instanceId) return;
+
+            return new chorus.models.Instance({
+                id: this.errorData.instanceId,
+                name: this.errorData.instanceName
+            });
+        }
     }
 };
 
 chorus.Mixins.InstanceCredentials.page = {
     requiredResourcesFetchFailed: function(resource) {
-        if (_.isFunction(resource.needsInstanceCredentials) && resource.needsInstanceCredentials()) {
-            var dialog = new chorus.dialogs.InstanceAccount({ title: t("instances.account.add.title"), instance: this.instance, reload: true });
+        var instance = resource.instanceRequiringCredentials && resource.instanceRequiringCredentials();
+        if (instance) {
+            var dialog = new chorus.dialogs.InstanceAccount({
+                title: t("instances.account.add.title"),
+                instance: instance,
+                reload: true
+            });
             dialog.launchModal();
         } else {
             this._super("requiredResourcesFetchFailed", arguments);
