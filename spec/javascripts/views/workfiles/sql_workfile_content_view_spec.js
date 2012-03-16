22112ea0d03a8632c79b16c5679fd097ad7e2dc1
Jacob Maine & Max Brunsfeld <pair+jacob+mbrunsfeld@pivotallabs.com>
2012-03-16 15:01:07 -0700

    On show page for sql workfile with no schema, fix error when ctrl-r is pressed

--- spec/javascripts/views/workfiles/sql_workfile_content_view_spec.js
+++ spec/javascripts/views/workfiles/sql_workfile_content_view_spec.js
@@ -55,110 +55,120 @@ describe("chorus.views.SqlWorkfileContentView", function() {
             })
 
             describe("running in the default schema", function() {
-                beforeEach(function() {
-                    this.executionInfo = {
-                        instanceId: '4',
-                        instanceName: "ned",
-                        databaseId: '5',
-                        databaseName: "rob",
-                        schemaId: '6',
-                        schemaName: "louis"
-                    }
-
-                    chorus.PageEvents.broadcast("file:runCurrent");
-                })
-
-                it("creates a task with the right parameters", function() {
-                    expect(this.view.task.get("sql")).toBe("select * from foos");
-                    expect(this.view.task.get("instanceId")).toBe("2");
-                    expect(this.view.task.get("databaseId")).toBe("3");
-                    expect(this.view.task.get("schemaId")).toBe("4");
-                    expect(this.view.task.get("entityId")).toBe(this.workfile.get("id"));
-                    expect(this.view.task.has("checkId")).toBeTruthy();
-                });
-
-                it("saves the task", function() {
-                    expect(this.server.creates().length).toBe(1);
-                    expect(this.server.lastCreate().url).toBe(this.view.task.url());
-                });
-
-                it("broadcasts file:executionStarted", function() {
-                    expect(chorus.PageEvents.broadcast).toHaveBeenCalledWith("file:executionStarted", jasmine.any(chorus.models.SqlExecutionTask));
-                })
-
-                describe("when the task completes successfully", function() {
+                context("when the workfile has a schema in which to execute", function() {
                     beforeEach(function() {
-                        this.server.lastCreate().succeed([
-                            {
-                                id: "10100",
-                                state: "success",
-                                result: {
-                                    message: "hi there"
-                                },
-                                executionInfo: this.executionInfo
-                            }
-                        ]);
+                        this.executionInfo = {
+                            instanceId: '4',
+                            instanceName: "ned",
+                            databaseId: '5',
+                            databaseName: "rob",
+                            schemaId: '6',
+                            schemaName: "louis"
+                        }
+
+                        chorus.PageEvents.broadcast("file:runCurrent");
                     })
 
-                    it("broadcasts file:executionSucceeded", function() {
-                        expect(chorus.PageEvents.broadcast).toHaveBeenCalledWith("file:executionSucceeded", jasmine.any(chorus.models.SqlExecutionTask));
+                    it("creates a task with the right parameters", function() {
+                        expect(this.view.task.get("sql")).toBe("select * from foos");
+                        expect(this.view.task.get("instanceId")).toBe("2");
+                        expect(this.view.task.get("databaseId")).toBe("3");
+                        expect(this.view.task.get("schemaId")).toBe("4");
+                        expect(this.view.task.get("entityId")).toBe(this.workfile.get("id"));
+                        expect(this.view.task.has("checkId")).toBeTruthy();
                     });
 
-                    it("sets the executing property to false", function() {
-                        expect(this.view.executing).toBeFalsy();
+                    it("saves the task", function() {
+                        expect(this.server.creates().length).toBe(1);
+                        expect(this.server.lastCreate().url).toBe(this.view.task.url());
                     });
 
-                    it("broadcasts workfile:executed", function() {
-                        expect(chorus.PageEvents.broadcast).toHaveBeenCalledWith("workfile:executed", this.workfile, this.executionInfo);
+                    it("broadcasts file:executionStarted", function() {
+                        expect(chorus.PageEvents.broadcast).toHaveBeenCalledWith("file:executionStarted", jasmine.any(chorus.models.SqlExecutionTask));
                     })
 
-                    describe("executing again", function() {
+                    describe("when the task completes successfully", function() {
                         beforeEach(function() {
-                            chorus.PageEvents.broadcast("file:runCurrent");
+                            this.server.lastCreate().succeed([
+                                {
+                                    id: "10100",
+                                    state: "success",
+                                    result: {
+                                        message: "hi there"
+                                    },
+                                    executionInfo: this.executionInfo
+                                }
+                            ]);
                         })
 
-                        it("executes the task again", function() {
-                            expect(this.server.creates().length).toBe(2);
+                        it("broadcasts file:executionSucceeded", function() {
+                            expect(chorus.PageEvents.broadcast).toHaveBeenCalledWith("file:executionSucceeded", jasmine.any(chorus.models.SqlExecutionTask));
+                        });
+
+                        it("sets the executing property to false", function() {
+                            expect(this.view.executing).toBeFalsy();
+                        });
+
+                        it("broadcasts workfile:executed", function() {
+                            expect(chorus.PageEvents.broadcast).toHaveBeenCalledWith("workfile:executed", this.workfile, this.executionInfo);
+                        })
+
+                        describe("executing again", function() {
+                            beforeEach(function() {
+                                chorus.PageEvents.broadcast("file:runCurrent");
+                            })
+
+                            it("executes the task again", function() {
+                                expect(this.server.creates().length).toBe(2);
+                            })
                         })
                     })
-                })
 
-                describe("when the task completion fails", function() {
-                    beforeEach(function() {
-                        this.server.lastCreate().fail("it broke", [ { executionInfo: this.executionInfo }]);
+                    describe("when the task completion fails", function() {
+                        beforeEach(function() {
+                            this.server.lastCreate().fail("it broke", [ { executionInfo: this.executionInfo }]);
+                        })
+
+                        it("broadcasts file:executionFailed", function() {
+                            expect(chorus.PageEvents.broadcast).toHaveBeenCalledWith("file:executionFailed", jasmine.any(chorus.models.SqlExecutionTask), jasmine.any(Object));
+                        });
+
+                        it("sets the executing property to false", function() {
+                            expect(this.view.executing).toBeFalsy();
+                        });
+
+                        it("broadcasts workfile:executed", function() {
+                            expect(chorus.PageEvents.broadcast).toHaveBeenCalledWith("workfile:executed", this.workfile, this.executionInfo);
+                        })
                     })
 
-                    it("broadcasts file:executionFailed", function() {
-                        expect(chorus.PageEvents.broadcast).toHaveBeenCalledWith("file:executionFailed", jasmine.any(chorus.models.SqlExecutionTask), jasmine.any(Object));
-                    });
+                    describe("when the task is cancelled", function() {
+                        beforeEach(function() {
+                            chorus.PageEvents.broadcast.reset();
+                            this.server.lastCreate().fail(fixtures.serverErrors({message: 'The task is cancelled'}), []);
+                        })
 
-                    it("sets the executing property to false", function() {
-                        expect(this.view.executing).toBeFalsy();
-                    });
+                        it("broadcasts file:executionFailed", function() {
+                            expect(chorus.PageEvents.broadcast).toHaveBeenCalledWith("file:executionFailed", jasmine.any(chorus.models.SqlExecutionTask), jasmine.any(Object));
+                        });
 
-                    it("broadcasts workfile:executed", function() {
-                        expect(chorus.PageEvents.broadcast).toHaveBeenCalledWith("workfile:executed", this.workfile, this.executionInfo);
+                        it("sets the executing property to false", function() {
+                            expect(this.view.executing).toBeFalsy();
+                        });
+
+                        it("does not broadcast workfile:executed", function() {
+                            expect(chorus.PageEvents.broadcast.callCount).toBe(1);
+                        })
                     })
-                })
+                });
 
-                describe("when the task is cancelled", function() {
-                    beforeEach(function() {
-                        chorus.PageEvents.broadcast.reset();
-                        this.server.lastCreate().fail(fixtures.serverErrors({message: 'The task is cancelled'}), []);
-                    })
-
-                    it("broadcasts file:executionFailed", function() {
-                        expect(chorus.PageEvents.broadcast).toHaveBeenCalledWith("file:executionFailed", jasmine.any(chorus.models.SqlExecutionTask), jasmine.any(Object));
+                context("when the workfile has no schema in which to execute", function() {
+                    it("does nothing when ctrl-r is pressed", function() {
+                        this.workfile.executionSchema.andReturn(undefined);
+                        // should not raise error
+                        chorus.PageEvents.broadcast("file:runCurrent");
                     });
-
-                    it("sets the executing property to false", function() {
-                        expect(this.view.executing).toBeFalsy();
-                    });
-
-                    it("does not broadcast workfile:executed", function() {
-                        expect(chorus.PageEvents.broadcast.callCount).toBe(1);
-                    })
-                })
+                });
             });
 
             context("when an execution is already outstanding", function() {
