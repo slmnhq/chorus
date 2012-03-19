19de30154d79e385edb12dde61be3ee03e6059f3
Danny Burkes <pair+danny@pivotallabs.com>
2012-03-19 10:05:47 -0700

    new help drop

---
+++ public/help/Data/Alias.js
@@ -0,0 +1,18 @@
+CMCXmlParser._FilePathToXmlStringMap.Add(
+	'Alias',
+	'<?xml version=\"1.0\" encoding=\"utf-8\"?>' +
+	'<CatapultAliasFile DefaultSkinName=\"NewSkin\">' +
+	'    <!-- saved from url=(0014)about:internet -->' +
+	'    <Map Name=\"workfiles\" Link=\"workfiles.htm\" ResolvedId=\"1008\" />' +
+	'    <Map Name=\"workspaces\" Link=\"workspaces.htm\" ResolvedId=\"1010\" />' +
+	'    <Map Name=\"workspace_summary\" Link=\"workspace_summary.htm\" ResolvedId=\"1009\" />' +
+	'    <Map Name=\"user\" Link=\"user.htm\" ResolvedId=\"1007\" />' +
+	'    <Map Name=\"user_edit\" Link=\"user_edit.htm\" ResolvedId=\"1006\" />' +
+	'    <Map Name=\"instances\" Link=\"instances.htm\" ResolvedId=\"1004\" />' +
+	'    <Map Name=\"getting_started\" Link=\"getting_started.htm\" ResolvedId=\"1003\" />' +
+	'    <Map Name=\"datasets\" Link=\"datasets.htm\" ResolvedId=\"1002\" />' +
+	'    <Map Name=\"dataset\" Link=\"dataset.htm\" ResolvedId=\"1001\" />' +
+	'    <Map Name=\"dashboard\" Link=\"dashboard.htm\" ResolvedId=\"1000\" />' +
+	'    <Map Name=\"search\" Link=\"search.htm\" ResolvedId=\"1005\" />' +
+	'</CatapultAliasFile>'
+);
