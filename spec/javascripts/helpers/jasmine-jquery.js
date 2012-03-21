a9e74430a47e22e3b06a327002b67892d57f2577
Bleicke Petersen & Max Brunsfeld <pair+bpetersen+mbrunsfeld@pivotallabs.com>
2012-03-21 15:44:40 -0700

    Revert "Only add Jasmine matchers once per suite, instead of once per spec."
    This reverts commit c1bd3793fc017e7e0bbcb60beaf3824269d630e5.

--- spec/javascripts/helpers/jasmine-jquery.js
+++ spec/javascripts/helpers/jasmine-jquery.js
@@ -255,6 +255,17 @@ jasmine.JQuery.matchersClass = {};
 
 beforeEach(function() {
   this.addMatchers(jasmine.JQuery.matchersClass);
+  this.addMatchers({
+    toHaveBeenTriggeredOn: function(selector) {
+      this.message = function() {
+        return [
+          "Expected event " + this.actual + " to have been triggered on" + selector,
+          "Expected event " + this.actual + " not to have been triggered on" + selector
+        ];
+      };
+      return jasmine.JQuery.events.wasTriggered(selector, this.actual);
+    }
+  })
 });
 
 afterEach(function() {
