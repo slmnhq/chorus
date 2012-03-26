402823a0f58d4159995a2a3318ebac3fb6ca1a0d
Danny Burkes <pair+danny@pivotallabs.com>
2012-03-26 16:53:17 -0700

    Creating a Greenplum Instance and Hadoop Instance selenium Tests

---
+++ spec/integration/create_instance_spec.rb
@@ -0,0 +1,36 @@
+require File.join(File.dirname(__FILE__), 'spec_helper')
+
+describe " add an instance " do 
+  before(:each) do
+    login('edcadmin', 'secret')
+  end  
+
+     it "creates an instance" do
+      page.find("a.add.dialog").click
+      within("#facebox") do
+       choose("register_existing_greenplum")
+       fill_in 'name', :with => "GPDB_inst_sel_test#{Time.now.to_i}"
+       fill_in 'description', :with => "GPDB instance creation"
+       fill_in 'host', :with => "gillette.sf.pivotallabs.com"
+       fill_in 'port', :with => "5432"
+       fill_in 'dbUserName', :with => "gpadmin"
+       fill_in 'dbPassword', :with => "secret"
+       check("register_greenplum_shared")
+       find(".submit").click
+      end
+     end
+
+     it "creates an Hadoop Instance" do
+      page.find("a.add.dialog").click 
+      within("#facebox") do
+       choose("register_existing_hadoop")
+       fill_in 'name', :with => "Hadoop_inst_sel_test#{Time.now.to_i}"
+       fill_in 'description', :with => "Hadoop Instance Creation"
+       fill_in 'host', :with => "gillette.sf.pivotallabs.com"
+       fill_in 'port', :with => "8020"
+       fill_in 'userName', :with => "hadoop"	
+       fill_in 'userGroups', :with => "hadoop"
+       find(".submit").click
+      end
+     end
+end
