0f9fd4d8ad3bd65d87696a251a99ed06aaffba73
Pushpavaneswaran <pushpm@usenpushpmmbp.corp.emc.com>
2012-03-27 13:42:12 -0700

    ruby selenium test for deleting a workspace

---
+++ spec/integration/delete_workspace_spec.rb
@@ -0,0 +1,13 @@
+require File.join(File.dirname(__FILE__), 'spec_helper')
+
+describe "deleting a workspace" do
+  before(:each) do
+    login('edcadmin', 'secret')
+    create_valid_workspace
+  end
+  it "deletes the workspace" do
+    click_link "Delete this Workspace"
+    find(".submit").click
+  end
+end
+
