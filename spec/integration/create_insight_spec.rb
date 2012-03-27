b227df9875c6d84b707bd281ac5c9101f0eb2c95
Pushpavaneswaran <pushpm@usenpushpmmbp.corp.emc.com>
2012-03-27 14:07:43 -0700

    ruby selenium test for adding an insight

---
+++ spec/integration/create_insight_spec.rb
@@ -0,0 +1,16 @@
+require File.join(File.dirname(__FILE__), 'spec_helper')
+require 'tempfile'
+
+describe "creating a note on a workspace" do
+  before(:each) do
+    login('edcadmin', 'secret')
+    create_valid_workspace
+    wait_until { page.find('a[data-dialog="InsightsNew"]').text == "Add an insight"}
+    click_link "Add an insight"
+    wait_until { page.find("#facebox .dialog h1").text == "Add an Insight" }
+  end
+  it "creates an insight" do
+    set_cleditor_value("body", "This is adding an Insight")
+    find(".submit").click
+  end
+end
