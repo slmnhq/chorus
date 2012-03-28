d7d821e2739705304fb54f14fa991abf20459bfe
Lenny Turetsky & Mark Rushakoff <pair+lenny+mrushakoff@pivotallabs.com>
2012-03-28 16:46:43 -0700

    WIP let user modify SQL before saving Chorus View [#26775087]

--- public/js/dialogs/tabular_data/name_chorus_view_dialog.js
+++ public/js/dialogs/tabular_data/name_chorus_view_dialog.js
@@ -16,12 +16,40 @@ chorus.dialogs.NameChorusView = chorus.dialogs.Base.extend({
         this.bindings.add(this.model, "validationFailed", this.chorusViewFailed);
     },
 
+    postRender: function() {
+        var opts = {
+            lineNumbers: true,
+            mode: "text/x-sql",
+            fixedGutter: true,
+            theme: "default",
+            lineWrapping: true
+        };
+
+        _.defer(_.bind(function() {
+            var textArea = this.$("textarea.sql_preview")[0];
+            if (textArea !== this.textArea) {
+                this.textArea = textArea;
+                this.editor = CodeMirror.fromTextArea(this.textArea, opts);
+
+                var ed = this.editor;
+                _.defer(function() {
+                    ed.refresh();
+                    ed.refresh();
+                    ed.refresh();
+                });
+
+                this.setupScrolling(this.$(".container"));
+            }
+        }, this));
+    },
+
     createChorusView: function(e) {
         e.preventDefault();
 
         this.model.set({
-            objectName: this.$("input[name=objectName]").val().trim()
-        })
+            objectName: this.$("input[name=objectName]").val().trim(),
+            query: this.editor.getValue()
+        });
 
         this.$("button.submit").startLoading("actions.creating")
         this.model.save();
