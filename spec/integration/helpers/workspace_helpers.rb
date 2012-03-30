9b897e413235a814720f2b7fbd9e1940d5901768
Bleicke Petersen & Charles Hansen <pair+bpetersen+chansen@pivotallabs.com>
2012-03-30 12:19:43 -0700

    Fix 'create_valid_workspace' rspec helper that broke when workspace quickstart page was added

--- spec/integration/helpers/workspace_helpers.rb
+++ spec/integration/helpers/workspace_helpers.rb
@@ -7,5 +7,7 @@ def create_valid_workspace(name=nil)
         fill_in 'name', :with => name
         click_button "Create Workspace"
     end
+    wait_until { current_route =~ /workspaces\/\d+\/quickstart/ && page.has_selector?("a.dismiss")}
+    click_link "Dismiss the workspace quick start guide"
     wait_until { current_route =~ /workspaces\/\d+/ && page.has_selector?("a[data-dialog=NotesNew]")}
 end
