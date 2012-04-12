05244de56267bed647f83adf222023c0349dfada
Bleicke Petersen & Jacob Maine <pair+bpetersen+jacob@pivotallabs.com>
2012-04-12 14:28:17 -0700

    initial commit

---
+++ Gemfile
@@ -0,0 +1,23 @@
+source :rubygems
+
+gem 'rails', '3.2.3'
+
+gem 'activerecord-jdbcpostgresql-adapter'
+
+gem 'jruby-openssl'
+gem 'json'
+
+group :assets do
+  # gem 'sass-rails',   '~> 3.2.3'
+  # gem 'coffee-rails', '~> 3.2.1'
+
+  # # See https://github.com/sstephenson/execjs#readme for more supported runtimes
+  # gem 'therubyrhino'
+
+  # gem 'uglifier', '>= 1.0.3'
+end
+
+# gem 'jquery-rails'
+
+# To use ActiveModel has_secure_password
+# gem 'bcrypt-ruby', '~> 3.0.0'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chorus::Application.config.secret_token = 'ec49c14149b35182a0a2bbb1479924011506174e2f97a635b6fbdf9ad34a4f054551b6df09132166536413a967d78fd4fec7fd0469c99430ee0b72063b881194'
