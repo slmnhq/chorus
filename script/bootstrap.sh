f4e0922e5945c155ed3e6f63899cc4789ade1eed
Charles Hansen & Jacob Maine <pair+chansen+jacob@pivotallabs.com>
2012-04-17 10:15:47 -0700

    document how to upgrade rvm, if there are problems installing ruby

--- script/bootstrap.sh
+++ script/bootstrap.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 ruby_version="1.9.3-p125"
+# set -e
 
-set -e
+update_rvm() {
+  curl -L get.rvm.io | bash -s stable
+  rvm reload
+}
 
 install_ruby_for_chorus() {
   echo "installing ruby"
+  export CC=/usr/bin/gcc
   rvm install $ruby_version
 }
 
@@ -14,11 +19,15 @@ install_bundler_for_chorus() {
   gem install bundler
 }
 
+source "$HOME/.rvm/scripts/rvm"
+
+# uncomment, if you think you need it
+# update_rvm
+
 echo "checking for ruby"
 rvm list | grep $ruby_version || install_ruby_for_chorus
 
 echo "checking for bundler"
-source "$HOME/.rvm/scripts/rvm"
 rvm $ruby_version
 gem list | grep bundler || install_bundler_for_chorus
 bundle install
