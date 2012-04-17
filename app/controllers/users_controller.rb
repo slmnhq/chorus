fa8f97986809d76d23ec5e53a23c5943318cfabb
Danny Burkes <pair+danny@pivotallabs.com>
2012-04-17 15:09:10 -0700

    require_admin before filter

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,12 @@
 class UsersController < ApplicationController
+  before_filter :require_admin, :only => :create
   def index
     render :json => UserPresenter.present_collection(User.order("LOWER(#{params[:order]})").paginate(params.slice(:page, :per_page)))
   end
 
   def create
-    if current_user.admin?
-      user = User.create! params.except(:controller, :action)
-      render :json => UserPresenter.present(user), :status => :created
-    else
-      render :status => :unauthorized, :json => "Only admins can do that"
-    end
+    user = User.create! params.except(:controller, :action)
+    render :json => UserPresenter.present(user), :status => :created
   rescue ActiveRecord::RecordInvalid => e
     render :json => { :errors => { :fields => e.record.errors } },
            :status => :unprocessable_entity
