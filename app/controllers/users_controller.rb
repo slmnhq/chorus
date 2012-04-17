65075145207dd27ae448f2043975f02a1879ffe8
Danny Burkes & Edgar Gonzalez <pair+danny+edgar@pivotallabs.com>
2012-04-17 10:05:08 -0700

    User collection can be paginated [fixes #27928723]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,11 @@ class UsersController < ApplicationController
   def index
     if session[:user_id] # should be converted to logged_in? or current_user, and then to a before filter
       params[:order] ||= "first_name"
+      params[:page] ||= 1
+      params[:per_page] ||= 50
       order_white_list = ["first_name", "last_name"]
       if (order_white_list.include?(params[:order]))
-        render :json => UserPresenter.present_collection(User.order("LOWER(#{params[:order]})"))
+        render :json => UserPresenter.present_collection(User.order("LOWER(#{params[:order]})").paginate(:page => params[:page], :per_page => params[:per_page]))
       else
         head :bad_request
       end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -29,26 +29,62 @@ describe UsersController do
         response_object.length.should == 2
       end
 
-      it "sorts by first name" do
-        get :index
-        response_object = JSON.parse(response.body)["response"]
-        response_object.first["username"].should == "other_user"
-        response_object.second["username"].should == "some_user"
-      end
-
-      context "with a recognized sort order" do
-        it "respects the sort order" do
-          get :index, :order => "last_name"
+      describe "sorting" do
+        it "sorts by first name" do
+          get :index
           response_object = JSON.parse(response.body)["response"]
-          response_object.first["username"].should == "some_user"
-          response_object.second["username"].should == "other_user"
+          response_object.first["username"].should == "other_user"
+          response_object.second["username"].should == "some_user"
+        end
+
+        context "with a recognized sort order" do
+          it "respects the sort order" do
+            get :index, :order => "last_name"
+            response_object = JSON.parse(response.body)["response"]
+            response_object.first["username"].should == "some_user"
+            response_object.second["username"].should == "other_user"
+          end
+        end
+
+        context "with an unrecognized sort order" do
+          it "fails" do
+            get :index, :order => "last_name; DROP TABLE users;"
+            response.code.should == "400"
+          end
         end
       end
 
-      context "with an unrecognized sort order" do
-        it "fails" do
-          get :index, :order => "last_name; DROP TABLE users;"
-          response.code.should == "400"
+      describe "pagination" do
+        before do
+          User.create! :username => 'third_user', :first_name => "zed", :last_name => "bob", :password => 'secret', :password_confirmation => 'secret'
+        end
+
+        it "paginates the collection" do
+          get :index, :page => 1, :per_page => 2
+          response_object = JSON.parse(response.body)["response"]
+          response_object.length.should == 2
+        end
+
+        it "defaults to page one" do
+          get :index, :per_page => 2
+          response_object = JSON.parse(response.body)["response"]
+          response_object.length.should == 2
+          response_object.first["username"].should == "other_user"
+          response_object.second["username"].should == "some_user"
+        end
+
+        it "accepts a page parameter" do
+          get :index, :page => 2, :per_page => 2
+          response_object = JSON.parse(response.body)["response"]
+          response_object.length.should == 1
+          response_object.first["username"].should == "third_user"
+        end
+
+        it "defaults the per_page to fifty" do
+          (1..48).each { |n| User.create! :username => "user#{n}", :first_name => "User #{n}", :last_name => "Last #{n}", :password => 'secret', :password_confirmation => 'secret' }
+          get :index
+          response_object = JSON.parse(response.body)["response"]
+          response_object.length.should == 50
         end
       end
     end
