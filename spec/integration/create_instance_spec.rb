5d0129add90101dc4c62e6e31c9257e7f604e07c
Bleicke Petersen & Max Brunsfeld <pair+bpetersen+mbrunsfeld@pivotallabs.com>
2012-04-18 15:55:44 -0700

    In create instance integration spec, scope input selectors to particular fieldsets

--- spec/integration/create_instance_spec.rb
+++ spec/integration/create_instance_spec.rb
@@ -3,36 +3,44 @@ require File.join(File.dirname(__FILE__), 'spec_helper')
 describe " add an instance " do
   before(:each) do
     login('edcadmin', 'secret')
+    page.find("a.add.dialog").click
   end
 
-     it "creates an instance" do
-      page.find("a.add.dialog").click
-      within("#facebox") do
-       choose("register_existing_greenplum")
-       wait_until { page.has_selector?(".register_existing_greenplum input[name=name]")}
-       fill_in 'name', :with => "GPDB_inst_sel_test#{Time.now.to_i}"
-       fill_in 'description', :with => "GPDB instance creation"
-       fill_in 'host', :with => "gillette.sf.pivotallabs.com"
-       fill_in 'port', :with => "5432"
-       fill_in 'dbUserName', :with => "gpadmin"
-       fill_in 'dbPassword', :with => "secret"
-       check("register_greenplum_shared")
-       find(".submit").click
-      end
-     end
+  it "creates an instance" do
+    within("#facebox") do
+      choose("register_existing_greenplum")
+      wait_until { page.has_selector?(".register_existing_greenplum input[name=name]")}
 
-     it "creates an Hadoop Instance" do
-      page.find("a.add.dialog").click
-      within("#facebox") do
-       choose("register_existing_hadoop")
-       wait_until { page.has_selector?(".register_existing_hadoop input[name=name]")}
-       fill_in 'name', :with => "Hadoop_inst_sel_test#{Time.now.to_i}"
-       fill_in 'description', :with => "Hadoop Instance Creation"
-       fill_in 'host', :with => "gillette.sf.pivotallabs.com"
-       fill_in 'port', :with => "8020"
-       fill_in 'userName', :with => "hadoop"
-       fill_in 'userGroups', :with => "hadoop"
-       find(".submit").click
+      within(".register_existing_greenplum") do
+        fill_in 'name', :with => "GPDB_inst_sel_test#{Time.now.to_i}"
+        fill_in 'description', :with => "GPDB instance creation"
+        fill_in 'host', :with => "gillette.sf.pivotallabs.com"
+        fill_in 'port', :with => "5432"
+        fill_in 'dbUserName', :with => "gpadmin"
+        fill_in 'dbPassword', :with => "secret"
+
+        check("register_greenplum_shared")
       end
-     end
+
+      find(".submit").click
+    end
+  end
+
+  it "creates an Hadoop Instance" do
+    within("#facebox") do
+      choose("register_existing_hadoop")
+      wait_until { page.has_selector?(".register_existing_hadoop input[name=name]")}
+
+      within(".register_existing_hadoop") do
+        fill_in 'name', :with => "Hadoop_inst_sel_test#{Time.now.to_i}"
+        fill_in 'description', :with => "Hadoop Instance Creation"
+        fill_in 'host', :with => "gillette.sf.pivotallabs.com"
+        fill_in 'port', :with => "8020"
+        fill_in 'userName', :with => "hadoop"
+        fill_in 'userGroups', :with => "hadoop"
+      end
+
+      find(".submit").click
+    end
+  end
 end
