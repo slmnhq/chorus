6053302e1b9bc4684fbab9eff4b0f37a7b786c63
Danny Burkes <pair+danny@pivotallabs.com>
2012-04-18 11:37:44 -0700

    Remove debugging command

--- cruise.sh
+++ cruise.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 source "$HOME/.rvm/scripts/rvm"
-rvm use 1.9.3-p125
 ruby -v
-gem list
 bundle install
 rake legacy:setup db:migrate db:test:prepare db:test:prepare:legacy
 script/test
