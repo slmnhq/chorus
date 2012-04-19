18aef765c743ee552c7b2cee7b3b024745df706e
ci <ci@greenplum-ci.sf.pivotallabs.com>
2012-04-19 15:50:42 -0700

    version formats differ slightly

--- script/bootstrap.sh
+++ script/bootstrap.sh
@@ -48,7 +48,9 @@ set -e
 
 echo "***** checking for ruby"
 rbenv versions | grep $ruby_version || install_ruby_for_chorus
-ruby -v | grep $ruby_version
+echo "***** ensuring ruby $ruby_version"
+ruby -v
+rbenv version | grep $ruby_version
 
 echo "***** checking for bundler"
 gem list | grep bundler || install_bundler_for_chorus
