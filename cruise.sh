66df1887191fcadd3d360d0a7dd4cd09b5dee5f5
Charles Hansen & Jacob Maine <pair+chansen+jacob@pivotallabs.com>
2012-04-19 16:11:13 -0700

    try to load correct ruby

--- cruise.sh
+++ cruise.sh
@@ -3,7 +3,8 @@
 set -e
 set -x
 
-export RBENV_VERSION=`cat .rbenv-version`
+rbenv init -
+rbenv shell `cat .rbenv-version`
 ruby -v | grep 1.9.3
 bundle install
 rake legacy:setup db:migrate db:test:prepare db:test:prepare:legacy
