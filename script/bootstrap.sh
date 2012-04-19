91e2d03ff5f0c89d7d06f9b85bbf93a8c9a5142d
Jacob Maine <pair+jacob@pivotallabs.com>
2012-04-19 09:15:43 -0700

    Switch back to rbenv, hoping to fix CI

--- script/bootstrap.sh
+++ script/bootstrap.sh
@@ -1,35 +1,61 @@
 #!/bin/bash
 
 ruby_version="1.9.3-p125"
-# set -e
 
-update_rvm() {
-  curl -L get.rvm.io | bash -s stable
-  rvm reload
+install_ruby_build() {
+  plugins_dir=$HOME/.rbenv/plugins
+  if [ ! -e $plugins_dir/ruby-build ]
+  then
+    (
+      mkdir -p $plugins_dir
+      cd $plugins_dir
+      git clone git://github.com/sstephenson/ruby-build.git
+    )
+  fi
 }
+
+brew update
+brew install rbenv
+install_ruby_build
+
+set -e
 
 install_ruby_for_chorus() {
   echo "installing ruby"
   export CC=/usr/bin/gcc
-  rvm install $ruby_version
+  rbenv install $ruby_version
+  rbenv rehash
 }
 
 install_bundler_for_chorus() {
   echo "installing bundler"
   gem install bundler
+  rbenv rehash
 }
 
-source "$HOME/.rvm/scripts/rvm"
+add_rbenv_to_bash() {
+    rbenv_config="$HOME/.bash_profile_includes/rbenv.sh"
+    if [ ! -e "$rbenv_config" ]
+    then
+        cat > $rbenv_config <<EOF
+    #/bin/bash
 
-# uncomment, if you think you need it
-# update_rvm
+    eval "\$(rbenv init -)"
+    EOF
+
+      echo "Added rbenv init to bash profile includes"
+    fi
+}
 
 echo "checking for ruby"
-rvm list | grep $ruby_version || install_ruby_for_chorus
+rbenv versions | grep $ruby_version || install_ruby_for_chorus
+rbenv local $ruby_version
 
 echo "checking for bundler"
-rvm $ruby_version
 gem list | grep bundler || install_bundler_for_chorus
+
+echo "add rbenv to bash"
+add_rbenv_to_bash
 
 echo "setting up project"
 bundle install
