328eb8bcf93d7386a7923d8de4f937e307652680
Charles Hansen & Kris Kelly <pair+chansen+kris@pivotallabs.com>
2012-04-25 11:17:40 -0700

    Switched to Handlebars.js library from handlebars_assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,7 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require handlebars.runtime
+//= require handlebars
 //= require_tree ./templates
 //= require vendor/jquery-1.7.1
 //= require vendor/underscore-1.2.1
