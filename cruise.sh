316c4342ff505acbec60da0742ec078104c158e2
Corina Lawu & Erich Douglass <pair+corina+edouglass@pivotallabs.com>
2012-04-27 12:24:10 -0700

    Make sure Jasmine is running before tests are kicked off.

--- cruise.sh
+++ cruise.sh
@@ -9,6 +9,13 @@ gem list bundler | grep bundler || gem install bundler
 bundle install
 rake legacy:setup db:migrate db:test:prepare db:test:prepare:legacy
 
+# Run rspec tests
 script/test  > $CC_BUILD_ARTIFACTS/rspec_tests.log 2>&1
 
+# Start Jasmine
+bundle exec rake jasmine > $CC_BUILD_ARTIFACTS/jasmine.log 2>&1 &
+JASMINE_PID=$!; sleep 5
+
+# Run Jasmine tests
 rake phantom > $CC_BUILD_ARTIFACTS/jasmine_tests.log 2>&1
+kill -9 $JASMINE_PID || true
