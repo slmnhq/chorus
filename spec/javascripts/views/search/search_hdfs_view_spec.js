70e5d274a0df3e3b7fabc22fb19dd66c19078ed7
Corina Lawu & Erich Douglass <pair+corina+edouglass@pivotallabs.com>
2012-04-27 17:37:16 -0700

    Add highlighting to hdfs path segments that match the search query [completes #28250795]

--- spec/javascripts/views/search/search_hdfs_view_spec.js
+++ spec/javascripts/views/search/search_hdfs_view_spec.js
@@ -1,4 +1,56 @@
 describe("chorus.views.SearchHdfs", function() {
+    describe("getHighlightedPathSegments", function() {
+        context("when there are no search results", function() {
+            beforeEach(function() {
+                this.result = fixtures.searchResult({hdfs: {
+                    docs: [
+                        fixtures.searchResultHdfsJson({
+                            path: "/aaa/bbb",
+                            isDir: false,
+                            isBinary: false
+                        })
+                    ],
+                    numFound: 10}
+                });
+
+                this.model = this.result.hdfs().models[0];
+                this.view = new chorus.views.SearchHdfs({model: this.model});
+                this.view.render()
+            });
+
+            it("should return the path segments without highlighting", function() {
+                expect(this.view.getHighlightedPathSegments()).toEqual(["aaa", "bbb"]);
+            });
+        });
+
+        context("when there are multiple search results", function() {
+            beforeEach(function() {
+                this.result = fixtures.searchResult({hdfs: {
+                    docs: [
+                        fixtures.searchResultHdfsJson({
+                            name: "bar",
+                            path: "/aaa/bbb/ccc",
+                            isDir: false,
+                            isBinary: false,
+                            highlightedAttributes: {
+                                path: ["/<em>aaa</em>/bbb/ccc"]
+                            }
+                        })
+                    ],
+                    numFound: 10}
+                });
+
+                this.model = this.result.hdfs().models[0];
+                this.view = new chorus.views.SearchHdfs({model: this.model});
+                this.view.render()
+            });
+
+            it("should return an array of path segments with highlighting", function() {
+                expect(this.view.getHighlightedPathSegments()).toEqual(["<em>aaa</em>", "bbb", "ccc"]);
+            });
+        });
+    });
+
     describe("when the file is not a binary file", function() {
         beforeEach(function() {
             this.result = fixtures.searchResult({hdfs: {
@@ -6,7 +58,10 @@ describe("chorus.views.SearchHdfs", function() {
                     fixtures.searchResultHdfsJson({
                         path: "/aaa/bbb",
                         isDir: false,
-                        isBinary: false
+                        isBinary: false,
+                        highlightedAttributes: {
+                            path: ["/<em>aaa</em>/bbb"]
+                        }
                     })
                 ],
                 numFound: 10}
@@ -36,14 +91,13 @@ describe("chorus.views.SearchHdfs", function() {
             var $links = this.view.$(".path_parts a");
             expect($links.length).toBe(2);
 
-            expect($links.eq(0).text()).toBe("aaa");
+            expect($links.eq(0).html()).toEqual("<em>aaa</em>");
             expect($links.eq(0).attr("href")).toBe("#/instances/10001/browse/aaa");
 
-            expect($links.eq(1).text()).toBe("bbb");
+            expect($links.eq(1).html()).toEqual("bbb");
             expect($links.eq(1).attr("href")).toBe("#/instances/10001/browse/aaa/bbb");
         });
-
-    })
+    });
 
     describe("when the file is a binary file", function() {
         beforeEach(function() {
@@ -85,5 +139,5 @@ describe("chorus.views.SearchHdfs", function() {
             expect($links.eq(1).text()).toBe("bbb");
             expect($links.eq(1).attr("href")).toBe("#/instances/10001/browse/aaa/bbb");
         });
-    })
+    });
 });
