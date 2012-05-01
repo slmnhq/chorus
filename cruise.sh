8fc830e2e1f9b0c334046a6a6f0c0dcf2c970ca2
Brandon Liu & Max Brunsfeld <pair+brandon+mbrunsfeld@pivotallabs.com>
2012-05-01 14:59:49 -0700

    Add one integration test to ci

--- cruise.sh
+++ cruise.sh
@@ -9,9 +9,12 @@ gem list bundler | grep bundler || gem install bundler
 bundle install
 rake legacy:setup db:migrate db:test:prepare db:test:prepare:legacy
 
-# Run rspec tests
-script/test  > $CC_BUILD_ARTIFACTS/rspec_tests.log 2>&1
+# Run rspec unit tests
+script/test > $CC_BUILD_ARTIFACTS/rspec_tests.log 2>&1
 
 # Run Jasmine tests
 RAILS_ENV=development rake devmode:enable assets:precompile
 rake jasmine:ci > $CC_BUILD_ARTIFACTS/jasmine_tests.log 2>&1
+
+# Run integration tests
+script/test spec/integration/new_user_spec.rb > $CC_BUILD_ARTIFACTS/integration_tests.log 2>&1
