8e909ea7823d43b6faef853223639dc79d889d91
Danny Burkes & Lenny Turetsky <pair+danny+lenny@pivotallabs.com>
2012-05-01 14:54:57 -0700

    pagination

--- app/presenters/presenter.rb
+++ app/presenters/presenter.rb
@@ -1,11 +1,14 @@
 class Presenter
   class ResponseWrapper
-    def initialize(response)
+    def initialize(response, pagination=nil)
       @response = response
+      @pagination = pagination
     end
 
     def as_json(options = {})
-      { :response => @response.as_json }
+      result = { :response => @response.as_json }
+      result[:pagination] = @pagination if @pagination
+      result
     end
   end
 
@@ -14,7 +17,12 @@ class Presenter
   end
 
   def self.present_collection(collection, view_context)
-    ResponseWrapper.new(collection.map { |model| new(model, view_context) })
+    ResponseWrapper.new(collection.map { |model| new(model, view_context) },
+                        (collection.respond_to?(:current_page) ? {
+                          :page => collection.current_page,
+                          :per_page => collection.per_page,
+                          :total => collection.total_entries
+                        } : nil))
   end
 
   def initialize(model, view_context)
--- spec/presenters/presenter_spec.rb
+++ spec/presenters/presenter_spec.rb
@@ -26,5 +26,21 @@ describe Presenter, :type => :view do
     it "serializes an array" do
       Presenter.present_collection([@user], view).as_json[:response].should be_a(Array)
     end
+
+    context "pagination" do
+      it "when un-paginated has no pagination info" do
+        models = User.order(:username)
+        Presenter.present_collection(models, view).as_json.should_not have_key(:pagination)
+      end
+
+      it "when paginated has pagination info" do
+        models = User.order(:username).paginate(:page => 1, :per_page => 2)
+        json = Presenter.present_collection(models, view).as_json
+        json.should have_key(:pagination)
+        json[:pagination][:page].should == 1
+        json[:pagination][:per_page].should == 2
+        json[:pagination][:total].should == User.count
+      end
+    end
   end
 end
