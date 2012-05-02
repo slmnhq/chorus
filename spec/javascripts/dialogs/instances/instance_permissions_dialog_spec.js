5baf6db95ca47085b7b690aed15afa03ae5e0007
Lenny Turetsky <pair+lenny@pivotallabs.com>
2012-05-02 09:31:24 -0700

    fix failing test

--- spec/javascripts/dialogs/instances/instance_permissions_dialog_spec.js
+++ spec/javascripts/dialogs/instances/instance_permissions_dialog_spec.js
@@ -421,7 +421,7 @@ describe("chorus.dialogs.InstancePermissions", function() {
                 });
 
                 it("should call delete on the accounts", function() {
-                    expect(this.server.lastDestroy().url).toBe("/instances/5/accounts/" + this.accountBeingRemoved.id + "?");
+                    expect(this.server.lastDestroy().url).toBe("/instances/" + this.instance.get("id") + "/accounts/" + this.accountBeingRemoved.id + "?");
                 });
 
                 context("when the delete succeeds", function() {
