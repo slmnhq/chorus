b4032bc7f2418d5993d27d3d32c4e3b2bf946c86
Corina Lawu & Max Brunsfeld <pair+corina+mbrunsfeld@pivotallabs.com>
2012-05-03 15:01:45 -0700

    Add image fixture, generated by rspec test [#27928725]

--- spec/controllers/images_controller_spec.rb
+++ spec/controllers/images_controller_spec.rb
@@ -1,12 +1,15 @@
 require "spec_helper"
 
 describe ImagesController do
+  before do
+    any_instance_of(User) do |user|
+      stub(user).save_attached_files { true }
+    end
+  end
+
   describe "#update" do
     before do
       @user = FactoryGirl.create(:user, :username => 'some_user', :first_name => "marty")
-      any_instance_of(User) do |user|
-        stub(user).save_attached_files { true }
-      end
       log_in @user
     end
 
@@ -22,6 +25,11 @@ describe ImagesController do
       @user.reload
       decoded_response.original.should == @user.image.url(:original)
       decoded_response.icon.should == @user.image.url(:icon)
+    end
+
+    it "generates image jasmine fixtures" do
+      put :update, :id => @user.id, :files => [Rack::Test::UploadedFile.new(File.expand_path("spec/fixtures/small.png", Rails.root), "image/jpeg")]
+      save_fixture "image.json"
     end
   end
 end
