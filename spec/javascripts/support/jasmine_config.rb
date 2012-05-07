30f1ad600fcd83194fefd99d11ff16d0099ecc2e
Brandon Liu & Nitin Grewal <pair+brandon+nitin@pivotallabs.com>
2012-05-07 11:39:16 -0700

    Fix typo in old jasmine runner

--- spec/javascripts/support/jasmine_config.rb
+++ spec/javascripts/support/jasmine_config.rb
@@ -3,7 +3,7 @@ module Jasmine
     def run(focused_suite = nil)
       jasmine_files = @jasmine_files
       css_files = @jasmine_stylesheets + (@config.css_files || [])
-      js_files = @config.src_files(focused_suite)
+      js_files = @config.js_files(focused_suite)
       body = ERB.new(File.read("spec/javascripts/support/old-jasmine-core/run.html.erb")).result(binding)
       [
         200,
