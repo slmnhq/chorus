51e6a9baee294a3fc131b1939bd45e05bd97259f
Jacob Maine & Manikandan Pushpavaneswaran <pair+jacob+manikandan@pivotallabs.com>
2012-05-07 10:38:04 -0700

    revert to having not-found render 200
    Otherwise, front-end required resources never end up loaded
    which means their views don't render.

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -6,6 +6,7 @@ class RootController < ApplicationController
   end
 
   def not_found
-    render :json => {:route => :not_found}, :status => :not_found
+    #render :json => {:route => :not_found}, :status => :not_found
+    render :json => {:route => :not_found}, :status => :ok # TODO: REWRITE: status OK is weird, but it keeps the front-end working.
   end
 end
--- spec/controllers/root_controller_spec.rb
+++ spec/controllers/root_controller_spec.rb
@@ -5,7 +5,8 @@ describe RootController do
       it "renders 'not found' JSON when record not found" do
         get :not_found
 
-        response.code.should == "404"
+        response.code.should == "200"
+        #response.code.should == "404" # See note in root_controller#not_found
         response.parsed_body['route'].should == "not_found"
       end
     end
