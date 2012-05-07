51ed0407b9a7c06364242af6151116fd6abff51d
Bleicke Petersen & Max Brunsfeld <pair+bpetersen+mbrunsfeld@pivotallabs.com>
2012-05-07 15:17:36 -0700

    Completely rewrite the jquery download plugin. [#28841453]
    It was dumb.

--- app/assets/javascripts/vendor/jquery.download.js
+++ app/assets/javascripts/vendor/jquery.download.js
@@ -1,28 +1,21 @@
 /*
- * --------------------------------------------------------------------
- * jQuery-Plugin - $.download - allows for simple get/post requests for files
- * by Scott Jehl, scott@filamentgroup.com
- * http://www.filamentgroup.com
- * reference article: http://www.filamentgroup.com/lab/jquery_plugin_for_requesting_ajax_like_file_downloads/
- * Copyright (c) 2008 Filament Group, Inc
- * Dual licensed under the MIT (filamentgroup.com/examples/mit-license.txt) and GPL (filamentgroup.com/examples/gpl-license.txt) licenses.
- * --------------------------------------------------------------------
+ *   based on a plugin found here:
+ *   http://www.filamentgroup.com
  */
- 
-jQuery.download = function(url, data, method){
-	//url and data options required
-	if( url && data ){ 
-		//data can be string of parameters or array/object
-		data = typeof data == 'string' ? data : jQuery.param(data);
-		//split params into form inputs
-		var inputs = '';
-		jQuery.each(data.split('&'), function(){ 
-			var pair = this.split('=');
-			inputs+='<input type="hidden" name="'+ pair[0] +'" value="'+ pair[1] +'" />'; 
-		});
-		//send request
-		jQuery('<form action="'+ url +'" method="'+ (method||'post') +'">'+inputs+'</form>')
-		.appendTo('body').submit().remove();
-	};
+
+jQuery.download = function(url, data, method) {
+    var form = $("<form/>").attr({
+        "action": url,
+        "method": method || "post"
+    });
+
+    $.each(data, function(key, val) {
+        form.append($("<input/>").attr({
+            "name": key,
+            "value": val
+        }));
+    });
+
+    form.appendTo("body").submit().remove();
 };
 
