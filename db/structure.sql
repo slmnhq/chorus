8cf65e19d3e0a8fda655aaffcafd9aa29c8bf086
Jacob Maine, Max Brunsfeld & Yaneeka Huq <pair+jacob+mbrunsfeld+yaneeka@pivotallabs.com>
2012-05-08 10:36:57 -0700

    Use sql dump instead of schema.rb

---
+++ db/structure.sql
@@ -0,0 +1,378 @@
+--
+-- PostgreSQL database dump
+--
+
+SET statement_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = off;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET escape_string_warning = off;
+
+--
+-- Name: plpgsql; Type: PROCEDURAL LANGUAGE; Schema: -; Owner: -
+--
+
+CREATE OR REPLACE PROCEDURAL LANGUAGE plpgsql;
+
+
+SET search_path = public, pg_catalog;
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- Name: queue_classic_jobs; Type: TABLE; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE TABLE queue_classic_jobs (
+    id integer NOT NULL,
+    q_name character varying(255),
+    method character varying(255),
+    args text,
+    locked_at timestamp with time zone
+);
+
+
+--
+-- Name: lock_head(character varying); Type: FUNCTION; Schema: public; Owner: -
+--
+
+CREATE FUNCTION lock_head(tname character varying) RETURNS SETOF queue_classic_jobs
+    LANGUAGE plpgsql
+    AS $_$
+BEGIN
+  RETURN QUERY EXECUTE 'SELECT * FROM lock_head($1,10)' USING tname;
+END;
+$_$;
+
+
+--
+-- Name: lock_head(character varying, integer); Type: FUNCTION; Schema: public; Owner: -
+--
+
+CREATE FUNCTION lock_head(q_name character varying, top_boundary integer) RETURNS SETOF queue_classic_jobs
+    LANGUAGE plpgsql
+    AS $_$
+DECLARE
+  unlocked integer;
+  relative_top integer;
+  job_count integer;
+BEGIN
+  -- The purpose is to release contention for the first spot in the table.
+  -- The select count(*) is going to slow down dequeue performance but allow
+  -- for more workers. Would love to see some optimization here...
+
+  EXECUTE 'SELECT count(*) FROM '
+    || '(SELECT * FROM queue_classic_jobs WHERE q_name = '
+    || quote_literal(q_name)
+    || ' LIMIT '
+    || quote_literal(top_boundary)
+    || ') limited'
+  INTO job_count;
+
+  SELECT TRUNC(random() * (top_boundary - 1))
+  INTO relative_top;
+
+  IF job_count < top_boundary THEN
+    relative_top = 0;
+  END IF;
+
+  LOOP
+    BEGIN
+      EXECUTE 'SELECT id FROM queue_classic_jobs '
+        || ' WHERE locked_at IS NULL'
+        || ' AND q_name = '
+        || quote_literal(q_name)
+        || ' ORDER BY id ASC'
+        || ' LIMIT 1'
+        || ' OFFSET ' || quote_literal(relative_top)
+        || ' FOR UPDATE NOWAIT'
+      INTO unlocked;
+      EXIT;
+    EXCEPTION
+      WHEN lock_not_available THEN
+        -- do nothing. loop again and hope we get a lock
+    END;
+  END LOOP;
+
+  RETURN QUERY EXECUTE 'UPDATE queue_classic_jobs '
+    || ' SET locked_at = (CURRENT_TIMESTAMP)'
+    || ' WHERE id = $1'
+    || ' AND locked_at is NULL'
+    || ' RETURNING *'
+  USING unlocked;
+
+  RETURN;
+END;
+$_$;
+
+
+--
+-- Name: instance_accounts; Type: TABLE; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE TABLE instance_accounts (
+    id integer NOT NULL,
+    db_username character varying(255),
+    db_password bytea,
+    instance_id integer,
+    owner_id integer
+);
+
+
+--
+-- Name: instance_credentials_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE instance_credentials_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: instance_credentials_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE instance_credentials_id_seq OWNED BY instance_accounts.id;
+
+
+--
+-- Name: instances; Type: TABLE; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE TABLE instances (
+    id integer NOT NULL,
+    name character varying(255),
+    description text,
+    host character varying(255),
+    port integer,
+    maintenance_db character varying(255),
+    created_at timestamp without time zone NOT NULL,
+    updated_at timestamp without time zone NOT NULL,
+    owner_id integer NOT NULL,
+    shared boolean DEFAULT false,
+    provision_type character varying(255),
+    instance_provider character varying(255),
+    state character varying(255) DEFAULT 'offline'::character varying,
+    version character varying(255)
+);
+
+
+--
+-- Name: instances_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE instances_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: instances_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE instances_id_seq OWNED BY instances.id;
+
+
+--
+-- Name: queue_classic_jobs_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE queue_classic_jobs_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: queue_classic_jobs_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE queue_classic_jobs_id_seq OWNED BY queue_classic_jobs.id;
+
+
+--
+-- Name: schema_migrations; Type: TABLE; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE TABLE schema_migrations (
+    version character varying(255) NOT NULL
+);
+
+
+--
+-- Name: users; Type: TABLE; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE TABLE users (
+    id integer NOT NULL,
+    username character varying(255),
+    password_digest character varying(255),
+    created_at timestamp without time zone NOT NULL,
+    updated_at timestamp without time zone NOT NULL,
+    first_name character varying(255),
+    last_name character varying(255),
+    email character varying(255),
+    title character varying(255),
+    dept character varying(255),
+    notes text,
+    admin boolean DEFAULT false,
+    deleted_at timestamp without time zone,
+    image_file_name character varying(255),
+    image_content_type character varying(255),
+    image_file_size integer,
+    image_updated_at timestamp without time zone
+);
+
+
+--
+-- Name: users_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE users_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: users_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE users_id_seq OWNED BY users.id;
+
+
+--
+-- Name: id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE instance_accounts ALTER COLUMN id SET DEFAULT nextval('instance_credentials_id_seq'::regclass);
+
+
+--
+-- Name: id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE instances ALTER COLUMN id SET DEFAULT nextval('instances_id_seq'::regclass);
+
+
+--
+-- Name: id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE queue_classic_jobs ALTER COLUMN id SET DEFAULT nextval('queue_classic_jobs_id_seq'::regclass);
+
+
+--
+-- Name: id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE users ALTER COLUMN id SET DEFAULT nextval('users_id_seq'::regclass);
+
+
+--
+-- Name: instance_credentials_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
+--
+
+ALTER TABLE ONLY instance_accounts
+    ADD CONSTRAINT instance_credentials_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: instances_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
+--
+
+ALTER TABLE ONLY instances
+    ADD CONSTRAINT instances_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: queue_classic_jobs_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
+--
+
+ALTER TABLE ONLY queue_classic_jobs
+    ADD CONSTRAINT queue_classic_jobs_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: users_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
+--
+
+ALTER TABLE ONLY users
+    ADD CONSTRAINT users_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: idx_qc_on_name_only_unlocked; Type: INDEX; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE INDEX idx_qc_on_name_only_unlocked ON queue_classic_jobs USING btree (q_name, id) WHERE (locked_at IS NULL);
+
+
+--
+-- Name: index_instances_on_owner_id; Type: INDEX; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE INDEX index_instances_on_owner_id ON instances USING btree (owner_id);
+
+
+--
+-- Name: unique_schema_migrations; Type: INDEX; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE UNIQUE INDEX unique_schema_migrations ON schema_migrations USING btree (version);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
+INSERT INTO schema_migrations (version) VALUES ('20120413003345');
+
+INSERT INTO schema_migrations (version) VALUES ('20120416234945');
+
+INSERT INTO schema_migrations (version) VALUES ('20120417103900');
+
+INSERT INTO schema_migrations (version) VALUES ('20120417105500');
+
+INSERT INTO schema_migrations (version) VALUES ('20120417184201');
+
+INSERT INTO schema_migrations (version) VALUES ('20120418000741');
+
+INSERT INTO schema_migrations (version) VALUES ('20120418211041');
+
+INSERT INTO schema_migrations (version) VALUES ('20120419193206');
+
+INSERT INTO schema_migrations (version) VALUES ('20120419194129');
+
+INSERT INTO schema_migrations (version) VALUES ('20120423191005');
+
+INSERT INTO schema_migrations (version) VALUES ('20120424221335');
+
+INSERT INTO schema_migrations (version) VALUES ('20120427234448');
+
+INSERT INTO schema_migrations (version) VALUES ('20120502001925');
+
+INSERT INTO schema_migrations (version) VALUES ('20120502012345');
+
+INSERT INTO schema_migrations (version) VALUES ('20120502205544');
+
+INSERT INTO schema_migrations (version) VALUES ('20120503221103');
+
+INSERT INTO schema_migrations (version) VALUES ('20120504210541');
+
+INSERT INTO schema_migrations (version) VALUES ('20120504214919');
+
+INSERT INTO schema_migrations (version) VALUES ('20120504235249');
