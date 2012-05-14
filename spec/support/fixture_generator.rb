1900029e2a68eae3920329c04f419dd76b0f51d5
Max Brunsfeld <pair+mbrunsfeld@pivotallabs.com>
2012-05-14 10:36:18 -0700

    Add optional argument to save_fixture helper method
    To save a fixture other than the 'response' key
    of the json body, pass the object as a second argument
    to 'save_fixture'

--- spec/support/fixture_generator.rb
+++ spec/support/fixture_generator.rb
@@ -1,10 +1,11 @@
 require "json"
 
 module FixtureGenerator
-  def save_fixture(filename)
+  def save_fixture(filename, content=nil)
+    content ||= decoded_response
     path = File.join(Rails.root, "spec/javascripts/fixtures/", filename)
     File.open(path, 'w') do |f|
-      f.write JSON.pretty_generate(decoded_response.as_json)
+      f.write JSON.pretty_generate(content.as_json)
     end
   end
 end
