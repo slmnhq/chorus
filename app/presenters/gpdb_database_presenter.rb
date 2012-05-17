7584e2c5203865eaced617231f9c999a25aefa71
Corina Lawu & Jacob Maine <pair+corina+jacob@pivotallabs.com>
2012-05-17 15:08:00 -0700

    Add id to db presenter.

--- app/presenters/gpdb_database_presenter.rb
+++ app/presenters/gpdb_database_presenter.rb
@@ -1,6 +1,7 @@
 class GpdbDatabasePresenter < Presenter
   def to_hash
     {
+        :id => model.id,
         :name => model.name,
         :instance_id => model.instance_id
     }
--- spec/presenters/gpdb_database_presenter_spec.rb
+++ spec/presenters/gpdb_database_presenter_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe GpdbDatabasePresenter, :type => :view do
   before(:each) do
-    database = FactoryGirl.build(:gpdb_database, name: "abc", instance: FactoryGirl.build(:instance, :id => 123))
+    database = FactoryGirl.build(:gpdb_database, name: "abc", id: 456, instance: FactoryGirl.build(:instance, :id => 123))
     @presenter = GpdbDatabasePresenter.new(database, view)
   end
 
@@ -13,6 +13,7 @@ describe GpdbDatabasePresenter, :type => :view do
 
     it "includes the fields" do
       @hash[:name].should == "abc"
+      @hash[:id].should == 456
       @hash[:instance_id].should == 123
     end
   end
