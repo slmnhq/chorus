9186231767c22262bedbe75922013e3ff2f57822
Brandon Liu <pair+brandon@pivotallabs.com>
2012-05-17 18:38:06 -0700

    tee the output of CI commands. You can now see the live output of the tests while Jenkins is running them.

--- ci.sh
+++ ci.sh
@@ -10,19 +10,19 @@ bundle install
 rake legacy:setup db:migrate db:test:prepare db:test:prepare:legacy
 
 # Run rspec unit tests
-script/test > $WORKSPACE/rspec_tests.log 2>&1
+script/test 2>&1 | tee $WORKSPACE/rspec_tests.log
 
 # Run Jasmine tests
 
 # start jasmine
 ps aux | grep jasmine | grep -v grep | awk '{print $2}' | xargs kill -9
-bundle exec rake jasmine > $WORKSPACE/jasmine.log 2>&1 &
+bundle exec rake jasmine 2>&1 | tee $WORKSPACE/jasmine.log  &
 jasmine_pid=$!
 echo $jasmine_pid
 sleep 5
 
 RAILS_ENV=development rake devmode:enable assets:precompile
-rake phantom > $WORKSPACE/jasmine_tests.log 2>&1
+rake phantom 2>&1 | tee $WORKSPACE/jasmine_tests.log
 
 # Run integration tests
-script/test spec/integration/ > $WORKSPACE/integration_tests.log 2>&1
+script/test spec/integration/ 2>&1 | tee $WORKSPACE/integration_tests.log
