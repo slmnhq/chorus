58b14e15835dc4feed4cc62092c9dd5322cec737
Bleicke Petersen <pair+bpetersen@pivotallabs.com>
2012-05-21 14:22:30 -0700

    Fix foreman

--- Procfile
+++ Procfile
@@ -1,4 +1,4 @@
-db:     script/start_db.sh
+db:     script/start_db_noisy.sh
 web:	rails s
 worker: script/start_worker.rb
 clock:  script/start_clock.rb
---
+++ script/start_db_noisy.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+ps aux | grep -- '-p8543' | grep -v grep | awk '{print $2}' | xargs kill -9
+if [ -f ~/workspace/chorusrails/var/db/postmaster.pid ];
+then
+    rm ~/workspace/chorusrails/var/db/postmaster.pid
+fi
+
+postgres -D ~/workspace/chorusrails/var/db -h localhost -p8543 --bytea_output=escape
+
