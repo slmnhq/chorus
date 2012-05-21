512cee97c0a4f17984316c04828a9059111070bc
Boxiong Ding & Jacob Maine <pair+boxiong+jacob@pivotallabs.com>
2012-05-21 16:10:57 -0700

    Paginate db objects for schema browse page.
    [#27928765]

--- app/controllers/database_objects_controller.rb
+++ app/controllers/database_objects_controller.rb
@@ -2,8 +2,8 @@ class DatabaseObjectsController < ApplicationController
   def index
     schema = GpdbSchema.find(params[:schema_id])
     account = schema.database.instance.account_for_user! current_user
-    data = GpdbDatabaseObject.refresh(account, schema)
+    GpdbDatabaseObject.refresh(account, schema)
 
-    present data
+    present schema.database_objects.paginate(params.slice(:page, :per_page))
   end
 end
--- spec/controllers/database_objects_controller_spec.rb
+++ spec/controllers/database_objects_controller_spec.rb
@@ -8,17 +8,19 @@ describe DatabaseObjectsController do
   end
 
   context "#index" do
+    let(:instance) { FactoryGirl.create(:instance, :owner_id => user.id) }
+    let(:instanceAccount) { FactoryGirl.create(:instance_account, :instance_id => instance.id, :owner_id => user.id) }
+
+    let(:database) { FactoryGirl.create(:gpdb_database, :instance => instance, :name => "database1") }
+    let(:schema) { FactoryGirl.create(:gpdb_schema, :name => 'schema1', :database => database) }
+    let!(:table) { FactoryGirl.create(:gpdb_table, :name => 'table1', :schema => schema) }
+    let!(:view) { FactoryGirl.create(:gpdb_view, :name => 'view1', :schema => schema) }
+
+    before do
+      stub(GpdbDatabaseObject).refresh(instanceAccount, schema) { [] }
+    end
+
     it "should retrieve all db objects for a schema" do
-      instance = FactoryGirl.create(:instance, :owner_id => user.id)
-      instanceAccount = FactoryGirl.create(:instance_account, :instance_id => instance.id, :owner_id => user.id)
-
-      database = FactoryGirl.create(:gpdb_database, :instance => instance, :name => "database1")
-      schema = FactoryGirl.create(:gpdb_schema, :name => 'schema1', :database => database)
-      table = FactoryGirl.build_stubbed(:gpdb_table, :name => 'table1')
-      view = FactoryGirl.build_stubbed(:gpdb_view, :name => 'view1')
-
-      stub(GpdbDatabaseObject).refresh(instanceAccount, schema) { [table, view] }
-
       get :index, :schema_id => schema.to_param
 
       response.code.should == "200"
@@ -26,6 +28,17 @@ describe DatabaseObjectsController do
 
       decoded_response[0].id.should == table.id
       decoded_response[1].id.should == view.id
+    end
+
+    it "should not return db objects in another schema" do
+      different_table = FactoryGirl.create(:gpdb_table)
+      get :index, :schema_id => schema.to_param
+      decoded_response.map(&:id).should_not include different_table.id
+    end
+
+    it "should paginate results" do
+      get :index, :schema_id => schema.to_param, :per_page => 1
+      decoded_response.length.should == 1
     end
   end
 end
