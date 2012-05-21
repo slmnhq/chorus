0131b91f6945857557746bc007364aefd9b59055
Max Brunsfeld & Nitin Grewal <pair+mbrunsfeld+nitin@pivotallabs.com>
2012-05-21 13:58:50 -0700

    added 'response' key in fixtures

--- spec/support/fixture_generator.rb
+++ spec/support/fixture_generator.rb
@@ -2,7 +2,7 @@ require "json"
 
 module FixtureGenerator
   def save_fixture(filename, content=nil)
-    content ||= decoded_response
+    content ||= response.decoded_body
     path = File.join(Rails.root, "spec/javascripts/fixtures/", filename)
     File.open(path, 'w') do |f|
       f.write JSON.pretty_generate(content.as_json)
