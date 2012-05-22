a3c6047f0531aadb3f87574ec607738fa1d8a9ed
Boxiong Ding & Jacob Maine <pair+boxiong+jacob@pivotallabs.com>
2012-05-21 18:48:18 -0700

    put WURL back in api docs

--- spec/support/rspec_api_documentation_config.rb
+++ spec/support/rspec_api_documentation_config.rb
@@ -2,4 +2,5 @@ RspecApiDocumentation.configure do |config|
   config.docs_dir = Rails.root.join("public", "api")
   config.url_prefix = "api/"
   config.keep_source_order = true
+  config.format = "wurl"
 end
