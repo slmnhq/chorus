67ad7fc559904ceff4ba0a15677d2763685feef5
Boxiong Ding & Jacob Maine <pair+boxiong+jacob@pivotallabs.com>
2012-05-23 12:16:38 -0700

    Remove unused code. [#27928765]

--- app/assets/javascripts/collections/schema_set.js
+++ app/assets/javascripts/collections/schema_set.js
@@ -7,15 +7,5 @@ chorus.collections.SchemaSet = chorus.collections.Base.include(
 
     comparator:function (schema) {
         return schema.get('name').toLowerCase();
-    },
-
-    parse:function (resp) {
-        var resource = this._super("parse", arguments);
-        return _.map(resource, function (model) {
-            return _.extend({
-                instanceId: this.attributes.instanceId,
-                instanceName: this.attributes.instanceName
-            }, model)
-        }, this)
     }
 });
--- spec/javascripts/collections/schema_set_spec.js
+++ spec/javascripts/collections/schema_set_spec.js
@@ -26,26 +26,4 @@ describe("chorus.collections.SchemaSet", function() {
             expect(names).toEqual(['a', 'G', 'z']);
         })
     });
-
-    describe("#parse", function() {
-        beforeEach(function() {
-            this.collection.fetch();
-            this.server.lastFetchFor(this.collection).succeed([
-                { name: "ron_the_schema", databaseName: "wicked_tables", databaseId: "41" },
-                { name: "michelle_the_schema", databaseName: "wicked_tables", databaseId: "41" }
-            ]);
-        });
-
-        it("sets the instanceId, instance name, database id and database name from the collection on each model", function() {
-            expect(this.collection.at(0).get("instanceId")).toBe("50");
-            expect(this.collection.at(0).get("instanceName")).toBe("jim");
-            expect(this.collection.at(0).database().id).toBe("41");
-            expect(this.collection.at(0).database().name()).toBe("wicked_tables");
-
-            expect(this.collection.at(1).get("instanceId")).toBe("50");
-            expect(this.collection.at(1).get("instanceName")).toBe("jim");
-            expect(this.collection.at(1).database().id).toBe("41");
-            expect(this.collection.at(1).database().name()).toBe("wicked_tables");
-        });
-    });
 });
