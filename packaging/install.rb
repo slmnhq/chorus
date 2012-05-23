d72616dc9b3bea497a89c1c30ef3ebc8cf0b2e14
Brandon Liu & Nitin Grewal <pair+brandon+nitin@pivotallabs.com>
2012-05-23 11:41:15 -0700

    Unpackaging installs ruby and rubygems

--- packaging/install.rb
+++ packaging/install.rb
@@ -2,12 +2,15 @@ COMPONENTS_DIR = File.expand_path("../components", __FILE__)
 ROOT = ENV['HOME']
 APP_DIR = File.join(ROOT, "chorusrails")
 PG_DIR = File.join(ROOT, "pgsql")
+RUBY_DIR = File.join(ROOT, "ruby")
 
 def install
   setup_directories
   install_postgres
   start_database
   create_database
+  install_ruby #TODO: need LibYAML
+  install_rubygems_and_bundler
 end
 
 def setup_directories
@@ -35,6 +38,28 @@ def start_database
   run "#{PG_DIR}/bin/pg_ctl init -D #{APP_DIR}/var/db -U vagrant"
   run "#{PG_DIR}/bin/pg_ctl start -D #{APP_DIR}/var/db -o '-h localhost -p8543 --bytea_output=escape'"
   sleep(5)
+end
+
+def install_ruby
+  puts "Installing ruby into #{COMPONENTS_DIR}..."
+  Dir.chdir(COMPONENTS_DIR) do
+    run "tar xzf ruby"
+    Dir.chdir("ruby-1.9.3-p125") do
+        run("./configure --prefix=#{RUBY_DIR}")
+        run ("make")
+        run("make install")
+    end
+  end
+end
+
+def install_rubygems_and_bundler
+  Dir.chdir(COMPONENTS_DIR) do
+    run "tar xzf rubygems"
+    Dir.chdir("rubygems-1.8.24") do
+      run "#{RUBY_DIR}/bin/ruby setup.rb --prefix=~/rubygems/"
+    end
+    run "#{ROOT}/rubygems/bin/gem install --local bundler-1.1.3.gem --no-ri --no-rdoc"
+  end
 end
 
 def run(cmd)
