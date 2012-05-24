a793112e1d859610b403d8c7f8a68ca51ab68b70
Brandon Liu & Manikandan Pushpavaneswaran <pair+brandon+manikandan@pivotallabs.com>
2012-05-24 14:22:55 -0700

    Integration test marked pending for delete workspace; not implemented yet

--- spec/integration/delete_workspace_spec.rb
+++ spec/integration/delete_workspace_spec.rb
@@ -3,13 +3,20 @@ require File.join(File.dirname(__FILE__), 'spec_helper')
 describe "deleting a workspace" do
   before(:each) do
     login('edcadmin', 'secret')
-    create_valid_workspace
   end
 
   xit "deletes the workspace" do
+    workspace_name = "DeleteWorkspace"
+    create_valid_workspace(:name => workspace_name)
     click_link "Delete this Workspace"
     wait_until { page.has_selector?(".submit") }
     find(".submit").click
+
+    visit('/#/workspaces')
+    wait_until { current_route == '/#/workspaces' }
+    page.execute_script("$('.popup').click()")
+    click_link("All Workspaces")
+    within(".workspace_list") { page.should_not have_content(workspace_name) }
   end
 end
 
