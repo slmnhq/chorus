8fcb78970f72d7c43a5ac852357e91104c0a1228
Corina Lawu & Max Brunsfeld <pair+corina+mbrunsfeld@pivotallabs.com>
2012-05-24 09:05:27 -0700

    In image workfile content view spec, use image url from server

--- spec/javascripts/views/workfiles/image_workfile_content_view_spec.js
+++ spec/javascripts/views/workfiles/image_workfile_content_view_spec.js
@@ -1,18 +1,18 @@
 describe("chorus.views.ImageWorkfileContent", function() {
     beforeEach(function() {
-        this.model = fixtures.imageWorkfile();
+        this.model = newFixtures.workfile.image();
         this.view = new chorus.views.ImageWorkfileContent({ model : this.model })
     });
 
     describe("render", function() {
         beforeEach(function() {
             this.view.render();
-        })
+        });
 
         it("creates an image tag referencing the workfile", function() {
             var img = this.view.$("img")
             expect(img).toExist();
-            expect(img).toHaveAttr("src", "/workspace/" + this.model.workspace().id + "/workfile/" + this.model.get("id") + "/file/" + this.model.get("versionInfo").versionFileId)
-        })
-    })
+            expect(img).toHaveAttr("src", this.model.get("versionInfo").contentUrl);
+        });
+    });
 });
