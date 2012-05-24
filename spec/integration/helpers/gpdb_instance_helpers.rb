575c9c4c067cb3ffb49005f3d1b275374b5bf2ec
Brandon Liu & Manikandan Pushpavaneswaran <pair+brandon+manikandan@pivotallabs.com>
2012-05-24 15:58:12 -0700

    Integration tests for adding instance accounts and changing between individual accounts and shared accounts.

--- spec/integration/helpers/gpdb_instance_helpers.rb
+++ spec/integration/helpers/gpdb_instance_helpers.rb
@@ -1,26 +1,24 @@
-def create_valid_instance(params)
-    name = params[:name] || "GPDB_instance#{Time.now.to_i}"
-    visit("#/instances")
-    wait_until { current_route == "/instances" && page.has_selector?("button[data-dialog=InstancesNew]") }
-    click_button "Add instance"
-    within("#facebox") do
-        choose("register_existing_greenplum")
-        wait_until { find("input[name=name]").visible? }
-        wait_until { find("textarea[name=description]").visible? }
-        wait_until { find("input[name=host]").visible? }
-        wait_until { find("input[name=port]").visible? }
-        wait_until { find("input[name=dbUsername]").visible? }
-        wait_until { find("input[name=dbPassword]").visible? }
+def create_valid_instance(params = {})
+  name = params[:name] || "GPDB_instance#{Time.now.to_i}"
+  visit("#/instances")
+  wait_until { current_route == "/instances" && page.has_selector?("button[data-dialog=InstancesNew]") }
+  click_button "Add instance"
+  within("#facebox") do
+    choose("register_existing_greenplum")
+    wait_until { find("input[name=name]").visible? }
+    wait_until { find("textarea[name=description]").visible? }
+    wait_until { find("input[name=host]").visible? }
+    wait_until { find("input[name=port]").visible? }
+    wait_until { find("input[name=dbUsername]").visible? }
+    wait_until { find("input[name=dbPassword]").visible? }
 
-        fill_in 'name', :with => name
-        fill_in 'description', :with => "sandbox creation"
-        fill_in 'host', :with => "gillette.sf.pivotallabs.com"
-        #fill_in 'host', :with => "rh55-qvm89.sanmateo.greenplum.com"
-        fill_in 'port', :with => "5432"
-        fill_in 'dbUsername', :with => "gpadmin"
-        #fill_in 'db_username', :with => "edcadmin"
-        fill_in 'dbPassword', :with => "secret"
-        uncheck("register_greenplum_shared")
-        find(".submit").click
-    end
+    fill_in 'name', :with => name
+    fill_in 'description', :with => "sandbox creation"
+    fill_in 'host', :with => "gillette.sf.pivotallabs.com"
+    fill_in 'port', :with => "5432"
+    fill_in 'dbUsername', :with => "gpadmin"
+    fill_in 'dbPassword', :with => "secret"
+    check("register_greenplum_shared") if params[:shared] == true
+    find(".submit").click
+  end
 end
--- spec/integration/new_instance_account_spec.rb
+++ spec/integration/new_instance_account_spec.rb
@@ -3,27 +3,56 @@ require File.join(File.dirname(__FILE__), 'spec_helper')
 describe "creating an instance credential" do
   before(:each) do
     login('edcadmin', 'secret')
-    create_valid_instance
-    create_valid_user(:username => "testuser1")
   end
 
-  xit "works" do
+  it "creates a new instance account" do
+    create_valid_instance
+    create_valid_user(:username => "EddyNice", :first_name => "Eddy", :last_name => "Nice")
+
     visit("/#/instances")
     wait_until { current_route == "/instances" && page.has_selector?("a[data-dialog=InstancePermissions]") }
     click_link "Edit"
     within("#facebox") do
       click_button "Add Account"
-
       #TODO actually select the correct user
-      #select('Joe Blow', :from => '#user_name_select')
-      fill_in 'db_username', :with => "gpadmin"
-      fill_in 'db_password', :with => "secret"
+      fill_in 'dbUsername', :with => "gpadmin"
+      fill_in 'dbPassword', :with => "secret"
     end
 
     click_link('Save Changes')
     page.should_not have_selector('.close_errors')
     within('.collection_list') do
-      page.should have_content("Joe Blow")
+      page.should have_content("Eddy Nice")
+    end
+  end
+
+  it "switches from individual to shared accounts and back" do
+    create_valid_instance
+    visit("/#/instances")
+    wait_until { current_route == "/instances" && page.has_selector?("a[data-dialog=InstancePermissions]") }
+    click_link "Edit"
+    within("#facebox") do
+      click_link "Switch to single shared account"
+      click_button "Enable shared account"
+      page.should_not have_content "Add Account"
+      click_link "Switch to individual accounts"
+      click_button "Remove shared account"
+      page.should have_content "Add Account"
+    end
+  end
+
+  it "changes the shared password on a shared instance" do
+    create_valid_instance(:shared => true)
+    click_link "Edit"
+    within("#facebox") do
+      click_link "Edit"
+      fill_in 'dbUsername', :with => 'gpadmin'
+      fill_in 'dbPassword', :with => 'secret2'
+      click_link "Save Changes"
+      page.find('.errors').should have_content("FATAL: password authentication failed")
+      fill_in 'dbPassword', :with => 'secret'
+      click_link "Save Changes"
+      page.find('.errors').should_not have_content("failed")
     end
   end
 end
