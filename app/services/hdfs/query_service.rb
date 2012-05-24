1c556f8003b4f05b435f91e289a799c20a007950
Boxiong Ding & Vinicius Fuentes <pair+boxiong+vinicius@pivotallabs.com>
2012-05-24 14:09:07 -0700

    Adding 1 second timeout to HDFS query service queries.

--- app/services/hdfs/query_service.rb
+++ app/services/hdfs/query_service.rb
@@ -1,8 +1,10 @@
+require 'timeout'
 require 'httparty'
 
 module Hdfs
   class QueryService
     SERVICE_HOST = YAML.load_file(Rails.root.join("config", "hdfs_service.yml"))["query_service_url"]
+    TIMEOUT = 1.0 # second
 
     def self.instance_version(instance)
       new(instance).version
@@ -17,17 +19,29 @@ module Hdfs
     end
 
     def version
-      @version ||= HTTParty.get("#{SERVICE_HOST}/version", :query => @query)["response"]
+      timeout {
+        @version ||= HTTParty.get("#{SERVICE_HOST}/version", :query => @query)["response"]
+      }
     end
 
     def list(path)
-      escaped_path = Rack::Utils.escape(path)
-      HTTParty.get("#{SERVICE_HOST}/#{version}/list/#{escaped_path}", :query => @query)
+      timeout {
+        escaped_path = Rack::Utils.escape(path)
+        HTTParty.get("#{SERVICE_HOST}/#{version}/list/#{escaped_path}", :query => @query)
+      }
     end
 
     def show(path)
-      escaped_path = Rack::Utils.escape(path)
-      HTTParty.get("#{SERVICE_HOST}/#{version}/show/#{escaped_path}", :query => @query)["response"]
+      timeout {
+        escaped_path = Rack::Utils.escape(path)
+        HTTParty.get("#{SERVICE_HOST}/#{version}/show/#{escaped_path}", :query => @query)["response"]
+      }
+    end
+
+    private
+
+    def timeout(&block)
+        Timeout::timeout(TIMEOUT, &block)
     end
   end
 end
