81f0882a81ca8791564fa956f6c5fad1bd67169b
Jacob Maine <pair+jacob@pivotallabs.com>
2012-05-29 15:03:48 -0700

    adding selenium test for creating a  hadoop instance and updating the hadoop helpers file

--- spec/integration/create_instance_spec.rb
+++ spec/integration/create_instance_spec.rb
@@ -39,28 +39,7 @@ describe " add an instance " do
     find('.instance_list').should have_content(new_instance_name)
   end
 
-  xit "creates an Hadoop Instance" do
-    within("#facebox") do
-      wait_until { page.has_selector?(".register_existing_hadoop input[name=name]")}
-      choose("register_existing_hadoop")
-      wait_until { !page.has_selector?(".register_existing_hadoop.collapsed")}
-      within(".register_existing_hadoop") do
-        wait_until { find("input[name=name]").visible? }
-        wait_until { find("textarea[name=description]").visible? }
-        wait_until { find("input[name=host]").visible? }
-        wait_until { find("input[name=port]").visible? }
-        wait_until { find("input[name=userName]").visible? }
-        wait_until { find("input[name=userGroups]").visible? }
-
-        fill_in 'name', :with => "Hadoop_inst_sel_test#{Time.now.to_i}"
-        fill_in 'description', :with => "Hadoop Instance Creation"
-        fill_in 'host', :with => "gillette.sf.pivotallabs.com"
-        fill_in 'port', :with => "8020"
-        fill_in 'userName', :with => "hadoop"
-        fill_in 'userGroups', :with => "hadoop"
-      end
-
-      find(".submit").click
-    end
+  it "creates an Hadoop Instance" do
+    create_valid_hadoop_instance(:name => "hadoop_instance")
   end
 end
--- spec/integration/helpers/hadoop_instance_helpers.rb
+++ spec/integration/helpers/hadoop_instance_helpers.rb
@@ -1,19 +1,26 @@
-def create_valid_hadoop_instance(name=nil)
-    name ||= "hadoop_instance#{Time.now.to_i}"
-    visit("#/instances")
-    wait_until { current_route == "/instances" && page.has_selector?("button[data-dialog=InstancesNew]") }
-    click_button "Add instance"
-    within("#facebox") do
-       choose("register_existing_greenplum")
-       wait_until { page.has_selector?(".register_existing_hadoop input[name=name]")}
-       fill_in 'name', :with => "Hadoop_inst_sel_test#{Time.now.to_i}"
-       fill_in 'description', :with => "Hadoop Instance Creation"
-       fill_in 'host', :with => "gillette.sf.pivotallabs.com"
-       fill_in 'port', :with => "8020"
-       fill_in 'userName', :with => "hadoop"
-       fill_in 'userGroups', :with => "hadoop"
-       find(".submit").click
-    end
+def create_valid_hadoop_instance(params = {})
+  name = params[:name] || "GPDB_instance#{Time.now.to_i}"
+  visit("#/instances")
+  wait_until { current_route == "/instances" && page.has_selector?("button[data-dialog=InstancesNew]") }
+  click_button "Add instance"
+  within("#facebox") do
+    choose("register_existing_hadoop")
+    wait_until { find("input[name=name]").visible? }
+    wait_until { find("textarea[name=description]").visible? }
+    wait_until { find("input[name=host]").visible? }
+    wait_until { find("input[name=port]").visible? }
+    wait_until { find("input[name=username]").visible? }
+    wait_until { find("input[name=group_list]").visible? }
 
+    fill_in 'name', :with => name
+    fill_in 'description', :with => "hadoop instance"
+    fill_in 'host', :with => "gillette.sf.pivotallabs.com"
+    fill_in 'port', :with => "8020"
+    fill_in 'username', :with => "hadoop"
+    fill_in 'group_list', :with => "hadoop"
+    find(".submit").click
+  end
+  wait_until { current_route == "/instances" }
+  sleep(3)
 end
 
