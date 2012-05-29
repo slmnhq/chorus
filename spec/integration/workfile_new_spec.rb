49f60021424897d2805bf16a5319d0ed101bf12a
Jacob Maine <pair+jacob@pivotallabs.com>
2012-05-29 16:05:02 -0700

    integration test for creating a new sql workfile

--- spec/integration/workfile_new_spec.rb
+++ spec/integration/workfile_new_spec.rb
@@ -5,22 +5,17 @@ describe " add an instance " do
     login('edcadmin', 'secret')
   end
 
-  xit "creates a SQL workfile" do
-    visit("#/workspaces")
-    wait_until { current_route == '/workspaces' && page.has_selector?("button[data-dialog=WorkspacesNew]") } 
-    click_button "Create Workspace"
-    within("#facebox") do
-      fill_in 'name', :with => "new_sql_wf_ws#{Time.now.to_i}"
-      click_button "Create Workspace"    
-    end
-    wait_until { current_route =~ /workspaces\/\d+/ }
+  it "creates a SQL workfile" do
+    create_valid_workspace()
     click_link "Work Files"
-    wait_until { current_route =~ /workspaces\/\d+\/workfiles/ }
-
+    page.find("h1").should have_content("Work Files")
     click_button "Create SQL File"
-    fill_in 'fileName', :with => "new_sql_wf#{Time.now.to_i}"
-    click_button "Add SQL File"
-    wait_until { current_route =~ /workspaces\/\d+\/workfiles\/\d+/ }
+    wf_name = "new_sql_wf#{Time.now.to_i}"
+    within "#facebox" do
+      fill_in 'fileName', :with => wf_name
+      click_button "Add SQL File"
+    end
+    page.should have_content (wf_name)
   end
 
 end
