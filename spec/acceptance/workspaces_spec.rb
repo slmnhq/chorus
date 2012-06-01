91c063766564219d40e05f48f8841e447ea9d143
Bleicke Petersen & Yaneeka Huq <pair+bpetersen+yaneeka@pivotallabs.com>
2012-05-31 17:20:37 -0700

    Consolidate API docs workspace and workspaces, make workspace update API doc actually update something [#29869571]

--- spec/acceptance/workspaces_spec.rb
+++ spec/acceptance/workspaces_spec.rb
@@ -17,6 +17,33 @@ resource "Workspaces" do
     end
   end
 
+  get "/workspaces/:id" do
+    let(:id) { workspace.to_param }
+
+    example_request "Show workspace details" do
+      status.should == 200
+    end
+  end
+
+  put "/workspaces/:id" do
+    let(:id) { workspace.to_param }
+
+    parameter :name, "Workspace name"
+    parameter :public, "1 if the workspace should be public, 0 if it should be private. Defaults to public if the parameter is not provided."
+    parameter :summary, "Notes about the workspace"
+
+    required_parameters :name
+    scope_parameters :workspace, :all
+
+    let(:name) { "Bwesome Workspace" }
+    let(:public) { "1" }
+    let(:summary) { "I like big data and I cannot lie, all the other coders can't deny" }
+
+    example_request "Update workspace details" do
+      status.should == 200
+    end
+  end
+
   post "/workspaces" do
     parameter :name, "Workspace name"
     parameter :public, "1 if the workspace should be public, 0 if it should be private. Defaults to public if the parameter is not provided."
