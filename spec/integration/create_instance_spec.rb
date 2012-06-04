b782ba5701c1447eb2799e0e931f74921e33b31b
Manikandan Pushpavaneswaran <pair+manikandan@pivotallabs.com>
2012-06-04 14:43:37 -0700

    adding a test for testing invalid instance names

--- spec/integration/create_instance_spec.rb
+++ spec/integration/create_instance_spec.rb
@@ -39,6 +39,44 @@ describe " add an instance " do
     find('.instance_list').should have_content(new_instance_name)
   end
 
+  it "tries to create an instance with an invalid name" do
+
+      within("#facebox") do
+        wait_until { page.has_selector?(".register_existing_greenplum input[name=name]")}
+        choose("register_existing_greenplum")
+        wait_until { !page.has_selector?(".register_existing_greenplum.collapsed")}
+        within(".register_existing_greenplum") do
+          wait_until { find("input[name=name]").visible? }
+          wait_until { find("textarea[name=description]").visible? }
+          wait_until { find("input[name=host]").visible? }
+          wait_until { find("input[name=port]").visible? }
+          wait_until { find("input[name=dbUsername]").visible? }
+          wait_until { find("input[name=dbPassword]").visible? }
+
+          fill_in 'name', :with => "instance name"
+          fill_in 'description', :with => "GPDB instance creation"
+          fill_in 'host', :with => "gillette.sf.pivotallabs.com"
+          fill_in 'port', :with => "5432"
+          fill_in 'dbUsername', :with => "gpadmin"
+          fill_in 'dbPassword', :with => "secret"
+
+          check("register_greenplum_shared")
+        end
+
+        find(".submit").click
+        field_errors.should_not be_empty
+
+        within(".register_existing_greenplum") do
+        fill_in 'name', :with => "instance_name"
+        end
+      find(".submit").click
+      end
+
+      find('.instance_list').should have_content("instance_name")
+      visit("/#/instances")
+      find('.instance_list').should have_content("instance_name")
+    end
+
   it "creates an instance with an invalid host and port" do
     within("#facebox") do
       wait_until { page.has_selector?(".register_existing_greenplum input[name=name]")}
