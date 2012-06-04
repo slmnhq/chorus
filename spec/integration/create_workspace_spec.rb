0df721732def27279705246ed86ba59380228250
Manikandan Pushpavaneswaran <pair+manikandan@pivotallabs.com>
2012-06-04 15:05:45 -0700

    adding a test case to create a workspace with 256 characters

--- spec/integration/create_workspace_spec.rb
+++ spec/integration/create_workspace_spec.rb
@@ -17,6 +17,9 @@ describe "Create workspaces" do
     create_valid_workspace(:name => "spl_char_!@##%^&&*()}{|?><:;|")
   end
 
+  it "creates a workspace with special characters" do
+      create_valid_workspace(:name => "123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456")
+    end
   xit "creates a private workspace" do
 
   end
