086799365063a419b4e93202a6bd48aa5f29ff3e
Manikandan Pushpavaneswaran <pair+manikandan@pivotallabs.com>
2012-06-04 16:11:16 -0700

    Test for trying to add a hadoop instance with invalid host name and port

--- spec/integration/create_hadoop_instance_spec.rb
+++ spec/integration/create_hadoop_instance_spec.rb
@@ -39,7 +39,54 @@ describe " add an instance " do
     end
     wait_until { current_route == "/instances" }
     find('.instance_list').should have_content("hadoop_invalid_instance_name")
+  end
+
+  xit "tries to create a hadoop instance with invalid port and host" do
+    visit("#/instances")
+    wait_until { current_route == "/instances" && page.has_selector?("button[data-dialog=InstancesNew]") }
+    click_button "Add instance"
+    within("#facebox") do
+      choose("register_existing_hadoop")
+      wait_until { find("input[name=name]").visible? }
+      wait_until { find("textarea[name=description]").visible? }
+      wait_until { find("input[name=host]").visible? }
+      wait_until { find("input[name=port]").visible? }
+      wait_until { find("input[name=userName]").visible? }
+      wait_until { find("input[name=userGroups]").visible? }
+
+      fill_in 'name', :with => "hadoop_host_port"
+      fill_in 'description', :with => "hadoop instance"
+      fill_in 'host', :with => "gillett.sf.pivotallabs.com"
+      fill_in 'port', :with => "8020"
+      fill_in 'userName', :with => "hadoop"
+      fill_in 'userGroups', :with => "hadoop"
+      find(".submit").click
+    end
+    page.should have_content"Unable to determine HDFS server version. Check connection parameters"
+
+    within("#facebox") do
+      fill_in 'host', :with => "gillett.sf.pivotallabs.com"
+      fill_in 'port', :with => "802"
+      find(".submit").click
+    end
+    page.should have_content"Unable to determine HDFS server version. Check connection parameters"
+
+    within("#facebox") do
+      fill_in 'host', :with => "gillette.sf.pivotallabs.com"
+      fill_in 'port', :with => "802"
+      find(".submit").click
+    end
+    page.should have_content"Timeout while connecting to HDFS Query Service"
+
+    within("#facebox") do
+      fill_in 'host', :with => "gillette.sf.pivotallabs.com"
+      fill_in 'port', :with => "8020"
+      find(".submit").click
+    end
+    wait_until { current_route == "/instances" }
+    find('.instance_list').should have_content("hadoop_host_port")
 
   end
+
 end
 
