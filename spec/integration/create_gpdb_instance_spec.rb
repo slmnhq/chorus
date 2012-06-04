0f82670747f869719d6017b97679ecd2d4485a05
Manikandan Pushpavaneswaran <pair+manikandan@pivotallabs.com>
2012-06-04 14:56:56 -0700

    adding a test for checking the dbusernbame and password when registering an instance. renaming the file to reflect the gpdb instance test and moving out 1 hadoop test into a seperate file. Hadoop service must be enabled on CI for the hadoop tests to pass

--- spec/integration/create_gpdb_instance_spec.rb
+++ spec/integration/create_gpdb_instance_spec.rb
@@ -6,7 +6,7 @@ describe " add an instance " do
     page.find("a.add.dialog").click
   end
 
-  it "creates an instance" do
+  xit "creates an instance" do
     new_instance_name = "GPDB_inst_sel_test#{Time.now.to_i}"
 
     within("#facebox") do
@@ -39,7 +39,7 @@ describe " add an instance " do
     find('.instance_list').should have_content(new_instance_name)
   end
 
-  it "tries to create an instance with an invalid name" do
+  xit "tries to create an instance with an invalid name" do
 
       within("#facebox") do
         wait_until { page.has_selector?(".register_existing_greenplum input[name=name]")}
@@ -77,7 +77,7 @@ describe " add an instance " do
       find('.instance_list').should have_content("instance_name")
     end
 
-  it "creates an instance with an invalid host and port" do
+  xit "tries to create an instance with an invalid host and port" do
     within("#facebox") do
       wait_until { page.has_selector?(".register_existing_greenplum input[name=name]")}
       choose("register_existing_greenplum")
@@ -126,11 +126,53 @@ describe " add an instance " do
     find('.instance_list').should have_content("invalid_instance")
   end
 
-  xit "creates an Hadoop Instance" do
-    # TODO blocked on having query service running for tests
-    hadoop_instance_name = "hadoop_instance"
-    create_valid_hadoop_instance(:name => hadoop_instance_name)
-    visit("#/instances")
-    find('.instance_list').should have_content(hadoop_instance_name)
-  end
+  it "tries to create an instance with an invalid db username and password" do
+      within("#facebox") do
+        wait_until { page.has_selector?(".register_existing_greenplum input[name=name]")}
+        choose("register_existing_greenplum")
+        wait_until { !page.has_selector?(".register_existing_greenplum.collapsed")}
+        within(".register_existing_greenplum") do
+          wait_until { find("input[name=name]").visible? }
+          wait_until { find("textarea[name=description]").visible? }
+          wait_until { find("input[name=host]").visible? }
+          wait_until { find("input[name=port]").visible? }
+          wait_until { find("input[name=dbUsername]").visible? }
+          wait_until { find("input[name=dbPassword]").visible? }
+
+          fill_in 'name', :with => "dbpass_dbuser"
+          fill_in 'description', :with => "GPDB instance creation"
+          fill_in 'host', :with => "gillette.sf.pivotallabs.com"
+          fill_in 'port', :with => "5432"
+          fill_in 'dbUsername', :with => "gpadmin"
+          fill_in 'dbPassword', :with => "secre"
+          check("register_greenplum_shared")
+        end
+        click_button "Add Instance"
+        page.find('.errors').should have_content("FATAL: password authentication failed for user")
+
+        within(".register_existing_greenplum") do
+          fill_in 'dbUsername', :with => "gpadmi"
+          fill_in 'dbPassword', :with => "secre"
+        end
+        click_button "Add Instance"
+        page.find('.errors').should have_content("FATAL: password authentication failed for user")
+
+        within(".register_existing_greenplum") do
+          fill_in 'dbUsername', :with => "gpadmi"
+          fill_in 'dbPassword', :with => "secret"
+        end
+        click_button "Add Instance"
+        page.find('.errors').should have_content("FATAL: password authentication failed for user")
+
+        within(".register_existing_greenplum")do
+          fill_in 'dbUsername', :with => "gpadmin"
+          fill_in 'dbPassword', :with => "secret"
+        end
+        click_button "Add Instance"
+      end
+      find('.instance_list').should have_content("dbpass_dbuser")
+      visit("/#/instances")
+      find('.instance_list').should have_content("dbpass_dbuser")
+    end
+
 end
