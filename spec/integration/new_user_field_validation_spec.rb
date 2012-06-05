9c7917880fa1fff2a60c8118c639f26a74233a45
Pushpavaneswaran <pushpm@usenpushpmmbp.local>
2012-06-05 15:54:21 -0700

    adding test for field validation for email when creating a new user

--- spec/integration/new_user_field_validation_spec.rb
+++ spec/integration/new_user_field_validation_spec.rb
@@ -38,6 +38,37 @@ describe "creating a user" do
 
   end
 
+  it "should let the admin create a user with a wrong email address" do
+
+    visit("/#/users/new")
+    first_name = Forgery::Name.first_name
+    last_name = Forgery::Name.last_name
+    fill_in 'firstName', :with =>first_name
+    fill_in 'lastName', :with => last_name
+    username = "#{first_name}#{last_name}"
+    fill_in 'username', :with => username
+    fill_in 'email', :with => "someemail@email"
+    fill_in 'password', :with => "secret"
+    fill_in 'passwordConfirmation', :with => "secret"
+    fill_in 'dept', :with => "Greenplum"
+    fill_in 'title', :with => "QA"
+    page.find("button[type=submit]").click
+    field_errors.should_not be_empty
+
+    fill_in 'email', :with => " "
+    page.find("button[type=submit]").click
+    field_errors.should_not be_empty
+
+    fill_in 'email', :with => "someemail@email.com"
+    page.find("button[type=submit]").click
+    wait_until { current_route == "/users" }
+    click_link("#{first_name} #{last_name}")
+    page.find("div.department").should have_content("Greenplum")
+    page.find("div.title").should have_content("QA")
+    page.find("h1").should have_content("#{first_name} #{last_name}")
+
+  end
+
 
 
 end
