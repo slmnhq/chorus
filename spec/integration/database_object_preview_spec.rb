887d5d49fee0567a7d4aaebb3bea565dfe1fb2d7
Brandon Liu & Nitin Grewal <pair+brandon+nitin@pivotallabs.com>
2012-06-07 14:50:04 -0700

    Integration test for view definition and metadata [#27928779]

--- spec/integration/database_object_preview_spec.rb
+++ spec/integration/database_object_preview_spec.rb
@@ -22,12 +22,12 @@ describe "Viewing data inside GPDB instances" do
   end
 
   it "can view a tables statistics and metadata" do
-    create_valid_instance(:name => "InstanceToViewStatistics")
-    click_link "InstanceToViewStatistics"
+    create_valid_instance(:name => "InstanceToViewStatistics1")
+    click_link "InstanceToViewStatistics1"
     click_link "Analytics"
     click_link "analytics"
 
-    dataset_id = Instance.find_by_name("InstanceToViewStatistics").databases.find_by_name("Analytics").schemas.find_by_name("analytics").database_objects.find_by_name("a1000").id
+    dataset_id = Instance.find_by_name("InstanceToViewStatistics1").databases.find_by_name("Analytics").schemas.find_by_name("analytics").database_objects.find_by_name("a1000").id
 
     page.find("li[data-database-object-id='#{dataset_id}']").click
     sleep(1)
@@ -42,5 +42,35 @@ describe "Viewing data inside GPDB instances" do
       page.should have_content("Columns 5")
       page.should have_content("Rows 1000")
     end
+  end
+
+  it "can view a views statistics, metadata and definition" do
+    create_valid_instance(:name => "InstanceToViewStatistics2")
+      click_link "InstanceToViewStatistics2"
+      click_link "Analytics"
+      click_link "analytics"
+
+      dataset_id = Instance.find_by_name("InstanceToViewStatistics2").databases.find_by_name("Analytics").schemas.find_by_name("analytics").database_objects.find_by_name("___lenny").id
+
+      page.find("li[data-database-object-id='#{dataset_id}']").click
+      sleep(1)
+
+      within "#sidebar" do
+        page.find("li[data-name='statistics']").click
+      end
+
+      within ".statistics_detail" do
+        page.should have_content("Source View")
+        page.should have_content("Columns 2")
+        page.should have_content("Description This is the comment on view - __lenny")
+      end
+
+      within(".content") do
+        click_link("___lenny")
+      end
+
+      within ".definition" do
+        page.should have_content("SELECT a.artist, a.title FROM top_1_000_songs_to_hear_before_you_die a;")
+      end
   end
 end
