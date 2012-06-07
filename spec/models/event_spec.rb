0b322cd59bfc8e993618efd98fc01d92e2749b66
Manikandan Pushpavaneswaran & Max Brunsfeld <pair+manikandan+mbrunsfeld@pivotallabs.com>
2012-06-07 16:51:58 -0700

    Fix bad test for Event model

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -15,12 +15,15 @@ describe Event do
       let(:action) { "INSTANCE_CREATED" }
       let(:object) { FactoryGirl.create(:instance) }
 
-      it "creates activities for the instance and for the actor" do
+      it "creates activities for the instance and for the actor, and one global activity (with no entity)" do
         activities = Activity.where(:event_id => event.id)
-        activities.map(&:entity).should =~ [user, object]
-      end
 
-      it "creates a global activity" do
+        activities.map(&:entity).should =~ [
+          user,
+          object,
+          nil
+        ]
+
         Activity.global.find_by_event_id(event.id).should_not be_nil
       end
     end
