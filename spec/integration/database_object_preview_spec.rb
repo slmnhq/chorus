709be748054d5f05c70d856ba8c2ae84748c1044
Jacob Maine & Yaneeka Huq <pair+jacob+yaneeka@pivotallabs.com>
2012-06-07 10:53:32 -0700

    rename integration test to reflect what it really tests

--- spec/integration/database_object_preview_spec.rb
+++ spec/integration/database_object_preview_spec.rb
@@ -5,15 +5,15 @@ describe "Viewing data inside GPDB instances" do
     login('edcadmin', 'secret')
   end
 
-  it "can preview a dataset" do
+  it "can preview a table" do
     create_valid_instance(:name => "InstanceToPreviewData")
     click_link "InstanceToPreviewData"
     click_link "Analytics"
     click_link "analytics"
 
-    dataset_id = Instance.find_by_name("InstanceToPreviewData").databases.find_by_name("Analytics").schemas.find_by_name("analytics").database_objects.find_by_name("a1000").id
+    table_id = Instance.find_by_name("InstanceToPreviewData").databases.find_by_name("Analytics").schemas.find_by_name("analytics").database_objects.find_by_name("a1000").id
 
-    page.find("li[data-database-object-id='#{dataset_id}']").click
+    page.find("li[data-database-object-id='#{table_id}']").click
     sleep(1)
     click_link "Preview Data"
     within(".data_table") do
