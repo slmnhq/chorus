7947c415aff7f7d975316868a9d2594bb6e61ea1
Brandon Liu & Nitin Grewal <pair+brandon+nitin@pivotallabs.com>
2012-06-07 17:39:06 -0700

    LDAP schema is customizable via ldap.yml [#30573325]

--- lib/ldap/ldap_client.rb
+++ lib/ldap/ldap_client.rb
@@ -1,26 +1,29 @@
 require 'net/ldap'
 
 class LdapClient
+
+  # entered from UsersController#ldap
   def self.search(username)
     ldap = client
-    filter = Net::LDAP::Filter.eq("uid", "#{username}")
-    results = ldap.search :base => "dc=#{config["dc"]}", :filter =>filter
+    filter = Net::LDAP::Filter.eq(self.config['attribute_names']['uid'], "#{username}")
+    results = ldap.search :base => "dc=#{self.config['dc']}", :filter => filter
 
     results.map do |result|
       {
-          :username => result["uid"].first,
-          :first_name => result["givenname"].first,
-          :last_name => result["sn"].first,
-          :title => result["title"].first,
-          :dept => result["departmentnumber"].first,
-          :email => result["mail"].first
+        :username =>   result[self.config['attribute_names']['uid']].first,
+        :first_name => result[self.config['attribute_names']['gn']].first,
+        :last_name =>  result[self.config['attribute_names']['sn']].first,
+        :title =>      result[self.config['attribute_names']['title']].first,
+        :dept =>       result[self.config['attribute_names']['ou']].first,
+        :email =>      result[self.config['attribute_names']['mail']].first
       }
     end
   end
 
+  # entered from CredentialsValidator
   def self.authenticate(username, password)
     ldap = client
-    ldap.auth "#{config["auth_attribute"]}=#{username},cn=#{config["auth_cn"]},dc=#{config["dc"]}", password
+    ldap.auth "#{self.config['attribute_names']['uid']}=#{username},cn=#{self.config['cn']},dc=#{self.config['dc']}", password
     ldap.bind
   end
 
@@ -31,11 +34,11 @@ class LdapClient
   private
 
   def self.client
-    Net::LDAP.new :host => config["host"], :base => "dc=#{config["dc"]}"
+    Net::LDAP.new :host => self.config['host'], :base => "dc=#{self.config['dc']}"
   end
 
   def self.config
-    @@config ||= YAML.load_file(config_file_path())[Rails.env]
+    @@config ||= YAML.load_file(config_file_path())['ldap']
   end
 
   def self.enabled?
