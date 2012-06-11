a0b4b2f3796487f81db472394cc06c1d1c8383b4
Brandon Liu & Yaneeka Huq <pair+brandon+yaneeka@pivotallabs.com>
2012-06-11 16:43:08 -0700

    Fix hadoop integration tests.

--- spec/integration/edit_hadoop_instance_details_spec.rb
+++ spec/integration/edit_hadoop_instance_details_spec.rb
@@ -6,14 +6,9 @@ describe " add an instance " do
     page.find("a.add.dialog").click
   end
 
-  create_valid_hadoop_instance(:name => "edit_hadoop_instance")
-  hadoop_instance_id = Instance.find_by_name("edit_hadoop_instance").id
-
   it "edits the hadoop instance name"do
-    go_to_instance_page
-    fill_in 'name', :with => "HadoopInstanceNewName"
-        fill_in 'description', :with => "Change Description"
-        click_button "Save Configuration"
-
+    create_valid_hadoop_instance(:name => "edit_hadoop_instance")
+    # instance will be selected
+    edit_hadoop_instance(:name => "HadoopInstanceNewName", :description => "Change Description")
   end
 end
