855adf7e2a93d3ba55a1950f997ba728904df95f
Manikandan Pushpavaneswaran <pair+manikandan@pivotallabs.com>
2012-06-13 14:34:36 -0700

    dding Instance_Created selenium test for GPDB instances

--- spec/integration/activitystream_create_spec.rb
+++ spec/integration/activitystream_create_spec.rb
@@ -6,7 +6,7 @@ describe " add an instance " do
     page.find("a.add.dialog").click
   end
 
-  xit "creates an activity stream when a gpdb instance is created" do
+  it "creates an activity stream when a gpdb instance is created" do
     create_valid_instance(:name => "gpdb_instance")
     gpdb_instance_id = Instance.find_by_name("gpdb_instance").id
     go_to_home_page
@@ -22,18 +22,21 @@ describe " add an instance " do
       click_link "EDC Admin"
     end
     page.should have_content "EDC Admin added a new instance gpdb_instance"
+    p Instance.all
   end
 
 
   xit "creates an activity stream when a hadoop instance is created" do
     create_valid_hadoop_instance(:name => "hadoop_instance")
-    hadoop_instance_id = Instance.find_by_name("hadoop_instance").id
+    variable = Instance.find_by_name("hadoop_instance")
+    p Instance.all
+    hadoop_instance_id = variable.id
     go_to_home_page
     page.should have_content "EDC Admin added a new instance hadoop_instance"
     sleep(3)
     go_to_instance_page
     within(".instance_provider") do
-      page.find("li[data-greenplum-instance-id='#{hadoop_instance_id}']").click
+      page.find("li[data-hadoop-instance-id='#{hadoop_instance_id}']").click
     end
     page.should have_content "EDC Admin added a new instance hadoop_instance"
     go_to_user_list_page
@@ -43,4 +46,4 @@ describe " add an instance " do
     page.should have_content "EDC Admin added a new instance hadoop_instance"
   end
 
-end
+end 
