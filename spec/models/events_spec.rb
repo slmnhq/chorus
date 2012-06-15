ea068193fc36a774c47ca2c079d2b2f8e2a100a3
Boxiong Ding & Max Brunsfeld <pair+boxiong+mbrunsfeld@pivotallabs.com>
2012-06-15 13:21:18 -0700

    Clean up events spec [#29788347]

--- spec/models/events_spec.rb
+++ spec/models/events_spec.rb
@@ -1,34 +1,25 @@
 require "spec_helper"
 
 describe Events do
-  let!(:user) { FactoryGirl.create :user }
-
   describe "types" do
+    let(:actor) { FactoryGirl.create(:user) }
     let(:greenplum_instance) { FactoryGirl.create(:instance) }
     let(:hadoop_instance) { FactoryGirl.create(:hadoop_instance) }
     let(:user) { FactoryGirl.create(:user) }
 
-    let(:target1) { nil }
-    let(:target2) { nil }
-
-    subject do
-      klass.create!(:actor => user, :target1 => target1, :target2 => target2)
-    end
-
-    let(:activities) { Activity.where(:event_id => subject.id) }
-    let(:global_activity) { Activity.global.find_by_event_id(subject.id) }
-
     class << self
 
-      def it_creates_activities_for(&block)
+      def it_creates_activities_for(&entities_block)
         it "creates the right entity-specific activities" do
-          entities = self.instance_exec(&block)
-          activities.map(&:entity).compact.should =~ entities
+          expected_entities = instance_exec(&entities_block)
+          activities = Activity.where(:event_id => subject.id)
+          activities.map(&:entity).compact.should =~ expected_entities
         end
       end
 
       def it_creates_a_global_activity
         it "creates a global activity" do
+          global_activity = Activity.global.find_by_event_id(subject.id)
           global_activity.should_not be_nil
         end
       end
@@ -36,37 +27,49 @@ describe Events do
     end
 
     describe "GREENPLUM_INSTANCE_CREATED" do
-      let(:klass) { Events::GREENPLUM_INSTANCE_CREATED }
-      let(:target1) { greenplum_instance }
+      subject do
+        Events::GREENPLUM_INSTANCE_CREATED.create!(
+          :actor => actor,
+          :greenplum_instance => greenplum_instance,
+        )
+      end
 
       its(:greenplum_instance) { should == greenplum_instance }
       its(:targets) { should == { :greenplum_instance => greenplum_instance } }
 
-      it_creates_activities_for { [user, greenplum_instance] }
+      it_creates_activities_for { [actor, greenplum_instance] }
       it_creates_a_global_activity
     end
 
     describe "HADOOP_INSTANCE_CREATED" do
-      let(:klass) { Events::HADOOP_INSTANCE_CREATED }
-      let(:target1) { hadoop_instance }
+      subject do
+        Events::HADOOP_INSTANCE_CREATED.create!(
+          :actor => actor,
+          :hadoop_instance => hadoop_instance,
+        )
+      end
 
       its(:hadoop_instance) { should == hadoop_instance }
       its(:targets) { should == { :hadoop_instance => hadoop_instance } }
 
-      it_creates_activities_for { [user, hadoop_instance] }
+      it_creates_activities_for { [actor, hadoop_instance] }
       it_creates_a_global_activity
     end
 
     describe "GREENPLUM_INSTANCE_CHANGED_OWNER" do
-      let(:klass) { Events::GREENPLUM_INSTANCE_CHANGED_OWNER }
-      let(:target1) { greenplum_instance }
-      let(:target2) { user }
+      subject do
+        Events::GREENPLUM_INSTANCE_CHANGED_OWNER.create!(
+          :actor => actor,
+          :greenplum_instance => greenplum_instance,
+          :new_owner => user
+        )
+      end
 
       its(:greenplum_instance) { should == greenplum_instance }
       its(:new_owner) { should == user }
       its(:targets) { should == { :greenplum_instance => greenplum_instance, :new_owner => user } }
 
-      it_creates_activities_for { [user, greenplum_instance] }
+      it_creates_activities_for { [actor, greenplum_instance] }
       it_creates_a_global_activity
     end
   end
@@ -75,16 +78,17 @@ describe Events do
     it "creates an event with the given parameters" do
       instance1 = FactoryGirl.create(:instance)
       instance2 = FactoryGirl.create(:instance)
+      user1 = FactoryGirl.create(:user)
       user2 = FactoryGirl.create(:user)
       user3 = FactoryGirl.create(:user)
 
-      Events::GREENPLUM_INSTANCE_CREATED.by(user).add(:greenplum_instance => instance1)
+      Events::GREENPLUM_INSTANCE_CREATED.by(user1).add(:greenplum_instance => instance1)
       Events::GREENPLUM_INSTANCE_CHANGED_OWNER.by(user2).add(:greenplum_instance => instance2, :new_owner => user3)
 
       event1 = Events::GREENPLUM_INSTANCE_CREATED.first
       event2 = Events::GREENPLUM_INSTANCE_CHANGED_OWNER.first
 
-      event1.actor.should == user
+      event1.actor.should == user1
       event1.greenplum_instance.should == instance1
 
       event2.actor.should == user2
