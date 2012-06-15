811cecf6a1163fe0ce9ae94a16ea0d2f6e65fbc0
Manikandan Pushpavaneswaran <pair+manikandan@pivotallabs.com>
2012-06-15 16:11:12 -0700

    Create Workspace tests includes public and private workspace creation
    [#27928795]
    
    Signed-off-by: Manikandan Pushpavaneswaran <pair+manikandan@pivotallabs.com>

--- spec/integration/create_workspace_spec.rb
+++ spec/integration/create_workspace_spec.rb
@@ -20,7 +20,15 @@ describe "Create workspaces" do
   it "creates a workspace with special characters" do
       create_valid_workspace(:name => "123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456")
     end
-  xit "creates a private workspace" do
+  it "creates a private workspace" do
 
+    go_to_workspace_page
+    click_button "Create Workspace"
+    fill_in 'name', :with => "Private Workspace"
+    uncheck ("Make this workspace publicly available")
+    create_valid_user(:username => "private")
+    login('private','secret')
+    go_to_workspace_page
+    page.should_not have_content ("Private Workspace")
   end
 end
