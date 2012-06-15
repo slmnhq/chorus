28d9025fd02aa6413280e7bcd162a49682912be5
Bleicke Petersen & Brandon Liu <pair+bpetersen+brandon@pivotallabs.com>
2012-06-14 17:15:07 -0700

    Aurora SOAP API exploration for creating databases [#27928667]

--- lib/aurora/get_version_info.rb
+++ lib/aurora/get_version_info.rb
@@ -3,7 +3,20 @@ require 'savon'
 
 s = Savon.client('lib/aurora/datacloudWS.wsdl')
 
-s.wsdl.endpoint = "https://10.80.129.96/datadirector/services/datacloudWS"
+s.wsdl.endpoint = "https://tinker/datadirector/services/datacloudWS"
 s.http.auth.ssl.verify_mode = :none
+p "HEADERS"
+p s.http.headers
 response = s.request :get_version_information
 p response.to_hash
+p "TRYING LISTING TEMPLATES"
+
+
+
+s.request "ins0:GetDatabaseConfigTemplatesByOrgIdRequest" do
+  s.http.headers["SOAPAction"] = "getDatabaseConfigTemplatesByOrgId"
+  soap.version = 1
+  soap.body = {"ins0:orgId" => 1}
+end
+
+
