12e4e1f354639189018c27e179f82ed6950da01d
Boxiong Ding & Max Brunsfeld <pair+boxiong+mbrunsfeld@pivotallabs.com>
2012-06-15 11:26:58 -0700

    By default, order activities by time, descending
    [#29788349]

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -4,9 +4,10 @@ class Activity < ActiveRecord::Base
   belongs_to :entity, :polymorphic => true
   belongs_to :event, :class_name => 'Events::Base'
 
-  default_scope includes(:event)
+  default_scope { includes(:event).order("created_at DESC") }
 
   def self.global
     where(:entity_type => "GLOBAL")
   end
 end
+
--- spec/models/activity_spec.rb
+++ spec/models/activity_spec.rb
@@ -1,20 +1,22 @@
 require "spec_helper"
 
 describe Activity do
-  describe "scopes" do
-    let!(:user1) { FactoryGirl.create(:user) }
-    let!(:user2) { FactoryGirl.create(:user) }
+  let!(:user1) { FactoryGirl.create(:user) }
+  let!(:user2) { FactoryGirl.create(:user) }
 
-    let!(:activity1) { Activity.create!(:entity => user1) }
-    let!(:activity2) { Activity.create!(:entity => user2) }
+  let!(:activity1) { Activity.create!(:entity => user1, :created_at => Time.now) }
+  let!(:activity2) { Activity.create!(:entity => user2, :created_at => Time.now + 25) }
 
-    let!(:activity3) { Activity.create!(:entity_type => "GLOBAL") }
-    let!(:activity4) { Activity.create!(:entity_type => "GLOBAL") }
+  let!(:activity3) { Activity.create!(:entity_type => "GLOBAL", :created_at => Time.now + 50) }
+  let!(:activity4) { Activity.create!(:entity_type => "GLOBAL", :created_at => Time.now + 100) }
 
-    describe ".global" do
-      it "returns the activities whose 'entity_type' has the special value 'GLOBAL'" do
-        Activity.global.all.should =~ [ activity3, activity4 ]
-      end
+  describe ".global" do
+    it "returns the activities whose 'entity_type' has the special value 'GLOBAL'" do
+      Activity.global.all.should =~ [ activity3, activity4 ]
     end
+  end
+
+  it "is ordered with the most recent items first, by default" do
+    Activity.all.should == [ activity4, activity3, activity2, activity1 ]
   end
 end
