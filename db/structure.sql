7912fd816b67573e77ec537e64a0752645a27ea4
Jacob Maine & Phil Goodwin <pair+jacob+phil@pivotallabs.com>
2012-06-15 18:06:40 -0700

    add missing change to structure.sql from a migration

--- db/structure.sql
+++ db/structure.sql
@@ -215,7 +215,8 @@ CREATE TABLE events (
     created_at timestamp without time zone NOT NULL,
     updated_at timestamp without time zone NOT NULL,
     target2_type character varying(255),
-    target2_id integer
+    target2_id integer,
+    additional_data text
 );
 
 
@@ -1243,6 +1244,8 @@ INSERT INTO schema_migrations (version) VALUES ('20120613231928');
 INSERT INTO schema_migrations (version) VALUES ('20120614002526');
 
 INSERT INTO schema_migrations (version) VALUES ('20120614211513');
+
+INSERT INTO schema_migrations (version) VALUES ('20120615184353');
 
 INSERT INTO schema_migrations (version) VALUES ('20120615192737');
 
