1534096ee4d2297c83ea3c8057b07bdbc62e7c99
Corina Lawu, Max Brunsfeld, Phil Goodwin & Vinicius Fuentes <pair+corina+mbrunsfeld+phil+vinicius@pivotallabs.com>
2012-06-21 12:23:23 -0700

    Creating Hadoop Instance migrator [#31491749]

---
+++ lib/data_migration/hadoop_instance_migrator.rb
@@ -0,0 +1,38 @@
+class HadoopInstanceMigrator
+  def migrate
+    unless Legacy.connection.column_exists?(:edc_instance, :chorus_rails_instance_id)
+      Legacy.connection.add_column :edc_instance, :chorus_rails_instance_id, :integer
+    end
+
+    legacy_instances.each do |instance|
+      new_instance = HadoopInstance.new
+
+      new_instance.name = instance["name"]
+      new_instance.description = instance["description"]
+      new_instance.host = instance["host"]
+      new_instance.port = instance["port"].to_i
+      new_instance.owner_id = instance["chorus_rails_user_id"]
+
+      credentials = instance['db_user_name'].split(',')
+      new_instance.username = credentials[0]
+      new_instance.group_list = credentials[1..-1].join(",")
+
+
+      new_instance.save!
+
+      id = instance["id"]
+      Legacy.connection.update("Update edc_instance SET chorus_rails_instance_id = #{new_instance.id} WHERE id = '#{id}'")
+    end
+  end
+
+  def legacy_instances
+    Legacy.connection.select_all(<<SQL)
+      SELECT edc_instance.*, edc_user.chorus_rails_user_id, am.db_user_name
+      FROM edc_instance
+      INNER JOIN edc_user ON edc_instance.owner = edc_user.user_name
+      INNER JOIN edc_account_map am ON am.instance_id = edc_instance.id
+      WHERE instance_provider = 'Hadoop' AND edc_instance.is_deleted = 'f'
+SQL
+  end
+end
+
---
+++ spec/lib/data_migration/hadoop_instance_migrator_spec.rb
@@ -0,0 +1,41 @@
+require 'spec_helper'
+
+describe HadoopInstanceMigrator, :type => :data_migration do
+  describe ".migrate" do
+    describe "the new foreign key column" do
+      before(:each) do
+        Legacy.connection.column_exists?(:edc_instance, :chorus_rails_instance_id).should be_false
+      end
+
+      it "adds the new foreign key column" do
+        UserMigrator.new.migrate
+        HadoopInstanceMigrator.new.migrate
+        Legacy.connection.column_exists?(:edc_instance, :chorus_rails_instance_id).should be_true
+      end
+    end
+
+    describe "copying the data" do
+      before do
+        UserMigrator.new.migrate
+        HadoopInstanceMigrator.new.migrate
+      end
+
+      it "creates new instances for legacy hadoop instances" do
+        HadoopInstance.count.should == 2
+      end
+
+      it "copies the correct data fields from the legacy instance" do
+        HadoopInstanceMigrator.new.legacy_instances.each do |legacy_instance|
+          instance = HadoopInstance.find(legacy_instance["chorus_rails_instance_id"])
+          instance.name.should == legacy_instance["name"]
+          instance.username.should == legacy_instance["db_user_name"].split(',')[0]
+          instance.group_list.should == legacy_instance["db_user_name"].split(',')[1..-1].join(",")
+          instance.description.should == legacy_instance["description"]
+          instance.host.should == legacy_instance["host"]
+          instance.port.should == legacy_instance["port"].to_i
+          instance.owner.should == User.find_by_username(legacy_instance["owner"])
+        end
+      end
+    end
+  end
+end
