6cb54c3f1367009250819bf151dc37b427713fbe
Boxiong Ding & Corina Lawu <pair+boxiong+corina@pivotallabs.com>
2012-06-21 15:24:38 -0700

    Delete dataset association api docs
    [#27928685]

---
+++ spec/acceptance/dataset_association_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+resource "Dataset Association" do
+  let!(:user) { FactoryGirl.create :user }
+  let!(:gpdb_table) { FactoryGirl.create :gpdb_table }
+  let!(:workspace) { FactoryGirl.create :workspace }
+  let!(:membership) { FactoryGirl.create :membership, :workspace => workspace, :user => user}
+  let!(:association) { FactoryGirl.create(:associated_dataset, :dataset => gpdb_table, :workspace => workspace)}
+  let(:workspace_id) { workspace.id }
+  let(:id) { gpdb_table.id }
+
+  before do
+    log_in user
+  end
+
+  delete "/workspaces/:workspace_id/datasets/:id" do
+    example_request "Delete dataset association" do
+      status.should == 200
+    end
+  end
+
+end
