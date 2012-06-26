51eff0d8083ed1284eb3e1de7d14572f6e7694be
Bleicke Petersen & Brandon Liu <pair+bpetersen+brandon@pivotallabs.com>
2012-06-26 15:48:58 -0700

    Run all test suites regardless of failures [#31347569]

--- ci.sh
+++ ci.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 
 set -e
-
 eval "$(rbenv init -)"
 rbenv shell `cat .rbenv-version`
 ruby -v | grep "jruby 1.6.7"
 gem list bundler | grep bundler || gem install bundler
 bundle install
-rake legacy:setup db:migrate db:test:prepare db:test:prepare:legacy
+bundle exec rake legacy:setup db:migrate db:test:prepare db:test:prepare:legacy
 
-# Run rspec unit tests
-script/test 2>&1
-
-# Run Jasmine tests
-
+RAILS_ENV=development bundle exec rake devmode:enable assets:precompile
 # start jasmine
 bundle exec rake jasmine > $WORKSPACE/jasmine.log 2>&1 &
 jasmine_pid=$!
 echo "Jasmine process id is : $jasmine_pid"
 sleep 5
 
-RAILS_ENV=development rake devmode:enable assets:precompile
-rake phantom 2>&1
+set +e
+script/test 2>&1
+RUBY_TESTS_RESULT=$?
+bundle exec rake phantom 2>&1
+JS_TESTS_RESULT=$?
+script/test spec/integration/ 2>&1
+INTEGRATION_TESTS_RESULT=$?
+
+set -e
 echo "Cleaning up jasmine process $jasmine_pid"
 kill -s SIGINT $jasmine_pid
-
-# Run integration tests
-script/test spec/integration/ 2>&1
-
+SUCCESS=`expr $RUBY_TESTS_RESULT + $JS_TESTS_RESULT + $INTEGRATION_TESTS_RESULT`
+exit $SUCCESS
