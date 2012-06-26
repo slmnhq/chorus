24bc5ab431fb1b9c1c6dea172d39024b3b8e65a0
Max Brunsfeld & Nitin Grewal <pair+mbrunsfeld+nitin@pivotallabs.com>
2012-06-26 11:03:50 -0700

    Clean up activity presenter [finishes #29788355]

--- app/assets/javascripts/presenters/activity_presenters.js
+++ app/assets/javascripts/presenters/activity_presenters.js
@@ -1,102 +1,100 @@
 ;(function() {
-    var private, TYPE_OPTIONS = {
-        GREENPLUM_INSTANCE_CHANGED_NAME: {
-            links: [ "actor", "greenplumInstance" ],
-            attrs: [ "newName", "oldName" ]
-        },
-
-        HADOOP_INSTANCE_CHANGED_NAME: {
-            links: [ "actor", "hadoopInstance" ],
-            attrs: [ "newName", "oldName" ]
-        },
-
-        GREENPLUM_INSTANCE_CREATED: {
-            links: [ "actor", "greenplumInstance" ]
-        },
-
-        HADOOP_INSTANCE_CREATED: {
-            links: [ "actor", "hadoopInstance" ]
-        },
-
-        GREENPLUM_INSTANCE_CHANGED_OWNER: {
-            links: [ "actor", "greenplumInstance", "newOwner" ]
-        },
-
-        WORKFILE_CREATED: {
-            links: [ "actor", "workfile", "workspace" ]
-        },
-
-        WORKSPACE_ADD_SANDBOX: {
-            links: [ "actor", "sandbox_schema", "workspace" ]
-        },
-
-        SOURCE_TABLE_CREATED: {
-            links: [ "actor", "dataset", "workspace" ],
-            computed: [ "datasetType" ]
-        }
-    };
-
     chorus.presenters.Activity = chorus.presenters.Base.extend({
+        headerHtml: function() {
+            var string = t(private.headerTranslationKey(this), private.headerParams(this));
+            return new Handlebars.SafeString(string);
+        },
+
         timestamp: function() {
             return chorus.helpers.relativeTimestamp(this.model.get("timestamp"));
         },
 
-        headerHtml: function() {
-            var string = t(headerTranslationKey(this), headerParams(this));
-            return new Handlebars.SafeString(string);
-        },
-
         iconSrc: function() {
-            return actor(this).fetchImageUrl({ size: "icon" });
+            return this.model.actor().fetchImageUrl({ size: "icon" });
         },
 
         iconHref: function() {
-            return actor(this).showUrl();
+            return this.model.actor().showUrl();
         },
 
         iconClass: "profile"
     });
 
-    private = {
+    var private = {
+
+        headerParamOptions: {
+            GREENPLUM_INSTANCE_CHANGED_NAME: {
+                links: [ "actor", "greenplumInstance" ],
+                attrs: [ "newName", "oldName" ]
+            },
+
+            HADOOP_INSTANCE_CHANGED_NAME: {
+                links: [ "actor", "hadoopInstance" ],
+                attrs: [ "newName", "oldName" ]
+            },
+
+            GREENPLUM_INSTANCE_CREATED: {
+                links: [ "actor", "greenplumInstance" ]
+            },
+
+            HADOOP_INSTANCE_CREATED: {
+                links: [ "actor", "hadoopInstance" ]
+            },
+
+            GREENPLUM_INSTANCE_CHANGED_OWNER: {
+                links: [ "actor", "greenplumInstance", "newOwner" ]
+            },
+
+            WORKFILE_CREATED: {
+                links: [ "actor", "workfile", "workspace" ]
+            },
+
+            WORKSPACE_ADD_SANDBOX: {
+                links: [ "actor", "sandbox_schema", "workspace" ]
+            },
+
+            SOURCE_TABLE_CREATED: {
+                links: [ "actor", "dataset", "workspace" ],
+                computed: [ "datasetType" ]
+            }
+        },
+
+        headerParams: function(self) {
+            var model = self.model;
+            var action = model.get("action");
+
+            var params = {};
+            var options = private.headerParamOptions[action];
+
+            _.each(options.links, function(name) {
+                var associatedModel = model[name]();
+                params[name + "Link"] = private.modelLink(associatedModel);
+            });
+
+            _.each(options.attrs, function(name) {
+                params[name] = model.get(name);
+            });
+
+            _.each(options.computed, function(name) {
+                params[name] = private[name](self);
+            });
+
+            return params;
+        },
+
+        headerTranslationKey: function(self) {
+            return "activity.header." + self.model.get("action") + ".default";
+        },
+
         datasetType: function(self) {
             var type = self.model.dataset().metaType();
             return t("dataset.types." + type);
+        },
+
+        modelLink: function(model) {
+            return chorus.helpers.linkTo(model.showUrl(), model.name());
         }
+
     };
 
-
-    function headerTranslationKey(self) {
-        return "activity.header." + self.model.get("action") + ".default";
-    }
-
-    function headerParams(self) {
-        var model = self.model;
-        var action = model.get("action");
-
-        var params = {};
-        var options = TYPE_OPTIONS[action];
-
-        _.each(options.links, function(name) {
-            var associatedModel = model[name]();
-            params[name + "Link"] = modelLink(associatedModel);
-        });
-
-        _.each(options.attrs, function(name) {
-            params[name] = model.get(name);
-        });
-
-        _.each(options.computed, function(name) {
-            params[name] = private[name](self);
-        });
-
-        return params;
-    }
-
-    function actor(self) {
-        return self.model.actor();
-    }
-
-    function modelLink(model) {
-        return chorus.helpers.linkTo(model.showUrl(), model.name());
-    }
 })();
