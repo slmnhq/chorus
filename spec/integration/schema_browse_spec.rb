b16e308fb211ce5b65e125d06d0780479a7dce96
Brandon Liu & Max Brunsfeld <pair+brandon+mbrunsfeld@pivotallabs.com>
2012-07-02 15:21:26 -0700

    add waits to schema_browse_spec [#32093193]

--- spec/integration/schema_browse_spec.rb
+++ spec/integration/schema_browse_spec.rb
@@ -5,23 +5,18 @@ describe " add an instance " do
     login('edcadmin', 'secret')
   end
 
-  it "creates an instance" do
-    create_gpdb_gillette_instance(:name => "InstanceWithSchemas")
-    click_link("InstanceWithSchemas")
-    click_link("Analytics")
-    click_link("analytics")
-  end
-
-
   it "runs analyze on a table from instance browsing view" do
-      create_gpdb_gillette_instance(:name => "RunAnalyze")
-      click_link("RunAnalyze")
-      click_link("Analytics")
-      click_link("public")
-      click_link ("Run Analyze")
-      click_button "Run Analyze"
-      wait_for_ajax(10)
-      page.should have_content("Analyze is running")
+    create_gpdb_gillette_instance(:name => "RunAnalyze")
+    click_link("RunAnalyze")
+    wait_for_ajax
+    click_link("Analytics")
+    wait_for_ajax
+    click_link("public")
+    wait_for_ajax
+    click_link ("Run Analyze")
+    click_button "Run Analyze"
+    wait_for_ajax(10)
+    page.should have_content("Analyze is running")
   end
 
   xit "Associate a table/view with workspace (27928559)" do
