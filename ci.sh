08f40a066cfec482c90e2124af7359305e40add4
Max Brunsfeld & Vinicius Fuentes <pair+mbrunsfeld+vinicius@pivotallabs.com>
2012-07-03 11:21:58 -0700

    Fix ci database cleanup and create integration environment [#32093193]

--- ci.sh
+++ ci.sh
@@ -8,7 +8,7 @@ rbenv shell `cat .rbenv-version`
 ruby -v | grep "jruby 1.6.7"
 gem list bundler | grep bundler || gem install bundler
 bundle install
-bundle exec rake legacy:setup db:migrate db:test:prepare assets:precompile
+bundle exec rake legacy:setup db:drop db:create db:migrate db:seed assets:precompile
 
 # start jasmine
 bundle exec rake jasmine > $WORKSPACE/jasmine.log 2>&1 &
