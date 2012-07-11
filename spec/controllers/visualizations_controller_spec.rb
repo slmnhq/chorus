242242dcec67d7929a8ff380e3ce35f023926224
Max Brunsfeld, Phil Goodwin & Yaneeka Huq <pair+mbrunsfeld+phil+yaneeka@pivotallabs.com>
2012-07-11 14:05:51 -0700

    Add API for canceling visualization [#27928589]

--- spec/controllers/visualizations_controller_spec.rb
+++ spec/controllers/visualizations_controller_spec.rb
@@ -24,20 +24,19 @@ describe VisualizationsController do
             result.add_column('bucket', 'string')
             result.add_column('count', 'integer')
             result.add_rows([
-                                ['David Bowie', '12'],
-                                ['The Beatles', '9'],
-                                ['Vanilla Ice', '1']
-                            ])
+              ['David Bowie', '12'],
+              ['The Beatles', '9'],
+              ['Vanilla Ice', '1']
+            ])
           end
         end
       end
 
       it "returns json for visualization, in ascending order" do
-        post :create, :chart_task => {:type => "frequency"}, :dataset_id => dataset.id, :check_id => "43"
+        post :create, :chart_task => {:type => "frequency", :check_id => "43"}, :dataset_id => dataset.id
         response.status.should == 200
 
         rows = decoded_response["rows"]
-
         rows[0][:bucket].should == "Vanilla Ice"
         rows[0][:count].should == "1"
         rows[1][:bucket].should == "The Beatles"
@@ -46,7 +45,6 @@ describe VisualizationsController do
         rows[2][:count].should == "12"
 
         columns = decoded_response["columns"]
-
         columns[0][:name].should == "bucket"
         columns[0][:type_category].should == "STRING"
         columns[1][:name].should == "count"
@@ -54,22 +52,35 @@ describe VisualizationsController do
       end
 
       generate_fixture "frequencyTask.json" do
-        post :create, :chart_task => {:type => "frequency"}, :dataset_id => dataset.id
+        post :create, :chart_task => {:type => "frequency", :check_id => "43"}, :dataset_id => dataset.id
+      end
+    end
+
+    context "when there's an error'" do
+      before do
+        any_instance_of(Visualization::Histogram) do |visualization|
+          stub(visualization).fetch!(instance_account, '0.43214321') { raise CancelableQuery::QueryError }
+        end
+      end
+
+      it "returns an error if the query fails" do
+        post :create, :chart_task => {:type => "histogram", :check_id => '0.43214321'}, :dataset_id => dataset.id
+        response.code.should == "400"
+        decoded_errors.fields.query.INVALID.message.should_not be_nil
       end
     end
   end
 
-  context "when there's an error'" do
-    before do
-      any_instance_of(Visualization::Histogram) do |visualization|
-        stub(visualization).fetch!(instance_account, '0.43214321') { raise CancelableQuery::QueryError }
-      end
+  describe "#destroy" do
+    it "cancels the visualization query" do
+      mock(SqlExecutor).cancel_query(dataset, instance_account, "43")
+      delete :destroy, :id => "43", :dataset_id => dataset.to_param
     end
-    it "returns an error if the query fails" do
-      post :create, :chart_task => {:type => "histogram"}, :dataset_id => dataset.id, :check_id => '0.43214321'
 
-      response.code.should == "400"
-      decoded_errors.fields.query.INVALID.message.should_not be_nil
+    it "returns successfully" do
+      mock(SqlExecutor).cancel_query(dataset, instance_account, "43")
+      delete :destroy, :id => "43", :dataset_id => dataset.to_param
+      response.should be_success
     end
   end
 end
