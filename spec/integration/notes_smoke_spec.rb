846d051e1fdfd0bf76310b0f479ef35f6d872384
Manikandan Pushpavaneswaran <pair+manikandan@pivotallabs.com>
2012-07-23 14:43:27 -0700

    refactoring moving around few tests to be included in smoke

--- spec/integration/notes_smoke_spec.rb
+++ spec/integration/notes_smoke_spec.rb
@@ -35,3 +35,41 @@ describe "creating a note on a workspace" do
     page.find(".activity_content").should have_content("Note on the workspace")
   end
 end
+
+
+describe "creating a note on a hadoop instance" do
+  it "contains the new note on gpdb" do
+    login('edcadmin', 'secret')
+    new_instance_name = "HD_inst_sel_test#{Time.now.to_i}"
+    create_valid_hadoop_instance(:name => new_instance_name)
+    wait_until { page.has_selector?('a[data-dialog="NotesNew"]') }
+    sleep(1)
+    click_link "Add a note"
+
+    within_modal do
+      set_cleditor_value("body", "Note on the hadoop instance")
+      click_submit_button
+      wait_for_ajax
+    end
+
+    page.should have_content("Note on the hadoop instance")
+  end
+end
+
+describe "creating a note on a workfile" do
+
+  it "contains a note on the workfile list page" do
+    login('edcadmin', 'secret')
+    create_valid_workspace
+    create_valid_workfile(:name => "notewf")
+    click_link "Add a note"
+    within_modal do
+      set_cleditor_value("body", "Note on a workfile")
+      click_submit_button
+      wait_for_ajax
+    end
+    page.should have_content("Note on a workfile")
+
+  end
+
+end
--- spec/regression/adding_note_spec.rb
+++ spec/regression/adding_note_spec.rb
@@ -1,24 +1,5 @@
 require File.join(File.dirname(__FILE__), '../integration/spec_helper.rb')
 
-describe "creating a note on a hadoop instance" do
-  it "contains the new note on gpdb" do
-    login('edcadmin', 'secret')
-    new_instance_name = "HD_inst_sel_test#{Time.now.to_i}"
-    create_valid_hadoop_instance(:name => new_instance_name)
-    wait_until { page.has_selector?('a[data-dialog="NotesNew"]') }
-    sleep(1)
-    click_link "Add a note"
-
-    within_modal do
-      set_cleditor_value("body", "Note on the hadoop instance")
-      click_submit_button
-      wait_for_ajax
-    end
-
-    page.should have_content("Note on the hadoop instance")
-  end
-end
-
 describe "creating a note on the hadoop file" do
 
 it "contains the new note on hadoop" do
@@ -64,58 +45,3 @@ describe "creating a note on a dataset" do
 
 end
 
-
-describe "creating a note on a workfile" do
-
-  it "contains a note on the workfile list page" do
-    login('edcadmin', 'secret')
-    create_valid_workspace
-    create_valid_workfile(:name => "notewf")
-    click_link "Add a note"
-    within_modal do
-      set_cleditor_value("body", "Note on a workfile")
-      click_submit_button
-      wait_for_ajax
-    end
-    page.should have_content("Note on a workfile")
-
-  end
-
-  it "contains a note on the workfile show page" do
-
-    login('edcadmin', 'secret')
-    create_valid_workspace
-    create_valid_workfile(:name => "notewf1")
-    click_link "notewf1"
-    wait_for_ajax
-    click_link "Add a note"
-    within_modal do
-      set_cleditor_value("body", "Note on a workfile show page")
-      click_submit_button
-      wait_for_ajax
-    end
-    page.should have_content("Note on a workfile show page")
-
-  end
-
-
-end
-
-describe "creating a note on the workspace" do
-
-  it "contains the new note on workspace" do
-    login('edcadmin', 'secret')
-    create_valid_workspace
-    wait_until { page.has_selector?('a[data-dialog="NotesNew"]') }
-    sleep(1)
-    click_link "Add a note"
-
-    within_modal do
-      set_cleditor_value("body", "Note on the workspace")
-      click_submit_button
-      wait_for_ajax
-    end
-
-    page.should have_content("Note on the workspace")
-  end
-end
