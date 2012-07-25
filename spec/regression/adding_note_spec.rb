a9c65484b85bbdb922781a7ebc1d063304fdd0cb
Manikandan Pushpavaneswaran <manikandan.pushpavaneswaran@emc.com>
2012-07-25 14:47:32 -0700

    automation for Deleting a note
    [#27928623]
    
    
    
    Signed-off-by: Manikandan Pushpavaneswaran & Aleksey Myagkov <manikandan.pushpavaneswaran@emc.com> & <aleksey.myagkov@emc.com>

--- spec/regression/adding_note_spec.rb
+++ spec/regression/adding_note_spec.rb
@@ -45,3 +45,34 @@ describe "creating a note on a dataset" do
 
 end
 
+describe "deleting a note on a dataset" do
+
+  it "deletes the note" do
+    login('edcadmin','secret')
+    create_valid_workspace(:name => "Note Delete", :shared => true)
+    wait_for_ajax
+    click_link "Add a note"
+    within_modal do
+      set_cleditor_value("body", "This note will be deleted")
+      click_submit_button
+      wait_for_ajax
+    end
+    page.execute_script("$('{.delete_link.alert}').click();")
+    click_submit_button
+    page.should_not have_content("EDC Admin commented on the workspace Note Delete")
+
+    go_to_home_page
+    wait_for_ajax
+    page.should_not have_content("EDC Admin commented on the workspace Note Delete")
+
+    go_to_user_list_page
+    wait_for_ajax
+    within(".list")  do
+      click_link "EDC Admin"
+    end
+    wait_for_ajax
+    page.should_not have_content("EDC Admin commented on the workspace Note Delete")
+
+  end
+
+end
