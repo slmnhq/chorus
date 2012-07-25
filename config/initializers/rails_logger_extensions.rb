003b5548141d8394902387ff158a87e5786c8f39
Corina Lawu & Max Brunsfeld <pair+corina+mbrunsfeld@pivotallabs.com>
2012-07-24 17:26:22 -0700

    Make request ids appear on the right lines in logs
    [#33022043]

---
+++ config/initializers/rails_logger_extensions.rb
@@ -0,0 +1,25 @@
+module Rails
+  module Rack
+    class Logger
+
+      # This is a monkey-patch to the this file:
+      #
+      # /railties-3.2.5/lib/rails/rack/logger.rb
+      #
+      # All we did was remove some newlines from the logged string,
+      # so that request ids would appear on the *same* line as the
+      # 'starting request' message.
+      #
+      def call_app(env)
+        request = ActionDispatch::Request.new(env)
+        path = request.filtered_path
+        Rails.logger.info "Started #{request.request_method} \"#{path}\" for #{request.ip} at #{Time.now.to_default_s}"
+        @app.call(env)
+      ensure
+        ActiveSupport::LogSubscriber.flush_all!
+      end
+
+    end
+  end
+end
+
