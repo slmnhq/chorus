01e47850b77977cf52258def5b21bbe783968d14
Aleksey Myagkov <pair+aleksey@pivotallabs.com>
2012-07-24 17:04:01 -0700

    Adding sandbox test for a private workspace

--- spec/integration/sandbox_smoke_spec.rb
+++ spec/integration/sandbox_smoke_spec.rb
@@ -5,15 +5,18 @@ describe "Create Sandbox" do
     login('edcadmin', 'secret')
   end
 
-  xit "creates sandbox in private workspace" do
+  it "creates sandbox in private workspace" do
     go_to_workspace_page
+    create_gpdb_instance(:name => "Gillette")
     create_valid_workspace(:name => "Private Workspace", :shared => false)
-    fill_in 'name', :with => "Private Workspace"
-    uncheck ("Make this workspace publicly available")
-    create_valid_user(:username => "private")
-    login('private','secret')
-    go_to_workspace_page
-    page.should_not have_content ("Private Workspace")
+    #Sandbox
+    click_link "Add a sandbox"
+    page.execute_script %( $("select[name='instance'").selectmenu("value", "1"); )
+    sleep(10)
+    #page.mouseDown('div.instance div.select_container span a span.ui-selectmenu-status')
+    #page.select(1, 'div.instance div.select_container span a span.ui-selectmenu-status')
+
+    #page.execute_script('.ui-selectmenu-open a:contains(gillette)')
   end
 
   xit "creates sandbox in public workspace" do
