fc7dba0bfaa1d6b02ed1f052a78a83724b73b7a5
Manikandan Pushpavaneswaran <pair+manikandan@pivotallabs.com>
2012-07-30 11:26:12 -0700

    completes automation for User can request visualization data for frequency
    [#29662167]
    
    Signed-off-by: Manikandan Pushpavaneswaran <pair+manikandan@pivotallabs.com>

---
+++ spec/regression/visualization_spec.rb
@@ -0,0 +1,34 @@
+require File.join(File.dirname(__FILE__), '../integration/spec_helper')
+
+#These tests actually create the charts from the instances view. Need to write separate tests for visulaization from sandbox
+
+describe "Create frequency plot" do
+
+  it "creates frequency plot" do
+
+    login('edcadmin','secret')
+    create_gpdb_instance(:name => "frequency_plot")
+    wait_for_ajax
+    click_link "frequency_plot"
+    wait_for_ajax
+    click_link "ChorusAnalytics"
+    wait_for_ajax
+    click_link "analytics"
+    wait_for_ajax
+    click_link "2009_sfo_customer_survey"
+    wait_for_ajax
+    click_button "Visualize"
+
+    click_button "Create Chart"
+    within_modal do
+      page.should have_content "Visualization: 2009_sfo_customer_survey"
+      click_link "Show Data Table"
+      page.should have_content "Results Console"
+      click_link "Hide Data Table"
+      click_button "Close"
+    end
+
+  end
+
+
+end
