4a851b210becd52aab7f37590a8d7e9f1ba86391
Joseph Palermo & Yaneeka Huq <pair+joseph+yaneeka@pivotallabs.com>
2012-07-30 11:47:42 -0700

    Avoid using model in migration [#32156187]

--- db/migrate/20120511233557_change_hadoop_instance_state_to_online.rb
+++ db/migrate/20120511233557_change_hadoop_instance_state_to_online.rb
@@ -1,15 +1,13 @@
 class ChangeHadoopInstanceStateToOnline < ActiveRecord::Migration
   def up
     add_column :hadoop_instances, :online, :boolean, :default => true
-    HadoopInstance.reset_column_information
-    HadoopInstance.where("state <> 'online'").update_all(:online => false)
+    execute("UPDATE hadoop_instances SET online = f WHERE state <> 'online'")
     remove_column :hadoop_instances, :state
   end
 
   def down
     add_column :hadoop_instances, :state, :string, :default => "offline"
-    HadoopInstance.reset_column_information
-    HadoopInstance.where("online").update_all(:state => "online")
+    execute("UPDATE hadoop_instances SET state = 'online' WHERE online = t")
     remove_column :hadoop_instances, :online
   end
 end
