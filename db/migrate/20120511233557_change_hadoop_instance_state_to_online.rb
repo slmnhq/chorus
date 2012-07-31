3e39bc6833fb503bcd6e00c928b5b71f9aa3934d
Joseph Palermo, Vinicius Fuentes & Yaneeka Huq <pair+joseph+vinicius+yaneeka@pivotallabs.com>
2012-07-30 17:27:35 -0700

    Migrate without the usage of models

--- db/migrate/20120511233557_change_hadoop_instance_state_to_online.rb
+++ db/migrate/20120511233557_change_hadoop_instance_state_to_online.rb
@@ -1,13 +1,13 @@
 class ChangeHadoopInstanceStateToOnline < ActiveRecord::Migration
   def up
     add_column :hadoop_instances, :online, :boolean, :default => true
-    execute("UPDATE hadoop_instances SET online = f WHERE state <> 'online'")
+    execute("UPDATE hadoop_instances SET online = 'f' WHERE state <> 'online'")
     remove_column :hadoop_instances, :state
   end
 
   def down
     add_column :hadoop_instances, :state, :string, :default => "offline"
-    execute("UPDATE hadoop_instances SET state = 'online' WHERE online = t")
+    execute("UPDATE hadoop_instances SET state = 'online' WHERE online = 't'")
     remove_column :hadoop_instances, :online
   end
 end
