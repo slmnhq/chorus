97b45f1d478bc050e2abc3b92f15978051d96475
Bleicke Petersen & Boxiong Ding <pair+bpetersen+boxiong@pivotallabs.com>
2012-08-07 12:18:40 -0700

    Add gpfdist config params to chorus.yml, and to config controller [#33657335]

--- app/controllers/configurations_controller.rb
+++ app/controllers/configurations_controller.rb
@@ -5,7 +5,13 @@ class ConfigurationsController < ApplicationController
   skip_before_filter :require_login, :only => :version
 
   def show
-    render :json => { :response => { :external_auth_enabled => LdapClient.enabled? } }
+    render :json => { :response => {
+      :external_auth_enabled => LdapClient.enabled?,
+      :gpfdist_write_permissions => gpfdist_write_permissions,
+      :gpfdist_url => Chorus::Application.config.chorus['gpfdist']['url'],
+      :gpfdist_port => Chorus::Application.config.chorus['gpfdist']['port'],
+      :gpfdist_data_dir => Chorus::Application.config.chorus['gpfdist']['data_dir']
+    } }
   end
 
   def version
@@ -15,5 +21,12 @@ class ConfigurationsController < ApplicationController
   def build_string
     f = File.join(Rails.root, 'version_build')
     File.exists?(f) ? File.read(f) : Chorus::VERSION::STRING
+  end
+
+  private
+
+  def gpfdist_write_permissions
+    path = Chorus::Application.config.chorus['gpfdist']['data_dir']
+    File.directory?(path) && File.writable?(path)
   end
 end
--- spec/controllers/configurations_controller_spec.rb
+++ spec/controllers/configurations_controller_spec.rb
@@ -6,12 +6,33 @@ describe ConfigurationsController do
       @user = users(:alice)
       log_in @user
       mock(LdapClient).enabled? { true }
+      stub(File).directory? { true }
     end
 
     it "includes the ldap status" do
       get :show
       response.code.should == "200"
       decoded_response.external_auth_enabled.should == true
+    end
+
+    it "includes the gpfdist config and write permissions" do
+      stub(File).writable? { true }
+      get :show
+      response.code.should == "200"
+      decoded_response.gpfdist_write_permissions.should == true
+      decoded_response.gpfdist_url.should == "localhost"
+      decoded_response.gpfdist_port.should == 8181
+      decoded_response.gpfdist_data_dir.should == "/tmp"
+    end
+
+    it "includes the gpfdist config with no write permissions" do
+      stub(File).writable? { false }
+      get :show
+      response.code.should == "200"
+      decoded_response.gpfdist_write_permissions.should == false
+      decoded_response.gpfdist_url.should == "localhost"
+      decoded_response.gpfdist_port.should == 8181
+      decoded_response.gpfdist_data_dir.should == "/tmp"
     end
   end
 
