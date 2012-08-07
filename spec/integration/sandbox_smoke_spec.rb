10e55aa98acb07ce40a684bc1e1571dda1ae02e3
Aleksey Myagkov <pair+aleksey@pivotallabs.com>
2012-08-06 19:00:30 -0700

    Creates sandbox in private workspace

--- spec/integration/sandbox_smoke_spec.rb
+++ spec/integration/sandbox_smoke_spec.rb
@@ -5,18 +5,30 @@ describe "Create Sandbox" do
     login('edcadmin', 'secret')
   end
 
-  xit "creates sandbox in private workspace" do
-    go_to_workspace_page
-    create_gpdb_instance(:name => "Gillette")
-    create_valid_workspace(:name => "Private Workspace", :shared => false)
-    #Sandbox
-    click_link "Add a sandbox"
-    page.execute_script %( $("select[name='instance'").selectmenu("value", "1"); )
-    sleep(10)
-    #page.mouseDown('div.instance div.select_container span a span.ui-selectmenu-status')
-    #page.select(1, 'div.instance div.select_container span a span.ui-selectmenu-status')
+  it "creates sandbox in private workspace" do
 
-    #page.execute_script('.ui-selectmenu-open a:contains(gillette)')
+    inst_name = "Instance_gpdb40_sand"
+    create_gpdb_instance(:name => inst_name)
+    instance_id = Instance.find_by_name(inst_name).id
+    #database_id = GpdbDatabase.find_all_by_name
+    go_to_workspace_page
+    create_valid_workspace(:name => "Private Workspace", :shared => false)
+
+    click_link "Add a sandbox"
+    wait_for_ajax(5)
+    #instance
+    page.execute_script("$('select[name=instance]').selectmenu('value', '#{instance_id}')")
+    page.execute_script("$('.instance .select_container select').change();")
+    wait_for_ajax(5)
+    #database
+    page.execute_script("$('select[name=database]').selectmenu('value', '1')")
+    page.execute_script("$('.database .select_container select').change();")
+    wait_for_ajax(5)
+    #schema
+    page.execute_script("$('select[name=schema]').selectmenu('value', '1')")
+    page.execute_script("$('.schema .select_container select').change();")
+
+    click_submit_button
   end
 
   xit "creates sandbox in public workspace" do
