4094998214cbe269313e00b7a5bffc9bb3dbb997
Gregg Van Hove, Jesse Smith & Vinicius Fuentes <pair+gvanhove+jsmith+vinicius@pivotallabs.com>
2012-08-06 17:28:36 -0700

    Validate install script being run by non-root; link shared files and configs, exclude config files from install script build; copying the correct postgres build based on linux distro [#33311323]

--- packaging/install.rb
+++ packaging/install.rb
@@ -1,19 +1,89 @@
-#!/usr/bin/env ruby
+#!./chorus_installation/bin/ruby
 
 require 'fileutils'
 require 'securerandom'
+require 'yaml'
 
 class Install
+  InstallationFailed = Class.new(StandardError)
+  NonRootValidationError = Class.new(StandardError)
+
   attr_accessor :destination_path, :database_password, :database_user
 
+  def initialize(installer_home)
+    @installer_home = installer_home
+  end
+
+  def prompt(message, default="")
+    puts message
+  end
+
+  def validate_non_root
+    raise NonRootValidationError if Process.uid == 0
+  end
+
+  def chorus_installation_path
+    File.join(@installer_home, 'chorus_installation')
+  end
+  
+  def determine_postgres_installer
+    @postgres_package = get_postgres_build
+  end
+
   def get_destination_path
-    puts "Please enter Chorus destination path [/opt/chorus]:"
-    @destination_path = get_input || "/opt/chorus"
+    prompt "Please enter Chorus destination path [/opt/chorus]:"
+    @destination_path = File.expand_path(get_input || "/opt/chorus")
+  end
+
+  def get_postgres_build
+    input = nil
+    input = 3 if is_supported_suse?
+
+    redhat_version = supported_redhat_version
+    input = 1 if redhat_version == '5.5'
+    input = 2 if redhat_version == '6.2'
+
+    while !(1..4).include?(input)
+      prompt "Could not detect your Linux version. Please select one of the following:"
+      prompt "[1] - RedHat (CentOS/RHEL) 5.5 or compatible"
+      prompt "[2] - RedHat (CentOS/RHEL) 6.2 or compatible"
+      prompt "[3] - SuSE Enterprise Linux Server 11 or compatible"
+      prompt "[4] - Abort install"
+      input = get_input.to_i
+    end
+
+    case input
+      when 1
+        "postgres-redhat5.5-9.1.4.tar.gz"
+      when 2
+        "postgres-redhat6.2-9.1.4.tar.gz"
+      when 3
+        "postgres-suse11-9.1.4.tar.gz"
+      else
+        raise InstallationFailed, "Version not supported"
+    end
+  end
+
+  def supported_redhat_version
+    return nil unless File.exists?('/etc/redhat-release')
+
+    version_string = File.open('/etc/redhat-release').read
+    version_string =~ /release (\d\.\d)/
+    found_version = $1
+    found_version if %w(5.5 6.2).include?(found_version)
+  end
+
+  def is_supported_suse?
+    return false unless File.exists?('/etc/SuSE-release')
+
+    File.open('/etc/SuSE-release').readlines.any? do |release|
+      release.match(/^VERSION = 11$/)
+    end
   end
 
   def copy_chorus_to_destination
     FileUtils.mkdir_p(release_path)
-    FileUtils.cp_r './chorus_installation/.', release_path
+    FileUtils.cp_r File.join(chorus_installation_path, '.'), release_path, :preserve => true
   end
 
   def create_shared_structure
@@ -25,14 +95,24 @@ class Install
 
   def copy_config_files
     FileUtils.mkdir_p("#{destination_path}/shared")
-    FileUtils.cp('./chorus_installation/config/database.yml.example', "#{destination_path}/shared/database.yml")
-    FileUtils.cp('./chorus_installation/config/chorus.yml.example', "#{destination_path}/shared/chorus.yml")
+    FileUtils.cp("#{chorus_installation_path}/packaging/database.yml.example", "#{destination_path}/shared/database.yml")
+    FileUtils.cp("#{chorus_installation_path}/config/chorus.yml.example", "#{destination_path}/shared/chorus.yml")
   end
 
   def link_services
     FileUtils.ln_sf("#{release_path}/postgres", "#{destination_path}/postgres")
     FileUtils.ln_sf("#{release_path}/nginx_dist", "#{destination_path}/nginx_dist")
     FileUtils.ln_sf("#{release_path}/packaging/server_control.sh", "#{destination_path}/server_control.sh")
+  end
+
+  def link_shared_files
+    FileUtils.ln_sf("#{destination_path}/shared/chorus.yml", "#{release_path}/config/chorus.yml")
+    FileUtils.ln_sf("#{destination_path}/shared/database.yml", "#{release_path}/config/database.yml")
+
+    FileUtils.ln_sf("#{destination_path}/shared/tmp", "#{release_path}/tmp")
+    FileUtils.ln_sf("#{destination_path}/shared/solr", "#{release_path}/solr")
+    FileUtils.ln_sf("#{destination_path}/shared/log", "#{release_path}/log")
+    FileUtils.ln_sf("#{destination_path}/shared/system", "#{release_path}/system")
   end
 
   def create_database
@@ -54,15 +134,24 @@ class Install
   end
 
   def create_database_structure
-    system "#{destination_path}/server_control.sh start postgres"
+    system "cd #{destination_path} && ./server_control.sh start postgres > /dev/null"
     sleep 2
-    system %Q{#{destination_path}/postgres/bin/psql -d postgres -p8543 -c "CREATE ROLE '#{database_user}' PASSWORD '#{database_password}' SUPERUSER CREATEDB CREATEROLE INHERIT LOGIN}
-    system "cd #{release_path} && RAILS_ENV=production bin/rake db:create db:migrate"
-    system "#{destination_path}/server_control.sh stop postgres"
+    system %Q{#{destination_path}/postgres/bin/psql -d postgres -p8543 -c "CREATE ROLE #{database_user} PASSWORD '#{database_password}' SUPERUSER CREATEDB CREATEROLE INHERIT LOGIN" > /dev/null}
+    system "cd #{release_path} && RAILS_ENV=production bin/rake db:create db:migrate > /dev/null"
+    system "cd #{destination_path} && ./server_control.sh stop postgres > /dev/null"
   end
 
   def link_current_to_release
     FileUtils.ln_sf("#{release_path}", "#{destination_path}/current")
+  end
+
+  def extract_nginx
+    system("tar xzf #{release_path}/packaging/nginx_dist-1.2.2.tar.gz -C #{release_path}/")
+  end
+
+  def extract_postgres
+
+    system("tar xzf #{release_path}/packaging/postgres/#{@postgres_package} -C #{release_path}/")
   end
 
   private
@@ -72,7 +161,7 @@ class Install
   end
 
   def version
-    File.read('./chorus_installation/VERSION').strip
+    File.read("#{chorus_installation_path}/version_build").strip
   end
 
   def release_path
@@ -80,19 +169,43 @@ class Install
   end
 end
 
-
-
-
 if __FILE__ == $0
-  install = Install.new
+  begin
+    install = Install.new(File.dirname(__FILE__))
 
-  install.get_destination_path
-  install.copy_chorus_to_destination
-  install.create_shared_structure
-  install.copy_config_files
-  install.create_database_config
-  install.link_services
-  install.create_database_structure
-  install.link_current_to_release
+    install.validate_non_root
+    install.get_destination_path
+    install.determine_postgres_installer
+
+    print "Copying files into #{install.destination_path}..."
+    install.copy_chorus_to_destination
+    install.create_shared_structure
+    install.copy_config_files
+    install.create_database_config
+    install.link_shared_files
+    puts "Done"
+
+    print "Extracting NGINX..."
+    install.extract_nginx
+    puts "Done"
+
+    print "Extracting postgres..."
+    install.extract_postgres
+    puts "Done"
+
+    install.link_services
+
+    print "Creating database..."
+    install.create_database
+    install.create_database_structure
+    puts "Done"
+
+    install.link_current_to_release
+
+    puts "Installation completed."
+    puts "run ./server_control.sh start from #{install.destination_path} to start everything up!"
+  rescue Install::NonRootValidationError
+    puts "Aborting install: Please run the installer as a non-root user."
+  end
 end
 
--- spec/script/install_spec.rb
+++ spec/script/install_spec.rb
@@ -8,7 +8,10 @@ end
 describe "Install" do
   include FakeFS::SpecHelpers
 
-  let(:installer) { Install.new }
+  let(:installer) { Install.new('.') }
+  before do
+    stub(installer).prompt(anything)
+  end
 
   describe "#get_destination_path" do
     context "user types input" do
@@ -26,6 +29,118 @@ describe "Install" do
         installer.destination_path.should == "/opt/chorus"
       end
     end
+
+    describe "when the user types a relative path" do
+      it "returns the expanded path" do
+        stub_input "~/chorus"
+        installer.get_destination_path
+        installer.destination_path.should == "#{ENV['HOME']}/chorus"
+      end
+    end
+  end
+
+  describe "#get_postgres_build" do
+    context "when Linux is CentOS/RHEL 5.5" do
+      before do
+        FileUtils.mkdir_p("/etc/")
+        File.open("/etc/redhat-release", "w") { |f| f.puts "XXXXXXXXX release 5.5 (Final)" }
+      end
+
+      it "returns the RedHat 5.5 build" do
+        installer.get_postgres_build.should == 'postgres-redhat5.5-9.1.4.tar.gz'
+      end
+    end
+
+    context "when Linux is CentOS/RHEL 6.2" do
+      before do
+        FileUtils.mkdir_p("/etc/")
+        File.open("/etc/redhat-release", "w") { |f| f.puts "XXXXXXXXX release 6.2 (Final)" }
+      end
+
+      it "returns the RedHat 6.2 build" do
+        installer.get_postgres_build.should == 'postgres-redhat6.2-9.1.4.tar.gz'
+      end
+    end
+
+    context "when Linux is SLES 11" do
+      before do
+        FileUtils.mkdir_p("/etc/")
+        File.open("/etc/SuSE-release", "w") do |f|
+          f.puts "SuSE"
+          f.puts "VERSION = 11"
+          f.puts "PATCHLEVEL = 1"
+        end
+      end
+
+      it "returns the suse11 build" do
+        installer.get_postgres_build.should == 'postgres-suse11-9.1.4.tar.gz'
+      end
+    end
+
+    context "when couldn't guess version/distribution" do
+      before do
+        mock(installer).prompt("Could not detect your Linux version. Please select one of the following:").times(prompt_times)
+        mock(installer).prompt("[1] - RedHat (CentOS/RHEL) 5.5 or compatible").times(prompt_times)
+        mock(installer).prompt("[2] - RedHat (CentOS/RHEL) 6.2 or compatible").times(prompt_times)
+        mock(installer).prompt("[3] - SuSE Enterprise Linux Server 11 or compatible").times(prompt_times)
+        mock(installer).prompt("[4] - Abort install").times(prompt_times)
+      end
+
+      let(:prompt_times) { 1 }
+
+      context "when the user selects redhat 5.5" do
+        before do
+          stub_input("1")
+        end
+
+        it "returns the RedHat 5.5 build" do
+          installer.get_postgres_build.should == 'postgres-redhat5.5-9.1.4.tar.gz'
+        end
+      end
+
+      context "when the user selects redhat 6.2" do
+        before do
+          stub_input("2")
+        end
+
+        it "returns the RedHat 6.2 build" do
+          installer.get_postgres_build.should == 'postgres-redhat6.2-9.1.4.tar.gz'
+        end
+      end
+
+      context "when the user selects suse" do
+        before do
+          stub_input("3")
+        end
+
+        it "returns the Suse 11 build" do
+          installer.get_postgres_build.should == 'postgres-suse11-9.1.4.tar.gz'
+        end
+      end
+
+      context "when the user selects abort" do
+        before do
+          stub_input("4")
+        end
+
+        it "bails" do
+          expect { installer.get_postgres_build }.to raise_error(Install::InstallationFailed)
+        end
+      end
+
+      context "unsupported selected" do
+        before do
+          attempts = ["", "1"]
+          stub(installer).get_input() { attempts.shift }
+        end
+
+        let(:prompt_times) { 2 }
+
+        it "should show the menu again" do
+          installer.get_postgres_build
+        end
+      end
+    end
   end
 
   describe "#copy_chorus_to_destination" do
@@ -34,22 +149,13 @@ describe "Install" do
 
       FileUtils.mkpath './chorus_installation/'
       FileUtils.touch './chorus_installation/source_file_of_some_kind'
-      File.open('./chorus_installation/VERSION', 'w') { |f| f.puts "2.2.0.0" }
-    end
-
-    it "creates the destination path" do
-      installer.copy_chorus_to_destination
-      File.directory?('/opt/chorus').should be_true
+      stub_version
+      mock(FileUtils).cp_r('./chorus_installation/.', '/opt/chorus/releases/2.2.0.0', hash_including(:preserve => true))
     end
 
     it "creates the correct release path" do
       installer.copy_chorus_to_destination
       File.directory?('/opt/chorus/releases/2.2.0.0').should be_true
-    end
-
-    it "copies the files into the release path" do
-      installer.copy_chorus_to_destination
-      File.exists?('/opt/chorus/releases/2.2.0.0/source_file_of_some_kind').should be_true
     end
   end
 
@@ -88,7 +194,8 @@ describe "Install" do
       installer.destination_path = "/opt/chorus"
 
       FileUtils.mkdir_p './chorus_installation/config'
-      FileUtils.touch './chorus_installation/config/database.yml.example'
+      FileUtils.mkdir_p './chorus_installation/packaging'
+      FileUtils.touch './chorus_installation/packaging/database.yml.example'
       FileUtils.touch './chorus_installation/config/chorus.yml.example'
     end
 
@@ -108,30 +215,59 @@ describe "Install" do
   describe "#link_services" do
     before do
       installer.destination_path = "/opt/chorus"
-      FileUtils.mkdir_p './chorus_installation/postgres'
-      FileUtils.mkdir_p './chorus_installation/nginx_dist'
-      File.open('./chorus_installation/VERSION', 'w') { |f| f.puts "2.2.0.0" }
+      stub_version
+      FileUtils.mkdir_p(installer.destination_path)
     end
 
     it "creates a symlink to a postgres installation" do
-      installer.copy_chorus_to_destination
       installer.link_services
 
       File.readlink('/opt/chorus/postgres').should == '/opt/chorus/releases/2.2.0.0/postgres'
     end
 
     it "creates a symlink to a nginx installation" do
-      installer.copy_chorus_to_destination
       installer.link_services
 
       File.readlink('/opt/chorus/nginx_dist').should == '/opt/chorus/releases/2.2.0.0/nginx_dist'
     end
 
     it "creates a symlink to server_control.sh" do
-      installer.copy_chorus_to_destination
       installer.link_services
 
       File.readlink('/opt/chorus/server_control.sh').should == '/opt/chorus/releases/2.2.0.0/packaging/server_control.sh'
+    end
+  end
+
+  describe "#link_shared_files" do
+    before do
+      installer.destination_path = "/opt/chorus"
+      stub_version
+
+      installer.link_shared_files
+    end
+
+    it "links the chorus.yml file" do
+      File.readlink('/opt/chorus/releases/2.2.0.0/config/chorus.yml').should == '/opt/chorus/shared/chorus.yml'
+    end
+
+    it "links the database.yml file" do
+      File.readlink('/opt/chorus/releases/2.2.0.0/config/database.yml').should == '/opt/chorus/shared/database.yml'
+    end
+
+    it "links tmp" do
+      File.readlink('/opt/chorus/releases/2.2.0.0/tmp').should == '/opt/chorus/shared/tmp'
+    end
+
+    it "links solr" do
+      File.readlink('/opt/chorus/releases/2.2.0.0/solr').should == '/opt/chorus/shared/solr'
+    end
+
+    it "links log" do
+      File.readlink('/opt/chorus/releases/2.2.0.0/log').should == '/opt/chorus/shared/log'
+    end
+
+    it "links system" do
+      File.readlink('/opt/chorus/releases/2.2.0.0/system').should == '/opt/chorus/shared/system'
     end
   end
 
@@ -176,10 +312,10 @@ describe "Install" do
       mock(installer).version { "2.2.0.0" }
       mock(installer).sleep(2) { @call_order << "sleep" }
 
-      mock(installer).system("/opt/chorus/server_control.sh start postgres") { @call_order << 'start' }
-      mock(installer).system(%Q{/opt/chorus/postgres/bin/psql -d postgres -p8543 -c "CREATE ROLE 'the_user' PASSWORD 'secret' SUPERUSER CREATEDB CREATEROLE INHERIT LOGIN}) { @call_order << "create_user" }
-      mock(installer).system("cd /opt/chorus/releases/2.2.0.0 && RAILS_ENV=production bin/rake db:create db:migrate") { @call_order << "migrate" }
-      mock(installer).system("/opt/chorus/server_control.sh stop postgres") { @call_order << 'stop' }
+      mock(installer).system("cd /opt/chorus && ./server_control.sh start postgres > /dev/null") { @call_order << 'start' }
+      mock(installer).system(%Q{/opt/chorus/postgres/bin/psql -d postgres -p8543 -c "CREATE ROLE the_user PASSWORD 'secret' SUPERUSER CREATEDB CREATEROLE INHERIT LOGIN" > /dev/null}) { @call_order << "create_user" }
+      mock(installer).system("cd /opt/chorus/releases/2.2.0.0 && RAILS_ENV=production bin/rake db:create db:migrate > /dev/null") { @call_order << "migrate" }
+      mock(installer).system("cd /opt/chorus && ./server_control.sh stop postgres > /dev/null") { @call_order << 'stop' }
     end
 
     it "creates the database structure" do
@@ -202,7 +338,51 @@ describe "Install" do
     end
   end
 
+  describe "#extract_nginx" do
+    before do
+      stub_version
+      installer.destination_path = "/opt/chorus"
+    end
+
+    it "calls tar to unpack nginx" do
+      mock(installer).system("tar xzf /opt/chorus/releases/2.2.0.0/packaging/nginx_dist-1.2.2.tar.gz -C /opt/chorus/releases/2.2.0.0/")
+      installer.extract_nginx
+    end
+  end
+
+  describe "#extract_postgres" do
+    before do
+      stub_version
+      installer.destination_path = "/opt/chorus"
+    end
+
+    it "calls tar to unpack postgres" do
+      installer.instance_variable_set(:@postgres_package, 'postgres-blahblah.tar.gz')
+      mock(installer).system("tar xzf /opt/chorus/releases/2.2.0.0/packaging/postgres/postgres-blahblah.tar.gz -C /opt/chorus/releases/2.2.0.0/")
+      installer.extract_postgres
+    end
+  end
+
+  describe "#validate_non_root" do
+    context "when the script is being run by root"
+    it "raises an exception" do
+      mock(Process).uid() { 0 }
+      expect { installer.validate_non_root }.to raise_error(Install::NonRootValidationError)
+    end
+
+    context "when the script is not run as root" do
+      it "should not raise an exception" do
+        expect { installer.validate_non_root }.to_not raise_error()
+      end
+    end
+  end
+
   def stub_input(input)
     stub(installer).get_input() { input }
+  end
+
+  def stub_version
+    FileUtils.mkdir_p('./chorus_installation')
+    File.open('./chorus_installation/version_build', 'w') { |f| f.puts "2.2.0.0" }
   end
 end
