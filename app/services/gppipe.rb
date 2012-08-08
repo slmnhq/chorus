d59026b90e765e28c20b37c5e878d265d4660733
Brandon Liu & Jesse Smith <pair+brandon+jsmith@pivotallabs.com>
2012-08-07 18:09:35 -0700

    Refactor src and dst conn methods, cleanup cached connections in Gppipe [#27928567]

--- app/services/gppipe.rb
+++ app/services/gppipe.rb
@@ -30,39 +30,49 @@ class Gppipe
 
   def run
     pipe_file = File.join(GPFDIST_PIPE_DIR, pipe_name)
-    empty_table = (conn1.exec_query("SELECT count(*) from #{src_fullname};")[0]['count'] == 0)
-    table_def_rows = conn1.exec_query("SELECT column_name, data_type from information_schema.columns where table_name='#{src_table}' and table_schema='#{src_schema}';")
+    empty_table = (src_conn.exec_query("SELECT count(*) from #{src_fullname};")[0]['count'] == 0)
+    table_def_rows = src_conn.exec_query("SELECT column_name, data_type from information_schema.columns where table_name='#{src_table}' and table_schema='#{src_schema}';")
     table_definition = tabledef_from_query(table_def_rows)
 
     if empty_table
-      conn2.exec_query("CREATE TABLE #{dst_fullname}(#{table_definition})")
+      dst_conn.exec_query("CREATE TABLE #{dst_fullname}(#{table_definition})")
     else
       begin
         system "mkfifo #{pipe_file}"
-        conn2.exec_query("CREATE TABLE #{dst_fullname}(#{table_definition})")
+        dst_conn.exec_query("CREATE TABLE #{dst_fullname}(#{table_definition})")
 
         thr = Thread.new do
-          conn1.exec_query("CREATE WRITABLE EXTERNAL TABLE \"#{src_schema}\".#{pipe_name}(#{table_definition}) LOCATION ('gpfdist://gillette:8000/#{pipe_name}') FORMAT 'TEXT';")
-          conn1.exec_query("INSERT INTO \"#{src_schema}\".#{pipe_name} (SELECT * FROM #{src_fullname});")
+          src_conn.exec_query("CREATE WRITABLE EXTERNAL TABLE \"#{src_schema}\".#{pipe_name}(#{table_definition}) LOCATION ('gpfdist://gillette:8000/#{pipe_name}') FORMAT 'TEXT';")
+          src_conn.exec_query("INSERT INTO \"#{src_schema}\".#{pipe_name} (SELECT * FROM #{src_fullname});")
         end
 
-        conn2.exec_query("CREATE EXTERNAL TABLE \"#{dst_schema}\".#{pipe_name}(#{table_definition}) LOCATION ('gpfdist://gillette:8001/#{pipe_name}') FORMAT 'TEXT';")
-        conn2.exec_query("INSERT INTO #{dst_fullname} (SELECT * FROM \"#{dst_schema}\".#{pipe_name});")
+        dst_conn.exec_query("CREATE EXTERNAL TABLE \"#{dst_schema}\".#{pipe_name}(#{table_definition}) LOCATION ('gpfdist://gillette:8001/#{pipe_name}') FORMAT 'TEXT';")
+        dst_conn.exec_query("INSERT INTO #{dst_fullname} (SELECT * FROM \"#{dst_schema}\".#{pipe_name});")
 
         thr.join
       ensure
-        conn1.exec_query("DROP EXTERNAL TABLE IF EXISTS \"#{src_schema}\".#{pipe_name};")
-        conn2.exec_query("DROP EXTERNAL TABLE IF EXISTS \"#{dst_schema}\".#{pipe_name};")
+        src_conn.exec_query("DROP EXTERNAL TABLE IF EXISTS \"#{src_schema}\".#{pipe_name};")
+        dst_conn.exec_query("DROP EXTERNAL TABLE IF EXISTS \"#{dst_schema}\".#{pipe_name};")
         FileUtils.rm pipe_file if File.exists? pipe_file
       end
     end
   ensure
-    conn1.try(:disconnect!)
-    conn2.try(:disconnect!)
+    disconnect_src_conn
+    disconnect_dst_conn
   end
 
-  def conn1
-    @foo_gpdb1 ||= ActiveRecord::Base.postgresql_connection(
+  def disconnect_src_conn
+    src_conn.try(:disconnect!)
+    @raw_src_conn = nil
+  end
+
+  def disconnect_dst_conn
+    dst_conn.try(:disconnect!)
+    @raw_dst_conn = nil
+  end
+
+  def src_conn
+    @raw_src_conn ||= ActiveRecord::Base.postgresql_connection(
         :host => "chorus-gpdb42",
         :port => 5432,
         :database => "ChorusAnalytics",
@@ -72,8 +82,8 @@ class Gppipe
     )
   end
 
-  def conn2
-    @foo_gpdb2 ||= ActiveRecord::Base.postgresql_connection(
+  def dst_conn
+    @raw_dst_conn ||= ActiveRecord::Base.postgresql_connection(
         :host => "chorus-gpdb41",
         :port => 5432,
         :database => "ChorusAnalytics",
