f7496ac49c9dcec5b2b8c7eb9dd7e5e4fd89d267
Brandon Liu & Jesse Smith <pair+brandon+jsmith@pivotallabs.com>
2012-08-07 17:59:54 -0700

    Only use one connection for each db in the Gppipe class [#27928567]

--- app/services/gppipe.rb
+++ app/services/gppipe.rb
@@ -30,45 +30,57 @@ class Gppipe
 
   def run
     pipe_file = File.join(GPFDIST_PIPE_DIR, pipe_name)
-
-    table_definition = ""
-    empty_table = false
-
-    gpdb1 do |conn|
-      empty_table = (conn.exec_query("SELECT count(*) from #{src_fullname};")[0]['count'] == 0)
-      table_def_rows = conn.exec_query("SELECT column_name, data_type from information_schema.columns where table_name='#{src_table}' and table_schema='#{src_schema}';")
-      table_definition = tabledef_from_query(table_def_rows)
-    end
+    empty_table = (conn1.exec_query("SELECT count(*) from #{src_fullname};")[0]['count'] == 0)
+    table_def_rows = conn1.exec_query("SELECT column_name, data_type from information_schema.columns where table_name='#{src_table}' and table_schema='#{src_schema}';")
+    table_definition = tabledef_from_query(table_def_rows)
 
     if empty_table
-      gpdb2 { |conn| conn.exec_query("CREATE TABLE #{dst_fullname}(#{table_definition})") }
+      conn2.exec_query("CREATE TABLE #{dst_fullname}(#{table_definition})")
     else
       begin
         system "mkfifo #{pipe_file}"
-
-        gpdb2 do |conn|
-          conn.exec_query("CREATE TABLE #{dst_fullname}(#{table_definition})")
-        end
+        conn2.exec_query("CREATE TABLE #{dst_fullname}(#{table_definition})")
 
         thr = Thread.new do
-          gpdb1 do |conn|
-            conn.exec_query("CREATE WRITABLE EXTERNAL TABLE \"#{src_schema}\".#{pipe_name}(#{table_definition}) LOCATION ('gpfdist://gillette:8000/#{pipe_name}') FORMAT 'TEXT';")
-            conn.exec_query("INSERT INTO \"#{src_schema}\".#{pipe_name} (SELECT * FROM #{src_fullname});")
-          end
+          conn1.exec_query("CREATE WRITABLE EXTERNAL TABLE \"#{src_schema}\".#{pipe_name}(#{table_definition}) LOCATION ('gpfdist://gillette:8000/#{pipe_name}') FORMAT 'TEXT';")
+          conn1.exec_query("INSERT INTO \"#{src_schema}\".#{pipe_name} (SELECT * FROM #{src_fullname});")
         end
 
-        gpdb2 do |conn|
-          conn.exec_query("CREATE EXTERNAL TABLE \"#{dst_schema}\".#{pipe_name}(#{table_definition}) LOCATION ('gpfdist://gillette:8001/#{pipe_name}') FORMAT 'TEXT';")
-          conn.exec_query("INSERT INTO #{dst_fullname} (SELECT * FROM \"#{dst_schema}\".#{pipe_name});")
-        end
+        conn2.exec_query("CREATE EXTERNAL TABLE \"#{dst_schema}\".#{pipe_name}(#{table_definition}) LOCATION ('gpfdist://gillette:8001/#{pipe_name}') FORMAT 'TEXT';")
+        conn2.exec_query("INSERT INTO #{dst_fullname} (SELECT * FROM \"#{dst_schema}\".#{pipe_name});")
 
         thr.join
       ensure
-        gpdb1 { |conn| conn.exec_query("DROP EXTERNAL TABLE IF EXISTS \"#{src_schema}\".#{pipe_name};") }
-        gpdb2 { |conn| conn.exec_query("DROP EXTERNAL TABLE IF EXISTS \"#{dst_schema}\".#{pipe_name};") }
+        conn1.exec_query("DROP EXTERNAL TABLE IF EXISTS \"#{src_schema}\".#{pipe_name};")
+        conn2.exec_query("DROP EXTERNAL TABLE IF EXISTS \"#{dst_schema}\".#{pipe_name};")
         FileUtils.rm pipe_file if File.exists? pipe_file
       end
     end
+  ensure
+    conn1.try(:disconnect!)
+    conn2.try(:disconnect!)
+  end
+
+  def conn1
+    @foo_gpdb1 ||= ActiveRecord::Base.postgresql_connection(
+        :host => "chorus-gpdb42",
+        :port => 5432,
+        :database => "ChorusAnalytics",
+        :username => "gpadmin",
+        :password => "secret",
+        :adapter => "jdbcpostgresql"
+    )
+  end
+
+  def conn2
+    @foo_gpdb2 ||= ActiveRecord::Base.postgresql_connection(
+        :host => "chorus-gpdb41",
+        :port => 5432,
+        :database => "ChorusAnalytics",
+        :username => "gpadmin",
+        :password => "secret",
+        :adapter => "jdbcpostgresql"
+    )
   end
 
   def gpdb1
