a09375dbdd7526c402b26853c9d63930df17bdc3
Charles Hansen, Corina Lawu & Yaneeka Huq <pair+chansen+corina+yaneeka@pivotallabs.com>
2012-08-09 16:14:43 -0700

    [#33228237] Temporarily comment out drop_gpdb until we can resolve the brokenness.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -61,9 +61,10 @@ RSpec.configure do |config|
 
   config.treat_symbols_as_metadata_keys_with_true_values = true
 
-  config.before(:suite) do
-    GpdbIntegration.drop_gpdb
-  end
+  # Temporarily commented out until this is fixed
+  #config.before(:suite) do
+  #  GpdbIntegration.drop_gpdb
+  #end
 
   config.before(:all) do
     self.class.set_fixture_class :events => Events::Base
