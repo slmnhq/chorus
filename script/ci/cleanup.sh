3c76533c6076d25aeda770eed546043f3539a812
Gregg Van Hove <pair+gvanhove@pivotallabs.com>
2012-08-10 17:47:05 -0700

    [#34236609] cleanup script allows spaces in build names

--- script/ci/cleanup.sh
+++ script/ci/cleanup.sh
@@ -2,7 +2,7 @@
 
 rm $WORKSPACE/log/*
 
-for file_name in `ls $WORKSPACE/tmp/pids/*$RAILS_ENV*`
+for file_name in `ls "$WORKSPACE/tmp/pids/*$RAILS_ENV*"`
 do
     echo "Killing process id `cat $file_name`, file $file_name"
     kill -s SIGKILL `cat $file_name` 2>&1
