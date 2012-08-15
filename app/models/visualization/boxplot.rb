d803ed29f93cebf600af2146cb61f549d6fd736e
Bleicke Petersen & Corina Lawu <pair+bpetersen+corina@pivotallabs.com>
2012-08-14 17:24:31 -0700

    Guessing this might fix a bug [#34307617]

--- app/models/visualization/boxplot.rb
+++ app/models/visualization/boxplot.rb
@@ -16,7 +16,7 @@ module Visualization
     def fetch!(account, check_id)
       result = SqlExecutor.execute_sql(@schema, account, check_id, build_row_sql)
       row_data = result.rows.map { |row| {:bucket => row[0], :ntile => row[1].to_i, :min => row[2].to_f, :max => row[3].to_f, :count => row[4].to_i} }
-      @rows = BoxplotSummary.summarize(row_data, @bins)
+      @rows = ::BoxplotSummary.summarize(row_data, @bins)
     end
 
     private
