5f655aa49b14ae6792cbaf7aac20581500cbd6a9
Bleicke Petersen & Corina Lawu <pair+bpetersen+corina@pivotallabs.com>
2012-08-17 12:28:33 -0700

    Add some api docs [#34471407]

--- spec/acceptance/activities_spec.rb
+++ spec/acceptance/activities_spec.rb
@@ -3,12 +3,23 @@ require 'spec_helper'
 resource "Activities" do
   let(:user) { users(:bob) }
 
-  get "/activities?entity_type=dashboard" do
-    before do
-      log_in user
-    end
+  before do
+    log_in user
+  end
 
+  get "/activities?entity_type=dashboard" do
     example_request "Get list of activities" do
+      status.should == 200
+    end
+  end
+
+  get "/activities/:id" do
+    let(:id) { Events::USER_ADDED.first.id }
+
+    parameter :id, "Id of the activity/event"
+    required_parameters :id
+
+    example_request "Get an activity" do
       status.should == 200
     end
   end
