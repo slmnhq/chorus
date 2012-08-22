50058baaebe173880bb4367157c8149145c411c4
Aleksey Myagkov <pair+aleksey@pivotallabs.com>
2012-08-22 11:36:11 -0700

    Fixed sandbox smoke test

--- spec/integration/sandbox_smoke_spec.rb
+++ spec/integration/sandbox_smoke_spec.rb
@@ -7,7 +7,6 @@ describe "Create Sandbox" do
     inst_name = "Instance_gpdb40_sand"
     create_gpdb_instance(:name => inst_name)
     instance_id = Instance.find_by_name(inst_name).id
-    #database_id = GpdbDatabase.find_all_by_name
     go_to_workspace_page
     create_valid_workspace(:name => "Private Workspace", :shared => false)
 
@@ -31,11 +30,9 @@ describe "Create Sandbox" do
   it "creates sandbox in a public workspace" do
     login('edcadmin', 'secret')
     inst_name = "Instance_gpdb40_sand"
-    #create_gpdb_instance(:name => inst_name, :host => "rh55-qavm89", :port =>"5432", :dbuser => "edcadmin", :dbpass => "secret")
     instance_id = Instance.find_by_name(inst_name).id
-    database_id = GpdbDatabase.find_all_by_name
     go_to_workspace_page
-    create_valid_workspace(:name => "Private Workspace")
+    create_valid_workspace(:name => "Public Workspace")
 
     click_link "Add a sandbox"
     wait_for_ajax(5)
