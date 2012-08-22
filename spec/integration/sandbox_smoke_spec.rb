aa21d6d2eeacd0b6b535e6d8a8082fc8c92bfc6a
Aleksey Myagkov <pair+aleksey@pivotallabs.com>
2012-08-22 15:21:04 -0700

    Trying to make sandbox helper function work

--- spec/integration/sandbox_smoke_spec.rb
+++ spec/integration/sandbox_smoke_spec.rb
@@ -4,11 +4,13 @@ describe "Create Sandbox" do
 
   it "creates sandbox in a private workspace" do
     login('edcadmin', 'secret')
-    inst_name = "Instance_gpdb40_sand"
-    create_gpdb_instance(:name => inst_name)
-    instance_id = Instance.find_by_name(inst_name).id
+    inst_name = "Instance_gpdb42_sand"
+    create_gpdb_instance(:name => inst_name, :host => WEBPATH['gpdb_instance42']['gpdb_host'],
+    :port => WEBPATH['gpdb_instance42']['gpdb_port'])
     go_to_workspace_page
     create_valid_workspace(:name => "Private Workspace", :shared => false)
+    instance_id = Instance.find_by_name(inst_name).id
+#    add_sandbox(:inst_name => inst_name, :db_name => "ChorusAnalytics", :schema_name => "analytics")
 
     click_link "Add a sandbox"
     wait_for_ajax(5)
@@ -20,6 +22,7 @@ describe "Create Sandbox" do
     page.execute_script("$('select[name=database]').selectmenu('value', '1')")
     page.execute_script("$('.database .select_container select').change();")
     wait_for_ajax(5)
+
     #schema
     page.execute_script("$('select[name=schema]').selectmenu('value', '1')")
     page.execute_script("$('.schema .select_container select').change();")
@@ -27,13 +30,15 @@ describe "Create Sandbox" do
     click_submit_button
   end
 
-  it "creates sandbox in a public workspace" do
+  xit "creates sandbox in a public workspace" do
     login('edcadmin', 'secret')
     inst_name = "Instance_gpdb40_sand"
     instance_id = Instance.find_by_name(inst_name).id
     go_to_workspace_page
     create_valid_workspace(:name => "Public Workspace")
+    create_new_sandbox(:inst_name => inst_name)
 
+=begin
     click_link "Add a sandbox"
     wait_for_ajax(5)
     #instance
@@ -49,6 +54,7 @@ describe "Create Sandbox" do
     page.execute_script("$('.schema .select_container select').change();")
 
     click_submit_button
+=end
   end
 end
 
