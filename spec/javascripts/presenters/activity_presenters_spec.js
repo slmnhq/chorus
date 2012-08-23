d8d8379b14e669dafd171e8f3f1c26f95a018277
Andrew Brown & Nitin Grewal <pair+abrown+nitin@pivotallabs.com>
2012-08-23 10:05:58 -0700

    fix front end test HDFS file [#29788331]

--- spec/javascripts/presenters/activity_presenters_spec.js
+++ spec/javascripts/presenters/activity_presenters_spec.js
@@ -661,7 +661,7 @@ describe("chorus.presenters.Activity", function() {
 
         beforeEach(function() {
             model = rspecFixtures.activity.noteOnHdfsFileCreated({
-                hdfsFile: { hadoopInstanceId: 1234, path: "/random/path.csv" }
+                hdfsFile: { hadoopInstance: {id: 1234}, path: "/random/path.csv" }
             });
             presenter = new chorus.presenters.Activity(model);
             actor = model.actor();
