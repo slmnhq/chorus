5a6102e6f56009570959c57296e6285dbce4cc92
Bleicke Petersen & Joseph Palermo <pair+bpetersen+joseph@pivotallabs.com>
2012-08-27 16:59:02 -0700

    Fix broken API links on staging [fix #34236637]

--- spec/support/rspec_api_documentation_config.rb
+++ spec/support/rspec_api_documentation_config.rb
@@ -1,6 +1,6 @@
 RspecApiDocumentation.configure do |config|
   config.docs_dir = Rails.root.join("public", "api")
-  config.url_prefix = "api/"
+  config.url_prefix = "/api"
   config.keep_source_order = true
   config.format = "wurl"
 end
