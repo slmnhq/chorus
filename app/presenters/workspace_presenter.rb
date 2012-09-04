a05006e550bbab34d19c0e78b56e288769c9099e
Brandon Liu <pair+brandon@pivotallabs.com>
2012-09-04 10:42:48 -0700

    Workspaces only render id and name as part of dashboard activity stream [#33022265]

--- app/presenters/workspace_presenter.rb
+++ app/presenters/workspace_presenter.rb
@@ -2,26 +2,29 @@ class WorkspacePresenter < Presenter
   delegate :id, :name, :summary, :owner, :archiver, :archived_at, :public, :image, :sandbox, :permissions_for, :has_added_member, :has_added_workfile, :has_added_sandbox, :has_changed_settings, to: :model
 
   def to_hash
-    {
-        :id => id,
-        :name => h(name),
-        :summary => sanitize(summary),
-        :owner => present(owner),
-        :archiver => present(archiver),
-        :archived_at => archived_at,
-        :public => public,
-        :image => present(image),
-        :permission => permissions_for(@view_context.current_user),
-        :has_added_member => has_added_member,
-        :has_added_workfile => has_added_workfile,
-        :has_added_sandbox => has_added_sandbox,
-        :has_changed_settings => has_changed_settings,
-        :sandbox_info => sandbox_hash,
-        :latest_comment_list => nil   # temporarily added for rspec fixture - needed by JS specs
-    }
-  end
-
-  def sandbox_hash
-    rendering_activities? ? {:id => model.sandbox_id } : present(sandbox)
+    if rendering_activities?
+      {
+          :id => id,
+          :name => h(name)
+      }
+    else
+      {
+          :id => id,
+          :name => h(name),
+          :summary => sanitize(summary),
+          :owner => present(owner),
+          :archiver => present(archiver),
+          :archived_at => archived_at,
+          :public => public,
+          :image => present(image),
+          :permission => permissions_for(@view_context.current_user),
+          :has_added_member => has_added_member,
+          :has_added_workfile => has_added_workfile,
+          :has_added_sandbox => has_added_sandbox,
+          :has_changed_settings => has_changed_settings,
+          :sandbox_info => present(sandbox),
+          :latest_comment_list => nil   # temporarily added for rspec fixture - needed by JS specs
+      }
+    end
   end
 end
