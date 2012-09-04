3aa0c5f6a1c203ee26dbe02f5921496425f9a8d7
Brandon Liu & Daniel Onggunhao <pair+brandon+donggunhao@pivotallabs.com>
2012-09-04 11:43:34 -0700

    UserPresenter only renders id, name, image inside activity stream [#33022265]

--- app/presenters/user_presenter.rb
+++ app/presenters/user_presenter.rb
@@ -2,17 +2,27 @@ class UserPresenter < Presenter
   delegate :id, :username, :first_name, :last_name, :email, :title, :dept, :notes, :admin?, :image, to: :model
 
   def to_hash
-    {
-      :id => id,
-      :username => h(username),
-      :first_name => h(first_name),
-      :last_name => h(last_name),
-      :email => h(email),
-      :title => h(title),
-      :dept => h(dept),
-      :notes => h(notes),
-      :admin => admin?,
-      :image => present(image)
-    }
+    if rendering_activities?
+      {
+          :id => id,
+          :username => h(username),
+          :first_name => h(first_name),
+          :last_name => h(last_name),
+          :image => present(image)
+      }
+    else
+      {
+          :id => id,
+          :username => h(username),
+          :first_name => h(first_name),
+          :last_name => h(last_name),
+          :email => h(email),
+          :title => h(title),
+          :dept => h(dept),
+          :notes => h(notes),
+          :admin => admin?,
+          :image => present(image)
+      }
+    end
   end
 end
