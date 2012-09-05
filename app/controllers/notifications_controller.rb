307dc088cd64b146f03e6495da03af710cf2efba
Adriana Diakite & Brandon Liu <pair+adriana+brandon@pivotallabs.com>
2012-09-05 13:24:13 -0700

    Add read receipt to notifications with NotificationsController#read endpoint [#27928655]

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,4 +2,9 @@ class NotificationsController < ApplicationController
   def index
     present paginate(current_user.notification_events)
   end
+
+  def read
+    Notification.where(:id => params[:notification_ids]).update_all(:read => true)
+    head :ok
+  end
 end
--- spec/controllers/notifications_controller_spec.rb
+++ spec/controllers/notifications_controller_spec.rb
@@ -1,14 +1,15 @@
 require "spec_helper"
 
 describe NotificationsController do
+  let(:bobs_notification) { notifications(:bobs_notification1) }
+  let(:bobs_event) { bobs_notification.notification_event }
+  let(:current_user) { users(:bob) }
+
+  before do
+    log_in current_user
+  end
+
   describe "#index" do
-    let(:bobs_event) { notifications(:bobs_notification1).notification_event }
-    let(:current_user) { users(:bob) }
-
-    before do
-      log_in current_user
-    end
-
     it "is successful" do
       get :index
       response.code.should == "200"
@@ -30,6 +31,15 @@ describe NotificationsController do
 
     generate_fixture "notificationSet.json" do
       get :index
+    end
+  end
+
+  describe '#read' do
+    it "marks all notifications passed as read" do
+      bobs_notification.read.should be_false
+      put :read, :notification_ids => [bobs_notification.id]
+      bobs_notification.reload.read.should be_true
+      response.code.should == '200'
     end
   end
 end
