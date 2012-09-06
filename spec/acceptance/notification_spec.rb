becf6064e8b0963b89f39c3b3f4bd7021b08f5ea
Adriana Diakite & Brandon Liu <pair+adriana+brandon@pivotallabs.com>
2012-09-05 17:30:37 -0700

    API documentation for notifications.read endpoint [#27928695]

--- spec/acceptance/notification_spec.rb
+++ spec/acceptance/notification_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 resource "Notifications" do
   let(:user) { users(:bob) }
+  let(:event1) { notifications(:bobs_notification1).notification_event }
+  let(:event2) { notifications(:bobs_notification2).notification_event }
 
   before do
     log_in user
@@ -9,6 +11,18 @@ resource "Notifications" do
 
   get "/notifications" do
     example_request "Returns a list of notifications" do
+      status.should == 200
+    end
+  end
+
+  put "/notifications/read" do
+    parameter :notification_ids, "IDs of events to be marked as read"
+
+    let(:notification_ids) { [event1.id, event2.id] }
+
+    required_parameters :notification_ids
+
+    example_request "Marks notifications as read" do
       status.should == 200
     end
   end
