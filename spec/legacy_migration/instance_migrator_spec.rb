88b88ac0e9cf5022d262002ac86cd1b830f9c142
Brandon Liu & Fiona Tay <pair+brandon+fiona@pivotallabs.com>
2012-09-07 10:12:37 -0700

    remove extraneous comments from instance migrator [#29177409]

--- spec/legacy_migration/instance_migrator_spec.rb
+++ spec/legacy_migration/instance_migrator_spec.rb
@@ -22,19 +22,9 @@ describe InstanceMigrator do
           gpdb_instance.description.should == legacy_instance["description"]
           gpdb_instance.host.should == legacy_instance["host"]
           gpdb_instance.port.should == legacy_instance["port"].try(:to_i)
-          #gpdb_instance.expire.should == legacy_instance["expire"]
-          #gpdb_instance.state.should == legacy_instance["state"]
           gpdb_instance.provision_type.should == legacy_instance["provision_type"]
-          #gpdb_instance.provision_id.should == legacy_instance["provision_id"]
-          #gpdb_instance.size.should == legacy_instance["size"]
           gpdb_instance.instance_provider.should == legacy_instance["instance_provider"]
-          #gpdb_instance.last_check.should == legacy_instance["last_check"]
-          #gpdb_instance.provision_name.should == legacy_instance["provision_name"]
-          #gpdb_instance.is_deleted.should == (legacy_instance["is_deleted"] != "f")
-          #gpdb_instance.instance_version.should == legacy_instance["instance_version"]
           gpdb_instance.maintenance_db.should == legacy_instance["maintenance_db"]
-          #gpdb_instance.connection_string.should == legacy_instance["connection_string"]
-
           gpdb_instance.owner.should == User.find_by_username(legacy_instance["owner"])
         end
       end
