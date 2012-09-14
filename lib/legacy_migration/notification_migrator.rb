cf790412958d6425519e79dc76bcf1ece1d74a4d
Jesse Smith & Nitin Grewal <pair+jsmith+nitin@pivotallabs.com>
2012-09-14 12:34:18 -0700

    [finishes #29816509] migrating the MEMBERS_ADDED notifications using activitystreamObject

--- lib/legacy_migration/notification_migrator.rb
+++ lib/legacy_migration/notification_migrator.rb
@@ -3,6 +3,7 @@ class NotificationMigrator < AbstractMigrator
     def prerequisites(options)
       UserMigrator.migrate
       NoteMigrator.migrate(options)
+      ActivityMigrator.migrate(options)
     end
 
     def classes_to_validate
@@ -37,6 +38,39 @@ class NotificationMigrator < AbstractMigrator
           WHERE type = 'NOTE'
           AND edc_alert.id NOT IN (SELECT legacy_id FROM notifications);
         ))
+
+      Legacy.connection.exec_query(%Q(
+        INSERT INTO notifications(
+          legacy_id,
+          recipient_id,
+          event_id,
+          created_at,
+          updated_at,
+          read
+        )
+        SELECT
+          ea.id,
+          users.id,
+          events.id,
+          ea.created_stamp,
+          ea.last_updated_stamp,
+          ea.is_read
+        FROM
+        edc_alert ea
+        INNER JOIN edc_activity_stream_object aso
+          ON  aso.object_id = ea.recipient AND aso.entity_type = 'user'
+          AND aso.created_tx_stamp = ea.created_tx_stamp
+        INNER JOIN edc_activity_stream eas
+          ON eas.id = aso.activity_stream_id
+          AND eas.type = 'MEMBERS_ADDED' and eas.workspace_id = ea.reference
+        INNER JOIN users
+          ON users.legacy_id = ea.recipient
+        INNER JOIN events
+          ON events.legacy_id = eas.id AND events.legacy_type = 'edc_activity_stream'
+        WHERE ea.type = 'MEMBERS_ADDED'
+          AND ea.id NOT IN (SELECT legacy_id FROM notifications);
+        ))
+
     end
   end
 end
--- spec/legacy_migration/notification_migrator_spec.rb
+++ spec/legacy_migration/notification_migrator_spec.rb
@@ -12,18 +12,19 @@ describe NotificationMigrator do
 
   describe ".migrate" do
     it "creates new notifications for legacy notifications" do
-      count = Legacy.connection.select_all("SELECT count(*) FROM edc_alert WHERE type = 'NOTE'").first["count"]
+      count = Legacy.connection.select_all("SELECT count(*) FROM edc_alert WHERE type IN ('NOTE', 'MEMBERS_ADDED')").first["count"]
       Notification.unscoped.count.should == count
       NotificationMigrator.migrate('workfile_path' => SPEC_WORKFILE_PATH)
       Notification.unscoped.count.should == count
     end
 
     it "migrates the notes" do
-      count = 0
+      count = Notification.count
       Legacy.connection.select_all("
         SELECT ea.*
         FROM edc_alert ea
         WHERE type = 'NOTE'
+        AND ea.id NOT IN (SELECT legacy_id FROM notifications);
       ").each do |legacy_comment|
         count += 1
         notification = Notification.find_by_legacy_id(legacy_comment["id"])
@@ -37,6 +38,31 @@ describe NotificationMigrator do
         notification.created_at.should == legacy_comment["created_stamp"]
         notification.updated_at.should == legacy_comment["last_updated_stamp"]
         notification.read.should == (legacy_comment["is_read"] == 't' ? true : false)
+      end
+      count.should > 0
+      Notification.count.should == count
+    end
+
+    it "migrates the notification for MEMBERS_ADDED to a workspace" do
+      count = Notification.count
+      Legacy.connection.select_all("
+        SELECT ea.*
+        FROM edc_alert ea
+        WHERE type = 'MEMBERS_ADDED'
+        AND ea.id NOT IN (SELECT legacy_id FROM notifications);
+      ").each do |legacy_alert|
+        count += 1
+        notification = Notification.find_by_legacy_id(legacy_alert["id"])
+
+        recipient = User.find(notification.recipient_id)
+        recipient.legacy_id.should == legacy_alert["recipient"]
+
+        event = Events::Base.find(notification.event_id)
+        event.workspace_id == Workspace.find_by_legacy_id(legacy_alert["reference"]).id
+
+        notification.created_at.should == legacy_alert["created_stamp"]
+        notification.updated_at.should == legacy_alert["last_updated_stamp"]
+        notification.read.should == (legacy_alert["is_read"] == 't' ? true : false)
       end
       count.should > 0
       Notification.count.should == count
