546b148c9306d7e7fd480560d2826769a0e2b82f
Joseph Palermo <pair+joseph@pivotallabs.com>
2012-09-17 11:44:05 -0700

    When writing database.yml, use the correct enviornment.

--- packaging/update_database_pool_size.rb
+++ packaging/update_database_pool_size.rb
@@ -1,5 +1,7 @@
 require 'yaml'
 
+environment = ENV['RAILS_ENV']
+
 chorus_home = File.expand_path(File.dirname(__FILE__) + '/../')
 require File.join(chorus_home, 'config', 'boot')
 require File.join(chorus_home, 'app/models/chorus_config')
@@ -10,8 +12,8 @@ database_yml = File.join(chorus_home, 'config', 'database.yml')
 db_config = YAML.load_file database_yml
 
 pool_size = [chorus_config['webserver_threads'].to_i, chorus_config['worker_threads'].to_i].max
-if db_config['production']['pool'] != pool_size
-  db_config['production']['pool'] = pool_size
+if db_config[environment]['pool'] != pool_size
+  db_config[environment]['pool'] = pool_size
 
   File.open(database_yml, 'w') do |f|
     f.write(YAML.dump(db_config))
