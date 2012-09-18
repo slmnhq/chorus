f1e2806f04b82b5824a0cad97cb39b0bca8d6578
Daniel Onggunhao & Gregg Van Hove <pair+donggunhao+gvanhove@pivotallabs.com>
2012-09-18 11:29:21 -0700

    [#29200313] User gets a notification if (note user created || note user commented on) gets commented on. Notification text is still the original event rather than a proper notification

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,11 +9,23 @@ class CommentsController < ApplicationController
   def create
     comment = Comment.new
     attributes = params[:comment]
-    attributes[:author_id] = current_user[:id]
+    attributes[:author_id] = current_user.id
     comment.attributes = attributes
 
     authorize! :create_comment_on, Comment, Events::Base.find(comment.event_id)
     comment.save!
+
+    event_id = comment.event_id
+    event = Events::Base.find(event_id)
+    users_to_notify = event.comments.map(&:author_id)
+    users_to_notify << event.actor_id
+    users_to_notify = users_to_notify.uniq - [current_user.id]
+    users_to_notify.each do |user_id|
+      Notification.create!(
+          :recipient_id => user_id,
+          :event_id => event_id
+      )
+    end
 
     present comment, :status => :created
   end
--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -1,12 +1,16 @@
 require 'spec_helper'
 
 describe CommentsController do
-  let(:author) { users(:carly) }
   let(:event) { events(:note_on_bob_public_workfile) }
+  let(:event_author) { users(:bob) }
+  let(:first_commenter) { users(:carly) }
+  let(:second_commenter) { users(:admin) }
 
   before do
-    log_in author
+    log_in commenter
   end
+
+  let(:commenter) { event_author }
 
   describe "#create" do
     before do
@@ -18,22 +22,82 @@ describe CommentsController do
 
     it "uses authorization" do
       mock(subject).authorize! :create_comment_on, Comment, event
-      post :create, { :comment => @params }
+      post :create, {:comment => @params}
     end
 
     it "should post with appropriate response" do
-      post :create, { :comment => @params }
+      post :create, {:comment => @params}
       response.code.should == "201"
     end
 
     it "should create make the current user the author" do
-      post :create, { :comment => @params }
-      Comment.find_by_text(@params[:text]).author.should == author
+      post :create, {:comment => @params}
+      Comment.find_by_text(@params[:text]).author.should == commenter
+    end
+
+    context "when event author comments" do
+      before do
+        Comment.create!({:event => event, :author => first_commenter, :text => "Nice event"}, :without_protection => true)
+        Comment.create!({:event => event, :author => second_commenter, :text => "Great event"}, :without_protection => true)
+        post :create, {:comment => @params}
+      end
+
+      it "notifies the other commenters" do
+        Notification.where(:recipient_id => first_commenter.id, :event_id => event.id).should exist
+        Notification.where(:recipient_id => second_commenter.id, :event_id => event.id).should exist
+      end
+
+      it "doesn't notify the event author" do
+        Notification.where(:recipient_id => event_author.id, :event_id => event.id).should_not exist
+      end
+    end
+
+    context "when someone else comments" do
+      let(:commenter) { first_commenter }
+
+      before do
+        Comment.create!({:event => event, :author => second_commenter, :text => "I am a second comment"}, :without_protection => true)
+        post :create, {:comment => @params}
+      end
+
+      it "notifies the event author" do
+        Notification.where(:recipient_id => event_author.id, :event_id => event.id).should exist
+      end
+
+      it "doesn't notify the current commenter" do
+        Notification.where(:recipient_id => commenter.id, :event_id => event.id).should_not exist
+      end
+
+      it "notifies other commenters" do
+        Notification.where(:recipient_id => second_commenter.id, :event_id => event.id).should exist
+      end
+    end
+
+    context "when some user's have commented multiple times" do
+      before do
+        Comment.create!(:event_id => event.id, :author_id => event_author.id, :text => "I comment on myself")
+        Comment.create!(:event_id => event.id, :author_id => first_commenter.id, :text => "Great event")
+        Comment.create!(:event_id => event.id, :author_id => first_commenter.id, :text => "Great event again")
+      end
+
+      let(:commenter) { second_commenter }
+
+      it "only notifies the same user once" do
+        expect {
+          expect {
+            post :create, {:comment => @params}
+          }.to change {
+            Notification.where(:recipient_id => event_author.id, :event_id => event.id).count
+          }.by(1)
+        }.to change {
+          Notification.where(:recipient_id => first_commenter.id, :event_id => event.id).count
+        }.by(1)
+      end
     end
   end
 
   describe "#show" do
-    let(:comment) { Comment.new({:event_id => event.id, :text => "Comment on a note", :author_id => author.id}) }
+    let(:comment) { Comment.new({:event_id => event.id, :text => "Comment on a note", :author_id => commenter.id}) }
     before do
       comment.save!
     end
@@ -54,11 +118,10 @@ describe CommentsController do
     end
   end
 
-  describe "#destroy" do    
+  describe "#destroy" do
     before do
-      @comment = Comment.new({:event_id => event.id, :author_id => author.id, :text => "Delete me!"})
+      @comment = Comment.new({:event_id => event.id, :author_id => commenter.id, :text => "Delete me!"})
       @comment.save!
-      log_in author
     end
 
     it "uses authorization" do
@@ -71,7 +134,7 @@ describe CommentsController do
         delete :destroy, :id => @comment.id
       end
 
-      it "should soft delete the comment" do        
+      it "should soft delete the comment" do
         comment = Comment.find_with_destroyed(@comment.id)
         comment.deleted_at.should_not be_nil
       end
