bd3be942cecf5e58436cf5c6f8d1154fb65def69
Brandon Liu & Gregg Van Hove <pair+brandon+gvanhove@pivotallabs.com>
2012-09-21 10:57:13 -0700

    Fix worker connection leaks, it seems to be solved if we don't try to manage ActiveRecord::Base.connection ourselves. [#35061383]

--- app/services/chorus_worker.rb
+++ app/services/chorus_worker.rb
@@ -1,11 +1,4 @@
 class ChorusWorker < QC::Worker
-  def call(job)
-    ActiveRecord::Base.establish_connection
-    super
-  ensure
-    ActiveRecord::Base.connection.try(:disconnect!)
-  end
-
   def thread_pool_size
     Chorus::Application.config.chorus['worker_threads'].to_i
   end
