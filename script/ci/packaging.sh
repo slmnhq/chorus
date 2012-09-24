fc53252171cac8a11ef70402727f214f5c2c1688
Brandon Liu & Nitin Grewal <pair+brandon+nitin@pivotallabs.com>
2012-09-24 12:30:34 -0700

    Fix build - packaging script needs GPDB_HOST set to build test fixtures

--- script/ci/packaging.sh
+++ script/ci/packaging.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 export RAILS_ENV=packaging
+GPDB_HOST=chorus-gpdb42
 
 . script/ci/setup.sh
 
 rm -fr .bundle
-rake package:installer --trace
+GPDB_HOST=$GPDB_HOST rake package:installer --trace
