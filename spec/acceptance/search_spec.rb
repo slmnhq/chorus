a2a9681236fa08afe026700e050e8fc35c951caa
Boxiong Ding & Joseph Palermo <pair+boxiong+joseph@pivotallabs.com>
2012-09-26 18:03:35 -0700

    [#27928663] API docs for workspace search

--- spec/acceptance/search_spec.rb
+++ spec/acceptance/search_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 resource "Search" do
   let(:user) { users(:owner) }
+  let(:workspace) { workspaces(:search_public) }
 
   before do
     reindex_solr_fixtures
@@ -11,6 +12,9 @@ resource "Search" do
   get "/search" do
     parameter :query, "Search term"
     parameter :per_type, "Number of each entity to return"
+
+    required_parameters :query
+
     example "Global Search" do
       VCR.use_cassette('search_solr_query_all_types_as_owner') do
         do_request(:query => 'searchquery', :per_type => 3)
@@ -24,9 +28,29 @@ resource "Search" do
     parameter :entity_type, "The type of entity to search for (GpdbInstance, User, Workspace, Workfile)"
     parameter :page, "Page number"
     parameter :per_page, "Results per page"
+
+    required_parameters :query
+
     example "Entity Search" do
       VCR.use_cassette('search_solr_query_user_as_owner') do
         do_request(:query => 'searchquery', :entity_type => 'User', :page => 1, :per_page => 50)
+        status.should == 200
+      end
+    end
+  end
+
+  get "/workspaces/:workspace_id/search" do
+    let(:workspace_id) { workspace.id }
+    parameter :query, "Search term"
+    parameter :entity_type, "The type of entity to search for (GpdbInstance, User, Workspace, Workfile)"
+    parameter :page, "Page number"
+    parameter :per_page, "Results per page"
+
+    required_parameters :query
+
+    example "Search Within a Workspace" do
+      VCR.use_cassette('workspace_search_solr_query_as_owner') do
+        do_request(:query => 'searchquery', :page => 1, :per_page => 50)
         status.should == 200
       end
     end
