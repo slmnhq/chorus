c278b6254033764117864fd9d665862649b0070a
Gregg Van Hove & Joseph Palermo <pair+gvanhove+joseph@pivotallabs.com>
2012-09-27 12:29:45 -0700

    [#36799065] Don't do a rehash for ci builds, the master build is responsible for this.

--- script/ci/setup.sh
+++ script/ci/setup.sh
@@ -1,6 +1,6 @@
 export JRUBY_OPTS="--1.9 --client -J-Xmx512m -J-Xms512m -J-Xmn128m"
 export PATH="$HOME/phantomjs/bin:/opt/postgres/9.1/bin:$HOME/.rbenv/bin:$PATH"
-eval "$(rbenv init -)"
+eval "$(rbenv init - --no-rehash)"
 rbenv shell `cat .rbenv-version`
 export JASMINE_PORT=8888
 
@@ -8,7 +8,6 @@ gem list bundler | grep bundler || gem install bundler
 bundle install --binstubs=b/ || (echo "bundler failed!!!!!!!!" && exit 1)
 
 mkdir -p tmp/pids
-
 rm tmp/fixture_builder*.yml
 
 b/rake db:drop db:create db:migrate --trace > "$WORKSPACE/bundle.log"
