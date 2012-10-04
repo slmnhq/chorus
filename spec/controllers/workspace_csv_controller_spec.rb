d74916cda77a2c13fc0565f04332eaff8ddc98f5
Andrew Brown & Brandon Liu <pair+abrown+brandon@pivotallabs.com>
2012-10-04 13:41:41 -0700

    complete accidentally submitted partial refactor of workspace csv controller spec

--- spec/controllers/workspace_csv_controller_spec.rb
+++ spec/controllers/workspace_csv_controller_spec.rb
@@ -60,11 +60,7 @@ describe WorkspaceCsvController do
   end
 
   describe "#import" do
-    before do
-      FactorGirl.create :csv_file,
-      post :create, csv_file_params
-      @csv_file = CsvFile.last
-    end
+    let(:csv_file) { csv_files(:default) }
 
     let(:csv_import_params) do
       {
@@ -84,33 +80,33 @@ describe WorkspaceCsvController do
 
     context "when there's no table conflict" do
       before do
-        mock(QC.default_queue).enqueue.with("CsvImporter.import_file", @csv_file.id, anything) do | method, file_id, event_id |
+        mock(QC.default_queue).enqueue.with("CsvImporter.import_file", csv_file.id, anything) do | method, file_id, event_id |
           Events::FileImportCreated.by(user).first.id.should == event_id
         end
       end
 
       it "updates the necessary import fields on the csv file model" do
-        put :import, :workspace_id => workspace.id, :id => @csv_file.id, :csvimport => csv_import_params
-        @csv_file.reload
-        @csv_file.delimiter.should == ','
-        @csv_file.column_names.should == ['id', 'name']
-        @csv_file.types.should == ['integer', 'varchar']
-        @csv_file.to_table.should == "table_importing_into"
-        @csv_file.file_contains_header.should == false
-        @csv_file.workspace.should == workspace
+        put :import, :workspace_id => workspace.id, :id => csv_file.id, :csvimport => csv_import_params
+        csv_file.reload
+        csv_file.delimiter.should == ','
+        csv_file.column_names.should == ['id', 'name']
+        csv_file.types.should == ['integer', 'varchar']
+        csv_file.to_table.should == "table_importing_into"
+        csv_file.file_contains_header.should == false
+        csv_file.workspace.should == workspace
       end
 
       it "uses authentication" do
-        mock(subject).authorize! :import, @csv_file
-        put :import, :workspace_id => workspace.id, :id => @csv_file.id, :csvimport => csv_import_params
+        mock(subject).authorize! :import, csv_file
+        put :import, :workspace_id => workspace.id, :id => csv_file.id, :csvimport => csv_import_params
       end
 
       context "when has_header is true" do
         let(:has_header) { true }
         it "changes the has_header field to file_contains_header FIXME" do
-          put :import, :workspace_id => workspace.id, :id => @csv_file.id, :csvimport => csv_import_params
-          @csv_file.reload
-          @csv_file.file_contains_header.should be_true
+          put :import, :workspace_id => workspace.id, :id => csv_file.id, :csvimport => csv_import_params
+          csv_file.reload
+          csv_file.file_contains_header.should be_true
         end
       end
 
@@ -118,18 +114,18 @@ describe WorkspaceCsvController do
         context "new table" do
           it "sets the new_table field to true" do
             any_instance_of(CsvFile) { |csv| stub(csv).table_already_exists.with_any_args { false } }
-            put :import, :workspace_id => workspace.id, :id => @csv_file.id, :csvimport => csv_import_params
-            @csv_file.reload.new_table.should be_true
+            put :import, :workspace_id => workspace.id, :id => csv_file.id, :csvimport => csv_import_params
+            csv_file.reload.new_table.should be_true
           end
 
           it "makes a FILE_IMPORT_CREATED event with no associated dataset" do
-            put :import, :workspace_id => workspace.id, :id => @csv_file.id, :csvimport => csv_import_params
+            put :import, :workspace_id => workspace.id, :id => csv_file.id, :csvimport => csv_import_params
 
             event = Events::FileImportCreated.first
             event.actor.should == user
             event.dataset.should be_nil
             event.workspace.should == workspace
-            event.file_name.should == @csv_file.contents_file_name
+            event.file_name.should == csv_file.contents_file_name
             event.import_type.should == 'file'
             event.destination_table.should == 'table_importing_into'
           end
@@ -140,25 +136,25 @@ describe WorkspaceCsvController do
           let(:columns_map) { [{:targetOrder => 'name'}, {:targetOrder => 'id'}].to_json }
 
           it "sets the new_table field to false" do
-            put :import, :workspace_id => workspace.id, :id => @csv_file.id, :csvimport => csv_import_params
-            @csv_file.reload.new_table.should be_false
+            put :import, :workspace_id => workspace.id, :id => csv_file.id, :csvimport => csv_import_params.merge(:new_table => 'false')
+            csv_file.reload.new_table.should be_false
           end
 
           it "sets the column names based on the columns_map" do
-            put :import, :workspace_id => workspace.id, :id => @csv_file.id, :csvimport => csv_import_params
-            @csv_file.reload.column_names.should == ['name', 'id']
+            put :import, :workspace_id => workspace.id, :id => csv_file.id, :csvimport => csv_import_params
+            csv_file.reload.column_names.should == ['name', 'id']
           end
 
           it "makes a FILE_IMPORT_CREATED event with associated dataset" do
             dataset = datasets(:table)
 
-            put :import, :workspace_id => workspace.id, :id => @csv_file.id, :csvimport => csv_import_params.merge(:to_table => dataset.name)
+            put :import, :workspace_id => workspace.id, :id => csv_file.id, :csvimport => csv_import_params.merge(:to_table => dataset.name)
 
             event = Events::FileImportCreated.first
             event.actor.should == user
             event.dataset.should == dataset
             event.workspace.should == workspace
-            event.file_name.should == @csv_file.contents_file_name
+            event.file_name.should == csv_file.contents_file_name
             event.import_type.should == 'file'
             event.destination_table.should == 'table'
           end
@@ -175,14 +171,14 @@ describe WorkspaceCsvController do
       end
 
       it "returns an error" do
-        put :import, :workspace_id => workspace.id, :id => @csv_file.id, :csvimport => csv_import_params
+        put :import, :workspace_id => workspace.id, :id => csv_file.id, :csvimport => csv_import_params
 
         response.body.should include "TABLE_EXISTS"
       end
 
       it "does not create an FILE_IMPORT_CREATED event" do
         expect do
-          put :import, :workspace_id => workspace.id, :id => @csv_file.id, :csvimport => csv_import_params
+          put :import, :workspace_id => workspace.id, :id => csv_file.id, :csvimport => csv_import_params
         end.to_not change(Events::FileImportCreated, :count)
       end
     end
