c9afc3952f74db32a4b4bbca0732c46ca3ad1eed
Callum Stott and Gregg Van Hove <pair+callum+gvanhove@pivotallabs.com>
2012-10-09 15:26:38 -0700

    [finish #37468567] Add spec for HadoopInstancePresenter

---
+++ spec/presenters/hadoop_instance_presenter_spec.rb
@@ -0,0 +1,37 @@
+require 'spec_helper'
+
+describe HadoopInstancePresenter, :type => :view do
+  let(:hadoop_instance) { hadoop_instances(:hadoop) }
+  let(:user) { hadoop_instance.owner }
+  let(:presenter) { HadoopInstancePresenter.new(hadoop_instance, view, options) }
+  let(:options) { {} }
+
+  before do
+    stub(ActiveRecord::Base).current_user { user }
+  end
+
+  describe "#to_hash" do
+    let(:hash) { presenter.to_hash }
+
+    it "should have the correct keys" do
+      hash.should have_key(:name)
+      hash.should have_key(:host)
+      hash.should have_key(:port)
+      hash.should have_key(:id)
+      hash.should have_key(:owner)
+      hash.should have_key(:state)
+      hash.should have_key(:description)
+      hash.should have_key(:version)
+      hash.should have_key(:username)
+      hash.should have_key(:group_list)
+    end
+
+    it "should use ownerPresenter Hash method for owner" do
+      owner = hash[:owner]
+      owner.to_hash.should == (UserPresenter.new(user, view).presentation_hash)
+    end
+
+    it_behaves_like "sanitized presenter", :hadoop_instance, :name
+    it_behaves_like "sanitized presenter", :hadoop_instance, :host
+  end
+end
