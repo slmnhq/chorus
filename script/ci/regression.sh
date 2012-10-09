1ba8a9e659f3b82a142eda2bfa9271b6d3df79f5
Bleicke Petersen & Chris Brown <pair+bpetersen+cbrown@pivotallabs.com>
2012-10-09 15:54:16 -0700

    Make ci scripts use rake instead of script/ci-test [#36573811]

--- script/ci/regression.sh
+++ script/ci/regression.sh
@@ -18,7 +18,7 @@ sleep 20
 set +e
 
 echo "Running regression tests"
-script/ci-test spec/regression/ 2>&1
+b/rspec spec/regression/ 2>&1
 INTEGRATION_TESTS_RESULT=$?
 
 echo "Cleaning up solr process $solr_pid"
