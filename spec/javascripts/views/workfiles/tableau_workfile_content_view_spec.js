27b7ea431ba0861f453fb09e5282363c86ee3ad0
Brandon Liu & Nitin Grewal <pair+brandon+nitin@pivotallabs.com>
2012-10-10 10:55:03 -0700

    Remove placeholder image from tableau workfile show page [#36578859]

--- spec/javascripts/views/workfiles/tableau_workfile_content_view_spec.js
+++ spec/javascripts/views/workfiles/tableau_workfile_content_view_spec.js
@@ -3,15 +3,4 @@ describe("chorus.views.TableauWorkfileContent", function() {
         this.model = rspecFixtures.workfile.tableau();
         this.view = new chorus.views.TableauWorkfileContent({ model : this.model })
     });
-
-    describe("render", function() {
-        beforeEach(function() {
-            this.view.render();
-        });
-
-        it("creates an image tag", function() {
-            var img = this.view.$("img")
-            expect(img).toExist();
-        });
-    });
 });
