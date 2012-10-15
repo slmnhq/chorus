c001389ffb705765d4c97dd5b0b9419618dc88a6
Bleicke Petersen & Brandon Liu <pair+bpetersen+brandon@pivotallabs.com>
2012-10-15 14:35:05 -0700

    Check for existing table before downloading data when importing a gnip stream [#37374651]

--- app/controllers/gnip_instance_imports_controller.rb
+++ app/controllers/gnip_instance_imports_controller.rb
@@ -4,6 +4,16 @@ class GnipInstanceImportsController < ApplicationController
     workspace = Workspace.find(params['imports']['workspace_id'])
 
     authorize! :can_edit_sub_objects, workspace
+    table_name = params['imports']['to_table']
+
+    temp_csv_file = workspace.csv_files.new(
+        :to_table => table_name,
+    )
+    temp_csv_file.user = current_user
+    if temp_csv_file.table_already_exists(table_name)
+      raise ApiValidationError.new(:base, :table_exists, { :table_name => table_name })
+    end
+
     gnip_instance = GnipInstance.find(params['imports']['gnip_instance_id'])
     c = ChorusGnip.from_stream(gnip_instance.stream_url, gnip_instance.username, gnip_instance.password)
     result = c.to_result
@@ -13,15 +23,15 @@ class GnipInstanceImportsController < ApplicationController
       :column_names => result.column_names,
       :types => result.types,
       :delimiter => ',',
-      :to_table => params['imports']['to_table'],
+      :to_table => table_name,
       :new_table => true
     )
     csv_file.user = current_user
 
     if csv_file.save
-      create_import_event(csv_file)
-      GnipImporter.import_file(csv_file, nil)
-      render :json => '', :status => :created
+      event = create_import_event(csv_file)
+      GnipImporter.import_file(csv_file, event)
+      render :json => [], :status => :ok
     end
   end
 
--- spec/controllers/gnip_instance_imports_controller_spec.rb
+++ spec/controllers/gnip_instance_imports_controller_spec.rb
@@ -4,58 +4,79 @@ describe GnipInstanceImportsController do
 
   let(:user) { users(:owner) }
   let(:gnip_csv_result_mock) { GnipCsvResult.new("a,b,c\n1,2,3") }
+  let(:gnip_instance) { gnip_instances(:default) }
+  let(:workspace) { workspaces(:public) }
 
-  before do
-    log_in user
-
-    mock(ChorusGnip).from_stream(gnip_instance.stream_url,
-                                 gnip_instance.username,
-                                 gnip_instance.password) do |c|
-      o = Object.new
-      stub(o).to_result { gnip_csv_result_mock }
-    end
-
-    mock(GnipImporter).import_file(anything, anything)
-  end
+  let(:gnip_instance_import_params) { {
+      :imports => {
+          :gnip_instance_id => gnip_instance.id,
+          :to_table => 'foobar',
+          :workspace_id => workspace.id
+      }
+  } }
 
   describe "#create" do
-    let(:gnip_instance) { gnip_instances(:default) }
-    let(:workspace) { workspaces(:public) }
-
-    let(:gnip_instance_import_params) { {
-      :imports => {
-        :gnip_instance_id => gnip_instance.id,
-        :to_table => 'foobar',
-        :workspace_id => workspace.id
-      }
-    }}
-
-    it "uses authentication" do
-      mock(subject).authorize! :can_edit_sub_objects, workspace
-      post :create, gnip_instance_import_params
-      response.code.should == '201'
+    before do
+      log_in user
     end
 
-    it "creates a CsvFile and calls CsvImporter" do
-      expect {
-        post :create, gnip_instance_import_params
-      }.to change(CsvFile, :count).by(1)
+    context "table name doesn't exist already" do
+      before do
+        mock(ChorusGnip).from_stream(gnip_instance.stream_url,
+                                     gnip_instance.username,
+                                     gnip_instance.password) do |c|
+          o = Object.new
+          stub(o).to_result { gnip_csv_result_mock }
+        end
 
-      file = CsvFile.last
-      file.contents.should_not be_nil
-      file.column_names.should == gnip_csv_result_mock.column_names
-      file.types.should == ['text', 'text']
-      file.delimiter.should == ","
-      file.to_table.should == 'foobar'
-      file.new_table.should == true
-      file.user.should == user
-      file.workspace.should == workspace
+        mock(GnipImporter).import_file(anything, anything)
+        any_instance_of(CsvFile) do |file|
+          stub(file).table_already_exists(anything) { false }
+        end
+      end
+
+      it "uses authentication" do
+        mock(subject).authorize! :can_edit_sub_objects, workspace
+        post :create, gnip_instance_import_params
+        response.code.should == '200'
+      end
+
+      it "creates a CsvFile and calls CsvImporter" do
+        expect {
+          post :create, gnip_instance_import_params
+        }.to change(CsvFile, :count).by(1)
+
+        file = CsvFile.last
+        file.contents.should_not be_nil
+        file.column_names.should == gnip_csv_result_mock.column_names
+        file.types.should == ['text', 'text']
+        file.delimiter.should == ","
+        file.to_table.should == 'foobar'
+        file.new_table.should == true
+        file.user.should == user
+        file.workspace.should == workspace
+      end
+
+      it "creates an event before it is run" do
+        expect {
+          post :create, gnip_instance_import_params
+        }.to change(Events::FileImportCreated, :count).by(1)
+      end
     end
 
-    it "creates an event before it is run" do
-      expect {
+    context "table name already exists" do
+      before do
+        any_instance_of(CsvFile) do |file|
+          stub(file).table_already_exists(anything) { true }
+        end
+      end
+
+      it "checks for the table name being taken before downloading data" do
+        dont_allow(ChorusGnip).from_stream
         post :create, gnip_instance_import_params
-      }.to change(Events::FileImportCreated, :count).by(1)
+        response.code.should == "422"
+        response.body.should include "TABLE_EXISTS"
+      end
     end
   end
 end
