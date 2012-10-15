082dc672e9b9a435e3d5f52f7a6c9d2223a7aa19
Joseph Palermo <pair+joseph@pivotallabs.com>
2012-10-15 14:33:43 -0700

    clean up scripts; attempt to use rake in ci

--- script/bootstrap-queue.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-createdb queue_classic_chorus -Uedcadmin -p8543 -hlocalhost
-
--- script/bootstrap.sh
+++ script/bootstrap.sh
@@ -14,9 +14,6 @@ ${HOME}/workspace/chorusrails/script/init_db.sh bootstrap
 #pg_ctl start -D ${CHORUSDIR}/var/db -o "-h localhost -p8543 --bytea_output=escape"
 #sleep 5
 
-$SCRIPT_DIR/bootstrap-queue.sh
-
 echo "***** setting up project"
 bundle
-${HOME}/workspace/chorusrails/script/ci-test
 pg_ctl stop -D ${CHORUSDIR}/var/db
