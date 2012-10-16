6e25bfe92acf904ebb8772cf286afe82f1674c5f
Helge Holzmann & Jesse Smith <pair+helge+jsmith@pivotallabs.com>
2012-10-16 09:58:26 -0700

    [#37212315] Update kaggleSearchResults.json file with real userID

--- app/controllers/kaggle/users_controller.rb
+++ app/controllers/kaggle/users_controller.rb
@@ -5,7 +5,7 @@ module Kaggle
 
       sorted_user_list = user_list.map do |user_attributes|
         {
-            "id" => user_attributes['Id'],
+            "id" => user_attributes['UserId'],
             "username" => user_attributes['Username'],
             "location" => user_attributes['Location'],
             "rank" => user_attributes['KaggleRank'],
