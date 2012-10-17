e4f01e4f6483eb7e81ae68422e3ff15bcb9f6692
Brandon Liu & Gregg Van Hove <pair+brandon+gvanhove@pivotallabs.com>
2012-10-17 14:24:00 -0700

    don't wait for webserver to stop if the stop command was unsuccessful

--- packaging/stop-webserver.sh
+++ packaging/stop-webserver.sh
@@ -11,20 +11,31 @@ bin=`cd "$bin"; pwd`
 log_inline "stopping nginx "
 cd $CHORUS_HOME/vendor/nginx/nginx_dist/
 OPENSSL_CONF=$OPENSSL_CONF ./$NGINX -s stop
-wait_for_stop $NGINX_PID_FILE
+STOP_SUCCESS=$?
+if [ $STOP_SUCCESS -eq 0 ]; then
+    wait_for_stop $NGINX_PID_FILE
+fi
 cd $CHORUS_HOME
 
 case $RAILS_ENV in
     development )
         log_inline "stopping mizuno "
         bundle exec mizuno --stop --pidfile $MIZUNO_PID_FILE &>/dev/null
-        wait_for_stop $MIZUNO_PID_FILE
+        STOP_SUCCESS=`expr $STOP_SUCCESS + $?`
+        if [ $STOP_SUCCESS -eq 0 ]; then
+            wait_for_stop $MIZUNO_PID_FILE
+        fi
         ;;
     * )
         log_inline "stopping jetty "
         cd $CHORUS_HOME/vendor/jetty/
         JETTY_PID=$JETTY_PID_FILE RAILS_ENV=$RAILS_ENV ./jetty-init stop
+        STOP_SUCCESS=`expr $STOP_SUCCESS + $?`
         cd $CHORUS_HOME
-        wait_for_stop $JETTY_PID_FILE
+        if [ $STOP_SUCCESS -eq 0 ]; then
+            wait_for_stop $JETTY_PID_FILE
+        fi
         ;;
 esac
+
+exit $STOP_SUCCESS
