432c740578a31bc4f22402259211fe88ca809adf
Corina Lawu & Nitin Grewal <pair+corina+nitin@pivotallabs.com>
2012-10-16 17:44:04 -0700

    [#37212059]  send the filter in format - 'filter|comparator|value'

--- app/assets/javascripts/views/kaggle/kaggle_user_list_content_details_view.js
+++ app/assets/javascripts/views/kaggle/kaggle_user_list_content_details_view.js
@@ -2,6 +2,10 @@ chorus.views.KaggleUserListContentDetails = chorus.views.Base.extend({
     constructorName: "KaggleUserListContentDetailsView",
     templateName:"kaggle_user_list_content_details",
 
+    events: {
+        "click button.search_kaggle_user": "submitSearch"
+    },
+
     subviews: {
         '.filter_wizard': 'filterWizardView'
     },
@@ -13,5 +17,15 @@ chorus.views.KaggleUserListContentDetails = chorus.views.Base.extend({
 
     postRender: function() {
         this.$(".count").text(t("entity.name.User", {count: this.collection.count()}));
+    },
+
+    submitSearch: function() {
+        var params = this.filterWizardView.collection.map(function(model) {
+            return encodeURI(model.get("column").get("name")) + "|" +
+                encodeURI(model.get("comparator")) +"|" + encodeURI((model.get("input").value))
+        });
+
+        this.collection.urlParams = params;
+        this.collection.fetch();
     }
 });
--- spec/javascripts/views/kaggle/kaggle_user_list_content_details_view_spec.js
+++ spec/javascripts/views/kaggle/kaggle_user_list_content_details_view_spec.js
@@ -1,10 +1,10 @@
 describe("chorus.views.KaggleUserListContentDetails", function () {
     beforeEach(function () {
         this.collection = rspecFixtures.kaggleUserSet();
-        this.view = new chorus.views.KaggleUserListContentDetails({collection: this.collection});
+        this.view = new chorus.views.KaggleUserListContentDetails({collection:this.collection});
     });
 
-    it("sets up the KaggleFilterWizard", function() {
+    it("sets up the KaggleFilterWizard", function () {
         expect(this.view.filterWizardView).toBeA(chorus.views.KaggleFilterWizard);
     });
 
@@ -13,12 +13,26 @@ describe("chorus.views.KaggleUserListContentDetails", function () {
             this.view.render();
         });
 
-        it("puts the filter wizard subview in the filters div", function() {
+        it("puts the filter wizard subview in the filters div", function () {
             expect($(this.view.el).find(this.view.filterWizardView.el).length).toBeGreaterThan(0);
         });
 
         it("displays the kaggle users count", function () {
-            expect(this.view.$(".count").text()).toContainTranslation("entity.name.User", {count: this.collection.models.length});
+            expect(this.view.$(".count").text()).toContainTranslation("entity.name.User", {count:this.collection.models.length});
+        });
+        describe("#submitSearch", function () {
+            beforeEach(function () {
+                this.view.filterWizardView.collection = new chorus.collections.KaggleFilterSet([
+                    new chorus.models.KaggleFilter({column: new chorus.models.KaggleColumn({name: "column_1"}),
+                                                    comparator: "greater",
+                                                    input: { value : "||123"}})]);
+            });
+
+            it("send the params as format filter|comparator|value", function () {
+                this.view.$(".search_kaggle_user").click();
+                expect(this.view.collection).toHaveBeenFetched();
+                expect(this.server.lastFetchFor(this.view.collection).url).toContain(encodeURI("column_1|greater|%7C%7C123"))
+            });
         });
     });
 });
