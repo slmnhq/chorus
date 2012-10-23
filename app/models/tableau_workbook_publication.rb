dfe6c615d562591210045fd47944833e0a1bd32f
Andrew Brown & Brandon Liu <pair+abrown+brandon@pivotallabs.com>
2012-10-23 10:17:22 -0700

    Add port to generated tableau URLs [#38227961]

--- app/models/tableau_workbook_publication.rb
+++ app/models/tableau_workbook_publication.rb
@@ -6,12 +6,17 @@ class TableauWorkbookPublication < ActiveRecord::Base
   belongs_to :linked_tableau_workfile
 
   def workbook_url
-    base_url = Chorus::Application.config.chorus['tableau.url']
     "http://#{base_url}/workbooks/#{name}"
   end
 
   def project_url
-    base_url = Chorus::Application.config.chorus['tableau.url']
     "http://#{base_url}/workbooks?fe_project.name=#{project_name}"
+  end
+
+  def base_url
+    base_url = Chorus::Application.config.chorus['tableau.url']
+    port = Chorus::Application.config.chorus['tableau.port']
+    base_url += ":#{port}" if port && port != 80
+    base_url
   end
 end
--- spec/models/tableau_workbook_publication_spec.rb
+++ spec/models/tableau_workbook_publication_spec.rb
@@ -9,10 +9,10 @@ describe TableauWorkbookPublication do
     Chorus::Application.config.chorus = @previous_config
   end
 
-  context "tableau is configured" do
-    let(:model) { tableau_workbook_publications(:default) }
-    let(:tableau_base_url) { 'fake.domain.com' }
+  let(:model) { tableau_workbook_publications(:default) }
+  let(:tableau_base_url) { 'fake.domain.com' }
 
+  context "tableau is configured" do
     before do
       Chorus::Application.config.chorus = {'tableau.url' => tableau_base_url}
     end
@@ -26,6 +26,24 @@ describe TableauWorkbookPublication do
     describe "#project_url" do
       it "should return the correct url" do
         model.project_url.should == "http://#{tableau_base_url}/workbooks?fe_project.name=#{model.project_name}"
+      end
+    end
+  end
+
+  context "tableau is configured with a port" do
+    before do
+      Chorus::Application.config.chorus = {'tableau.url' => tableau_base_url, 'tableau.port' => 8000}
+    end
+
+    describe "#workbook_url" do
+      it "should return the correct url" do
+        model.workbook_url.should == "http://#{tableau_base_url}:8000/workbooks/#{model.name}"
+      end
+    end
+
+    describe "#project_url" do
+      it "should return the correct url" do
+        model.project_url.should == "http://#{tableau_base_url}:8000/workbooks?fe_project.name=#{model.project_name}"
       end
     end
   end
